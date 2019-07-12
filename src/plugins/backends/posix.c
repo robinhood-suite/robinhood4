8f210fc896217ebbeef1387dd2aed9786445ef08
Quentin Bouget <quentin.bouget@cea.fr>
2019-07-12 12:05:23 +0000

    posix: implement the backend plugin interface
    Change-Id: I3c200a7e57d9e7f329236a964800e0b1c878c161
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>

---
+++ src/plugins/backends/posix.c
@@ -0,0 +1,29 @@
+/* This file is part of the RobinHood Library
+ * Copyright (C) 2019 Commissariat a l'energie atomique et aux energies
+ * 		      alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ *
+ * author: Quentin Bouget <quentin.bouget@cea.fr>
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdlib.h>
+
+#include "robinhood/backends/posix.h"
+#include "robinhood/plugins/backend.h"
+
+static const struct rbh_backend_plugin_operations POSIX_BACKEND_PLUGIN_OPS = {
+    .new = rbh_posix_backend_new,
+};
+
+const struct rbh_backend_plugin RBH_BACKEND_PLUGIN_SYMBOL(POSIX) = {
+    .plugin = {
+        .name = RBH_POSIX_BACKEND_NAME,
+        .version = RBH_POSIX_BACKEND_VERSION,
+    },
+    .ops = &POSIX_BACKEND_PLUGIN_OPS,
+};
