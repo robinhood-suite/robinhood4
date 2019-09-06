614344d261309cf4cdadad912ca8f52947f6f994
Quentin Bouget <quentin.bouget@cea.fr>
2019-09-06 07:43:13 +0000

    style: replace every tab in C sources with spaces
    Change-Id: Ifc40f43cf4f30983a2eaac2ec67a5d2c7a47f859
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/robinhood/+/470454
    Reviewed-by: Thomas Leibovici <thomas.leibovici@cea.fr>

--- src/itertools.c
+++ src/itertools.c
@@ -1,6 +1,6 @@
 /* This file is part of the RobinHood Library
  * Copyright (C) 2019 Commissariat a l'energie atomique et aux energies
- * 		      alternatives
+ *                    alternatives
  *
  * SPDX-License-Identifer: LGPL-3.0-or-later
  *
