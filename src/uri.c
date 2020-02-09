902e66b0c26d0ea4138d62135d6611aa4b687e0c
Quentin Bouget <quentin.bouget@cea.fr>
2020-02-09 20:48:24 +0100

    uri: shorten a switch statement using case ranges
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>
    Change-Id: I094c5622c58b80bbf484302b855729b7eb4907ee
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/robinhood/+/484356
    Reviewed-by: Dominique Martinet <asmadeus@codewreck.org>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- src/uri.c
+++ src/uri.c
@@ -144,44 +144,12 @@ static int
 hex2int(char c)
 {
     switch (c) {
-    case '0':
-        return 0;
-    case '1':
-        return 1;
-    case '2':
-        return 2;
-    case '3':
-        return 3;
-    case '4':
-        return 4;
-    case '5':
-        return 5;
-    case '6':
-        return 6;
-    case '7':
-        return 7;
-    case '8':
-        return 8;
-    case '9':
-        return 9;
-    case 'a':
-    case 'A':
-        return 10;
-    case 'b':
-    case 'B':
-        return 11;
-    case 'c':
-    case 'C':
-        return 12;
-    case 'd':
-    case 'D':
-        return 13;
-    case 'e':
-    case 'E':
-        return 14;
-    case 'f':
-    case 'F':
-        return 15;
+    case '0' ... '9':
+        return c - '0';
+    case 'a' ... 'f':
+        return c - 'a' + 10;
+    case 'A' ... 'F':
+        return c - 'A' + 10;
     }
     errno = EINVAL;
     return -1;
