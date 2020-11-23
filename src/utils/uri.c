c6b05cd129f4e0e86e86a2d159abea0943e61eee
Quentin Bouget <quentin.bouget@cea.fr>
2020-11-23 01:25:39 +0100

    utils: support URIs with paths for posix backends
    Posix backends do not support any sort of filtering. But the generic
    implementation of rbh_backend_from_uri() needs it.
    
    This patch special cases posix backend URIs with paths so that they
    can be made to work as well.
    
    Note that posix backends require CAP_DAC_READ_SEARCH to branch(), and
    this patch does not change that.
    
    Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>
    Change-Id: I481bb27683474a71fa4f84b4e09b65673600eb49
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/librobinhood/+/506317
    Reviewed-by: Dominique Martinet <asmadeus@codewreck.org>

--- src/utils/uri.c
+++ src/utils/uri.c
@@ -12,8 +12,10 @@
 #include <dlfcn.h>
 #include <errno.h>
 #include <error.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "robinhood/plugins/backend.h"
 #include "robinhood/utils.h"
@@ -50,15 +52,103 @@ backend_new(const char *type, const char *fsname)
     return backend;
 }
 
+static struct file_handle *
+path2handle_at(int dirfd, const char *path, int flags)
+{
+    struct file_handle *handle;
+    int mount_id;
+
+    handle = malloc(MAX_HANDLE_SZ);
+    if (handle == NULL)
+        error(EXIT_FAILURE, errno, "malloc");
+    handle->handle_bytes = MAX_HANDLE_SZ - sizeof(*handle);
+
+    while (name_to_handle_at(dirfd, path, handle, &mount_id, flags)) {
+        if (errno != EOVERFLOW)
+            error(EXIT_FAILURE, errno, "name_to_handle_at: %s", path);
+
+        handle = realloc(handle, sizeof(*handle) + handle->handle_bytes);
+        if (handle == NULL)
+            error(EXIT_FAILURE, errno, "realloc");
+    }
+
+    return handle;
+}
+
+static struct rbh_id *
+path2id_at(int dirfd, const char *path, int flags)
+{
+    struct file_handle *handle;
+    struct rbh_id *id;
+
+    handle = path2handle_at(dirfd, path, flags);
+
+    id = rbh_id_from_file_handle(handle);
+    if (id == NULL)
+        error(EXIT_FAILURE, errno, "rbh_id_from_file_handle");
+
+    free(handle);
+    return id;
+}
+
 static struct rbh_backend *
-backend_from_uri(const struct rbh_uri *uri)
+posix_backend_branch_from_path(struct rbh_backend *backend, const char *fsname,
+                               const char *path)
+{
+    struct rbh_backend *branch;
+    struct rbh_id *id;
+    int save_errno;
+    int dirfd;
+
+    dirfd = open(fsname, O_RDONLY);
+    if (dirfd < 0)
+        error(EXIT_FAILURE, errno, "open: %s", fsname);
+
+    /* Discard every leading '/' in `path' */
+    while (*path == '/')
+        path++;
+
+    /* AT_EMPTY_PATH is required as `path' may be empty */
+    id = path2id_at(dirfd, path, AT_EMPTY_PATH);
+    /* Ignore errors on close */
+    close(dirfd);
+
+    branch = rbh_backend_branch(backend, id);
+    save_errno = errno;
+    free(id);
+    errno = save_errno;
+    return branch;
+}
+
+static struct rbh_backend *
+backend_branch_from_path(struct rbh_backend *backend, const char *path)
 {
     const struct rbh_filter_projection ID_ONLY = {
         .fsentry_mask = RBH_FP_ID,
     };
+    struct rbh_fsentry *fsentry;
+    struct rbh_backend *branch;
+    int save_errno;
+
+    fsentry = rbh_backend_fsentry_from_path(backend, path, &ID_ONLY);
+    if (fsentry == NULL)
+        error(EXIT_FAILURE, errno, "rbh_backend_fsentry_from_path");
+
+    if (!(fsentry->mask & RBH_FP_ID))
+        error(EXIT_FAILURE, ENODATA, "rbh_backend_fsentry_from_path");
+
+    branch = rbh_backend_branch(backend, &fsentry->id);
+    save_errno = errno;
+    free(fsentry);
+    errno = save_errno;
+    return branch;
+}
+
+static struct rbh_backend *
+backend_from_uri(const struct rbh_uri *uri)
+{
     struct rbh_backend *backend = backend_new(uri->backend, uri->fsname);
     struct rbh_backend *branch = NULL; /* gcc: unitialized variable */
-    struct rbh_fsentry *fsentry;
     int save_errno;
 
     switch (uri->type) {
@@ -68,17 +158,12 @@ backend_from_uri(const struct rbh_uri *uri)
         branch = rbh_backend_branch(backend, uri->id);
         break;
     case RBH_UT_PATH:
-        fsentry = rbh_backend_fsentry_from_path(backend, uri->path, &ID_ONLY);
-        if (fsentry == NULL)
-            error(EXIT_FAILURE, errno, "rbh_backend_fsentry_from_path");
-
-        if (!(fsentry->mask & RBH_FP_ID))
-            error(EXIT_FAILURE, ENODATA, "rbh_backend_fsentry_from_path");
-
-        branch = rbh_backend_branch(backend, &fsentry->id);
-        save_errno = errno;
-        free(fsentry);
-        errno = save_errno;
+        /* The posix backend does not support filtering, treat it differently */
+        if (backend->id == RBH_BI_POSIX)
+            branch = posix_backend_branch_from_path(backend, uri->fsname,
+                                                    uri->path);
+        else
+            branch = backend_branch_from_path(backend, uri->path);
         break;
     }
 
