43b9fcd18586dc05b5541a5a736acad011f5f255
Quentin Bouget <ypsah@devyard.org>
2021-06-29 01:34:13 +0200

    sink: implement a robinhood backend sink
    Signed-off-by: Quentin Bouget <ypsah@devyard.org>
    Change-Id: Id6f5e5f96d005b792d24dad334b72f59e84ab8cd
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/rbh-fsevents/+/519665
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>

--- src/sinks/backend.c
+++ src/sinks/backend.c
@@ -5,13 +5,55 @@
 #endif
 
 #include <error.h>
+#include <errno.h>
 #include <stdlib.h>
+
+#include <robinhood/backend.h>
 
 #include "sink.h"
 
-struct sink *
-sink_from_backend(struct rbh_backend *backend __attribute__((unused)))
+struct backend_sink {
+    struct sink sink;
+    struct rbh_backend *backend;
+};
+
+static int
+backend_sink_process(void *_sink, struct rbh_iterator *fsevents)
 {
-    error(EXIT_FAILURE, ENOSYS, __func__);
-    __builtin_unreachable();
+    struct backend_sink *sink = _sink;
+
+    return rbh_backend_update(sink->backend, fsevents) >= 0;
+}
+
+static void
+backend_sink_destroy(void *_sink)
+{
+    struct backend_sink *sink = _sink;
+
+    rbh_backend_destroy(sink->backend);
+    free(sink);
+}
+
+static const struct sink_operations BACKEND_SINK_OPS = {
+    .process = backend_sink_process,
+    .destroy = backend_sink_destroy,
+};
+
+static const struct sink BACKEND_SINK = {
+    .name = "backend",
+    .ops = &BACKEND_SINK_OPS,
+};
+
+struct sink *
+sink_from_backend(struct rbh_backend *backend)
+{
+    struct backend_sink *sink;
+
+    sink = malloc(sizeof(*sink));
+    if (sink == NULL)
+        error(EXIT_FAILURE, errno, "malloc");
+
+    sink->sink = BACKEND_SINK;
+    sink->backend = backend;
+    return &sink->sink;
 }
