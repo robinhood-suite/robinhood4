5eb9045c85fc5f0739f510ca208dcf284d0da16c
Quentin Bouget <ypsah@devyard.org>
2021-09-04 00:31:50 +0200

    headers: order headers alphabetically
    Signed-off-by: Quentin Bouget <ypsah@devyard.org>
    Change-Id: Ib658df888925c00a1772b0b50a05abef86e10c36
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/librobinhood/+/523459
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- src/filter.c
+++ src/filter.c
@@ -17,8 +17,8 @@
 #include "robinhood/filter.h"
 #include "robinhood/statx.h"
 
-#include "value.h"
 #include "utils.h"
+#include "value.h"
 
 static ssize_t
 filter_field_data_size(const struct rbh_filter_field *field)
