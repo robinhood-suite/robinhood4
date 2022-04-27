4e4ee23608235d36102e49a323893d029cf37215
Yoann Valeri <yoann.valeri@cea.fr>
2022-04-27 12:16:04 +0200

    lustre: record "hsm" attributes as xattrs
    Change-Id: I1b9f3dd475947ee861ed2a6e76da598e4cb87eef
    Signed-off-by: Yoann Valeri <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/librobinhood/+/537260
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- src/backends/lustre/lustre.c
+++ src/backends/lustre/lustre.c
@@ -9,6 +9,7 @@
 #include "robinhood/backends/lustre.h"
 
 static __thread struct rbh_sstack *_values;
+static __thread bool is_dir;
 
 static inline int
 fill_pair(const char *key, const struct rbh_value *value,
@@ -32,6 +33,17 @@ fill_string_pair(const char *key, const char *str, const int len,
     };
 
     return fill_pair(key, &string_value, pair);
+}
+
+static inline int
+fill_uint32_pair(const char *key, uint32_t integer, struct rbh_value_pair *pair)
+{
+    const struct rbh_value uint32_value = {
+        .type = RBH_VT_UINT32,
+        .uint32 = integer,
+    };
+
+    return fill_pair(key, &uint32_value, pair);
 }
 
 /**
@@ -66,7 +78,43 @@ xattrs_get_fid(int fd, struct rbh_value_pair *pairs)
     return rc ? : 1;
 }
 
-#define XATTRS_N_FUNC 1
+/**
+ * Record a file's hsm attributes (state and archive_id) in \p pairs
+ *
+ * @param fd        file descriptor to check
+ * @param pairs     list of pairs to fill
+ *
+ * @return          number of filled \p pairs
+ */
+static int
+xattrs_get_hsm(int fd, struct rbh_value_pair *pairs)
+{
+    struct hsm_user_state hus;
+    int subcount = 0;
+    int rc;
+
+    if (is_dir)
+        return 0;
+
+    rc = llapi_hsm_state_get_fd(fd, &hus);
+    if (rc) {
+        errno = -rc;
+        return -1;
+    }
+
+    rc = fill_uint32_pair("hsm_state", hus.hus_states, &pairs[subcount++]);
+    if (rc)
+        return -1;
+
+    rc = fill_uint32_pair("hsm_archive_id", hus.hus_archive_id,
+                          &pairs[subcount++]);
+    if (rc)
+        return -1;
+
+    return subcount;
+}
+
+#define XATTRS_N_FUNC 2
 
 static ssize_t
 lustre_ns_xattrs_callback(const int fd, const uint16_t mode,
@@ -74,11 +122,12 @@ lustre_ns_xattrs_callback(const int fd, const uint16_t mode,
                           struct rbh_sstack *values)
 {
     int (*xattrs_funcs[XATTRS_N_FUNC])(int, struct rbh_value_pair *) = {
-        xattrs_get_fid
+        xattrs_get_fid, xattrs_get_hsm
     };
     int count = 0;
     int subcount;
 
+    is_dir = S_ISDIR(mode);
     _values = values;
 
     for (int i = 0; i < XATTRS_N_FUNC; ++i) {
