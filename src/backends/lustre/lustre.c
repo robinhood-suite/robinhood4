79200af366dff91d854c66da5f93d21aa6c8db08
Yoann Valeri <yoann.valeri@cea.fr>
2022-04-28 09:34:31 +0200

    lustre: record basic "layout" attributes as xattrs
    Change-Id: I49ac7fb3282698420889eff1ebdd9a3bcd804cef
    Signed-off-by: Yoann Valeri <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/librobinhood/+/537261
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- src/backends/lustre/lustre.c
+++ src/backends/lustre/lustre.c
@@ -2,6 +2,10 @@
 # include "config.h"
 #endif
 
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+
 #include <lustre/lustreapi.h>
 
 #include "robinhood/backends/posix.h"
@@ -10,6 +14,7 @@
 
 static __thread struct rbh_sstack *_values;
 static __thread bool is_dir;
+static __thread bool is_reg;
 
 static inline int
 fill_pair(const char *key, const struct rbh_value *value,
@@ -114,23 +119,140 @@ xattrs_get_hsm(int fd, struct rbh_value_pair *pairs)
     return subcount;
 }
 
-#define XATTRS_N_FUNC 2
+/**
+ * Record a file's magic number and layout generation in \p pairs
+ *
+ * @param fd        file descriptor to check
+ * @param pairs     list of pairs to fill
+ *
+ * @return          number of filled \p pairs
+ */
+static int
+xattrs_get_magic_and_gen(int fd, struct rbh_value_pair *pairs)
+{
+    char lov_buf[XATTR_SIZE_MAX];
+    ssize_t xattr_size;
+    uint32_t magic = 0;
+    int magic_str_len;
+    int subcount = 0;
+    uint32_t gen = 0;
+    char *magic_str;
+    int rc;
+
+    xattr_size = fgetxattr(fd, XATTR_LUSTRE_LOV, lov_buf, sizeof(lov_buf));
+    if (xattr_size < 0)
+        return -1;
+
+    magic = ((struct lov_user_md *) lov_buf)->lmm_magic;
+
+    switch (magic) {
+    case LOV_USER_MAGIC_V1:
+        magic_str = "LOV_USER_MAGIC_V1";
+        magic_str_len = 18;
+        gen = ((struct lov_user_md_v1 *) lov_buf)->lmm_layout_gen;
+        break;
+    case LOV_USER_MAGIC_COMP_V1:
+        magic_str = "LOV_USER_MAGIC_COMP_V1";
+        magic_str_len = 23;
+        gen = ((struct lov_comp_md_v1 *) lov_buf)->lcm_layout_gen;
+        break;
+    case LOV_USER_MAGIC_SEL:
+        magic_str = "LOV_USER_MAGIC_SEL";
+        magic_str_len = 19;
+        gen = ((struct lov_comp_md_v1 *) lov_buf)->lcm_layout_gen;
+        break;
+    case LOV_USER_MAGIC_V3:
+        magic_str = "LOV_USER_MAGIC_V3";
+        magic_str_len = 18;
+        gen = ((struct lov_user_md_v3 *) lov_buf)->lmm_layout_gen;
+        break;
+    case LOV_USER_MAGIC_SPECIFIC:
+        magic_str = "LOV_USER_MAGIC_SPECIFIC";
+        magic_str_len = 24;
+        gen = ((struct lov_user_md_v3 *) lov_buf)->lmm_layout_gen;
+        break;
+    case LOV_USER_MAGIC_FOREIGN:
+        magic_str = "LOV_USER_MAGIC_FOREIGN";
+        magic_str_len = 23;
+        gen = -1;
+        break;
+    default:
+        errno = EINVAL;
+        return -1;
+    }
+
+    rc = fill_string_pair("magic", magic_str, magic_str_len,
+                          &pairs[subcount++]);
+    if (rc)
+        return -1;
+
+    rc = fill_uint32_pair("gen", gen, &pairs[subcount++]);
+    if (rc)
+        return -1;
+
+    return subcount;
+}
+
+/**
+ * Record a file's layout attributes (main flags, and magic number and layout
+ * generation if it is a regular file) in \p pairs
+ *
+ * @param fd        file descriptor to check
+ * @param pairs     list of pairs to fill
+ *
+ * @return          number of filled \p pairs
+ */
+static int
+xattrs_get_layout(int fd, struct rbh_value_pair *pairs)
+{
+    struct llapi_layout *layout;
+    int subcount = 0;
+    uint32_t flags;
+    int rc;
+
+    layout = llapi_layout_get_by_fd(fd, 0);
+    if (layout == NULL)
+        return -1;
+
+    rc = llapi_layout_flags_get(layout, &flags);
+    if (rc)
+        goto err;
+
+    rc = fill_uint32_pair("flags", flags, &pairs[subcount++]);
+    if (rc)
+        goto err;
+
+    if (is_reg) {
+        rc = xattrs_get_magic_and_gen(fd, &pairs[subcount]);
+        if (rc < 0)
+            goto err;
+
+
+        subcount += rc;
+        rc = 0;
+    }
+
+err:
+    llapi_layout_free(layout);
+    return rc ? rc : subcount;
+}
 
 static ssize_t
 lustre_ns_xattrs_callback(const int fd, const uint16_t mode,
                           struct rbh_value_pair *pairs,
                           struct rbh_sstack *values)
 {
-    int (*xattrs_funcs[XATTRS_N_FUNC])(int, struct rbh_value_pair *) = {
-        xattrs_get_fid, xattrs_get_hsm
+    int (*xattrs_funcs[])(int, struct rbh_value_pair *) = {
+        xattrs_get_fid, xattrs_get_hsm, xattrs_get_layout
     };
     int count = 0;
     int subcount;
 
     is_dir = S_ISDIR(mode);
+    is_reg = S_ISREG(mode);
     _values = values;
 
-    for (int i = 0; i < XATTRS_N_FUNC; ++i) {
+    for (int i = 0; i < sizeof(xattrs_funcs) / sizeof(xattrs_funcs[0]); ++i) {
         subcount = xattrs_funcs[i](fd, &pairs[count]);
         if (subcount == -1)
             return -1;
