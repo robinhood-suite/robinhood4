cea-hpc/rbh-find-lustre

filters: add "-fid" option for filtering
Change-Id: Id5b20d3d8da8254127156098ab98efe377759842
Signed-off-by: Yoann Valeri <yoann.valeri@cea.fr>

include/parser.h
@@ -13,7 +13,8 @@
 enum lustre_predicate {
     PRED_INITIAL = 128,
 
-    PRED_HSM_STATE = PRED_INITIAL,
+    PRED_FID = PRED_INITIAL,
+    PRED_HSM_STATE,
 
     PRED_MAX,
 };

robinhood-suite/robinhood4

include/parser.h
@@ -13,7 +13,8 @@
 enum lustre_predicate {
     PRED_MIN = 128,
 
-    PRED_HSM_STATE = PRED_MIN,
+    PRED_FID = PRED_MIN,
+    PRED_HSM_STATE,
 
     PRED_MAX,
 };