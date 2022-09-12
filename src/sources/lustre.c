5477f717d7bfab8ee06472d9b1d8a3adb177f892
VALERI Yoann <yoann.valeri@cea.fr>
2022-09-12 14:27:37 +0200

    changelog: implement initial switch for changelog events
    Change-Id: I7f003de0d50e34001a1f60717cffb38d27d7c61e
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/rbh-fsevents/+/543336
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-by: Dominique Martinet <asmadeus@codewreck.org>

--- src/sources/lustre.c
+++ src/sources/lustre.c
@@ -43,9 +43,13 @@ static const void *
 lustre_changelog_iter_next(void *iterator)
 {
     struct lustre_changelog_iterator *records = iterator;
+    const struct rbh_fsevent *partial_event;
     struct changelog_rec *record;
+    struct rbh_id *id = NULL;
+    int save_errno;
     int rc;
 
+retry:
     rc = llapi_changelog_recv(records->reader, &record);
     if (rc < 0) {
         errno = -rc;
@@ -56,7 +60,61 @@ lustre_changelog_iter_next(void *iterator)
         return NULL;
     }
 
-    return fsevent_from_record(record);
+    id = rbh_id_from_lu_fid(&record->cr_tfid);
+    if (id == NULL) {
+        save_errno = errno;
+        llapi_changelog_free(&record);
+        errno = save_errno;
+        return NULL;
+    }
+
+    switch(record->cr_type) {
+    case CL_CREATE:     /* RBH_FET_UPSERT */
+        partial_event = rbh_fsevent_upsert_new(id, NULL, NULL, NULL);
+        goto end_event;
+    case CL_CLOSE:
+        partial_event = rbh_fsevent_upsert_new(id, NULL, NULL, NULL);
+        goto end_event;
+    case CL_MKDIR:      /* RBH_FET_UPSERT */
+        partial_event = rbh_fsevent_upsert_new(id, NULL, NULL, NULL);
+        goto end_event;
+    case CL_HARDLINK:   /* RBH_FET_LINK? */
+    case CL_SOFTLINK:   /* RBH_FET_UPSERT + symlink */
+    case CL_MKNOD:
+    case CL_UNLINK:     /* RBH_FET_UNLINK or RBH_FET_DELETE */
+    case CL_RMDIR:      /* RBH_FET_UNLINK or RBH_FET_DELETE */
+    case CL_RENAME:     /* RBH_FET_UPSERT */
+    case CL_EXT:
+    case CL_OPEN:
+    case CL_LAYOUT:
+    case CL_TRUNC:
+    case CL_SETATTR:    /* RBH_FET_XATTR? */
+    case CL_SETXATTR:   /* RBH_FET_XATTR */
+    case CL_HSM:
+    case CL_MTIME:
+    case CL_ATIME:
+    case CL_CTIME:
+    case CL_MIGRATE:
+    case CL_FLRW:
+    case CL_RESYNC:
+    case CL_GETXATTR:
+    case CL_DN_OPEN:
+        partial_event = fsevent_from_record(record);
+        goto end_event;
+    default: /* CL_MARK or other events */
+        /* Events not managed yet, we go to the next record */
+        llapi_changelog_free(&record);
+        goto retry;
+    }
+
+    __builtin_unreachable();
+
+end_event:
+    save_errno = errno;
+    llapi_changelog_free(&record);
+    errno = save_errno;
+
+    return partial_event;
 }
 
 static void
