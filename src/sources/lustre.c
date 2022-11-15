155e51579d6eb6ac1e2733b8ce917b64e058955d
Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
2022-11-15 16:34:31 +0100

    lustre: add SETATTR/CLOSE/{ACM}TIME events
    Change-Id: Ic4a019f1d5b34c727dc6ff1ce7fb9b71ebfa0ecc
    Signed-off-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/rbh-fsevents/+/546282
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>
    Tested-by: VALERI Yoann <yoann.valeri@cea.fr>

--- src/sources/lustre.c
+++ src/sources/lustre.c
@@ -339,11 +339,24 @@ build_setxattr_event(unsigned int process_step, struct changelog_rec *record,
     __builtin_unreachable();
 }
 
+static int
+build_statx_event(uint32_t statx_enrich_mask, struct rbh_fsevent *fsevent)
+{
+    fsevent->type = RBH_FET_UPSERT;
+
+    fsevent->xattrs = build_enrich_map(fill_statx, &statx_enrich_mask);
+    if (fsevent->xattrs.pairs == NULL)
+        return -1;
+
+    return 0;
+}
+
 static const void *
 lustre_changelog_iter_next(void *iterator)
 {
     struct lustre_changelog_iterator *records = iterator;
     struct rbh_fsevent *fsevent = NULL;
+    uint32_t statx_enrich_mask = 0;
     struct changelog_rec *record;
     struct rbh_id *id;
     int save_errno;
@@ -386,7 +399,21 @@ retry:
     case CL_SETXATTR:
         rc = build_setxattr_event(records->process_step, record, fsevent);
         break;
+    case CL_SETATTR:
+        statx_enrich_mask = RBH_STATX_ALL;
+        /* fall through */
     case CL_CLOSE:
+    case CL_MTIME:
+        statx_enrich_mask |= RBH_STATX_MTIME_SEC | RBH_STATX_MTIME_NSEC |
+                             RBH_STATX_SIZE | RBH_STATX_BLOCKS;
+        /* fall through */
+    case CL_CTIME:
+        statx_enrich_mask |= RBH_STATX_CTIME_SEC | RBH_STATX_CTIME_NSEC;
+        /* fall through */
+    case CL_ATIME:
+        statx_enrich_mask |= RBH_STATX_ATIME_SEC | RBH_STATX_ATIME_NSEC;
+        rc = build_statx_event(statx_enrich_mask, fsevent);
+        break;
     case CL_MKDIR:      /* RBH_FET_UPSERT */
     case CL_HARDLINK:   /* RBH_FET_LINK? */
     case CL_SOFTLINK:   /* RBH_FET_UPSERT + symlink */
@@ -398,11 +425,7 @@ retry:
     case CL_OPEN:
     case CL_LAYOUT:
     case CL_TRUNC:
-    case CL_SETATTR:    /* RBH_FET_XATTR? */
     case CL_HSM:
-    case CL_MTIME:
-    case CL_ATIME:
-    case CL_CTIME:
     case CL_MIGRATE:
     case CL_FLRW:
     case CL_RESYNC:
