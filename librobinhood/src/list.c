d3d263b3adca3736a3802b4006e3d804532fa04e
Courrier Guillaume <guillaume.courrier@cea.fr>
2023-07-10 16:40:46 +0200

    librobinhood: add rbh_list_node type
    rbh_list_node is added to represent a doubly linked
    list based on the list_head kernel API.
    
    Change-Id: Ifc6c58ad0a7a15efe754d83722302120cf59debb
    Signed-off-by: Courrier Guillaume <guillaume.courrier@cea.fr>

---
+++ librobinhood/src/list.c
@@ -0,0 +1,51 @@
+#include <robinhood/list.h>
+
+bool
+rbh_list_empty(struct rbh_list_node *list)
+{
+    return list->next == list;
+}
+
+void
+rbh_list_init(struct rbh_list_node *list)
+{
+    list->next = list;
+    list->prev = list;
+}
+
+void
+rbh_list_add_tail(struct rbh_list_node *list, struct rbh_list_node *node)
+{
+    list->prev->next = node;
+    node->next = list;
+    node->prev = list->prev;
+    list->prev = node;
+}
+
+void
+rbh_list_add(struct rbh_list_node *list, struct rbh_list_node *node)
+{
+    list->next->prev = node;
+    node->next = list->next;
+    node->prev = list;
+    list->next = node;
+}
+
+void
+rbh_list_splice_tail(struct rbh_list_node *list1, struct rbh_list_node *list2)
+{
+    struct rbh_list_node *last = list1->prev;
+
+    list1->prev = list2->prev;
+    last->next = list2->next;
+
+    list1->prev->next = list1;
+    last->next->prev = last;
+}
+
+void
+rbh_list_del(struct rbh_list_node *node)
+{
+    node->next->prev = node->prev;
+    node->prev->next = node->next;
+}
