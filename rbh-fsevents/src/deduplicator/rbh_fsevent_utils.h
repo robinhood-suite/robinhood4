8eba4abf3323aed28e33c097d949def2ac891232
Courrier Guillaume <guillaume.courrier@cea.fr>
2023-08-09 11:51:23 +0200

    rbh-fsevents: add utils functions to manage fsevents
    Change-Id: Id3f66c0a59deb79cee08513b38114c512c4cb374
    Signed-off-by: Courrier Guillaume <guillaume.courrier@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/557620
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Patrice LUCAS <patrice.lucas@cea.fr>

---
+++ rbh-fsevents/src/deduplicator/rbh_fsevent_utils.h
@@ -0,0 +1,55 @@
+/* SPDX-License-Identifer: LGPL-3.0-or-later */
+
+#ifndef RBH_FSEVENT_UTILS_H
+#define RBH_FSEVENT_UTILS_H
+
+/**
+ * @file
+ *
+ * Utility functions to manage struct rbh_fsevent
+ *
+ * An RBH_FET_XATTR event can contain several elements:
+ * 1. an extended attribute to store in the backend:
+ *   xattrs:
+ *       fid: <binary>
+ * 2. a partial extended attribute that the enricher should fetch:
+ *   xattrs:
+ *       rbh-fsevents:
+ *           - "user.test"
+ * 3. a specific attribute to enrich that is specific to the source. For example,
+ *   with Lustre:
+ *   xattrs:
+ *       rbh-fsevents:
+ *           - "lustre"
+ *
+ * The following prototypes give tools to more easily manipulate these different
+ * types of FS events. The first type will be refered to as "xattr", the second
+ * one as "partial xattr" and the last one as "enrich elements".
+ */
+
+#include <robinhood.h>
+
+const struct rbh_value *
+rbh_map_find(const struct rbh_value_map *map, const char *key);
+
+const struct rbh_value_map *
+rbh_fsevent_find_fsevents_map(const struct rbh_fsevent *fsevent);
+
+const struct rbh_value *
+rbh_fsevent_find_partial_xattr(const struct rbh_fsevent *fsevent,
+                               const char *key);
+
+const struct rbh_value_pair *
+rbh_fsevent_find_enrich_element(const struct rbh_fsevent *fsevent,
+                                const char *key);
+
+const struct rbh_value_pair *
+rbh_fsevent_find_xattr(const struct rbh_fsevent *fsevent,
+                       const char *key);
+
+int
+rbh_fsevent_deep_copy(struct rbh_fsevent *dst,
+                      const struct rbh_fsevent *src,
+                      struct rbh_sstack *stack);
+
+#endif
