d190a978ab03f87a485236102120202edb6c3748
Courrier Guillaume <guillaume.courrier@cea.fr>
2023-08-11 11:16:20 +0200

    rbh-fsevents: add tests for link/unlink/delete dedup
    Change-Id: I8b382d86caf7f336db7917df6adef17595b11604
    Signed-off-by: Courrier Guillaume <guillaume.courrier@cea.fr>

--- rbh-fsevents/tests/unit/utils/utils.c
+++ rbh-fsevents/tests/unit/utils/utils.c
@@ -117,3 +117,94 @@ fake_create(struct rbh_fsevent *fsevent, struct rbh_id *id,
     fsevent->link.name = "test";
     fsevent->link.parent_id = parent;
 }
+
+void
+fake_link(struct rbh_fsevent *fsevent, struct rbh_id *id, const char *name,
+          struct rbh_id *parent)
+{
+    memset(fsevent, 0, sizeof(*fsevent));
+
+    fsevent->type = RBH_FET_LINK;
+    fsevent->id = *id;
+    fsevent->link.name = name;
+    fsevent->link.parent_id = parent;
+}
+
+void
+fake_unlink(struct rbh_fsevent *fsevent, struct rbh_id *id, const char *name,
+            struct rbh_id *parent)
+{
+    memset(fsevent, 0, sizeof(*fsevent));
+
+    fsevent->type = RBH_FET_UNLINK;
+    fsevent->id = *id;
+    fsevent->link.name = name;
+    fsevent->link.parent_id = parent;
+}
+
+void
+fake_delete(struct rbh_fsevent *fsevent, struct rbh_id *id)
+{
+    memset(fsevent, 0, sizeof(*fsevent));
+
+    fsevent->type = RBH_FET_DELETE;
+    fsevent->id = *id;
+}
+
+struct rbh_value_map
+make_upsert_statx(struct rbh_value_pair *pairs,
+                  struct rbh_value *map_values,
+                  uint32_t mask)
+{
+    struct rbh_value_map xattr = {
+        .count = 1,
+        .pairs = &pairs[0], /* "rbh-fsevents" */
+    };
+    struct rbh_value_pair rbh_fsevents_map = { /* pairs[0] */
+        .key = "rbh-fsevents",
+        .value = &map_values[0],
+    };
+    struct rbh_value rbh_fsevents_map_value = { /* map_values[0] */
+        .type = RBH_VT_MAP,
+        .map = {
+            .count = 1,
+            .pairs = &pairs[1], /* "statx" */
+        },
+    };
+    struct rbh_value_pair statx_mask = {
+        .key = "statx",
+        .value = &map_values[1], /* mask */
+    };
+    struct rbh_value statx_mask_value = {
+        .type = RBH_VT_UINT32,
+        .uint32 = mask,
+    };
+
+    memcpy(&pairs[0], &rbh_fsevents_map, sizeof(rbh_fsevents_map));
+    memcpy(&pairs[1], &statx_mask, sizeof(statx_mask));
+
+    memcpy(&map_values[0], &rbh_fsevents_map_value,
+           sizeof(rbh_fsevents_map_value));
+    memcpy(&map_values[1], &statx_mask_value, sizeof(statx_mask_value));
+
+    return xattr;
+}
+
+void
+fake_upsert(struct rbh_fsevent *fsevent, struct rbh_id *id, uint32_t mask,
+            struct rbh_statx *statx)
+{
+    struct rbh_value *map_values;
+    struct rbh_value_pair *pairs;
+
+    pairs = malloc(2 * sizeof(*pairs));
+    ck_assert_ptr_nonnull(pairs);
+
+    map_values = malloc(2 * sizeof(*map_values));
+    ck_assert_ptr_nonnull(map_values);
+
+    memset(fsevent, 0, sizeof(*fsevent));
+    fsevent->id = *id;
+    fsevent->upsert.statx = statx;
+    fsevent->xattrs = make_upsert_statx(pairs, map_values, mask);
+}
