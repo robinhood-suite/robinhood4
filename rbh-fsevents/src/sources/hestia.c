53deb473fdd0429f994007d3b665bac24058f5e4
Yoann Valeri <yoann.valeri@cea.fr>
2023-10-06 14:17:10 +0200

    hestia: add Hestia source
    This patch adds the Hestia source as a YAML file to read.
    
    Change-Id: Ifb05a1efd582fb735a847789645a35983c0c97ae
    Signed-off-by: Yoann Valeri <yoann.valeri@cea.fr>

---
+++ rbh-fsevents/src/sources/hestia.c
@@ -0,0 +1,104 @@
+/* SPDX-License-Identifer: LGPL-3.0-or-later */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <errno.h>
+#include <error.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <miniyaml.h>
+#include <robinhood/fsevent.h>
+
+#include "include/serialization.h"
+#include "source.h"
+
+#include "yaml_file.h"
+
+static const void *
+hestia_fsevent_iter_next(void *iterator)
+{
+    struct yaml_fsevent_iterator *fsevents = iterator;
+    yaml_event_type_t type;
+    yaml_event_t event;
+
+    if (fsevents->exhausted) {
+        errno = ENODATA;
+        return NULL;
+    }
+
+    if (!yaml_parser_parse(&fsevents->parser, &event))
+        parser_error(&fsevents->parser);
+
+    type = event.type;
+    yaml_event_delete(&event);
+
+    switch (type) {
+    case YAML_DOCUMENT_START_EVENT:
+        /* Remove any trace of the previous parsed fsevent */
+        memset(&fsevents->fsevent, 0, sizeof(fsevents->fsevent));
+
+        /* Parsing of the Hestia event will be here */
+
+        if (!yaml_parser_parse(&fsevents->parser, &event))
+            parser_error(&fsevents->parser);
+
+        assert(event.type == YAML_DOCUMENT_END_EVENT);
+        yaml_event_delete(&event);
+        return &fsevents->fsevent;
+    case YAML_STREAM_END_EVENT:
+        fsevents->exhausted = true;
+        errno = ENODATA;
+        return NULL;
+    default:
+        error(EXIT_FAILURE, 0, "unexpected YAML event: type = %i", event.type);
+        __builtin_unreachable();
+    }
+}
+
+static void
+hestia_fsevent_iter_destroy(void *iterator)
+{
+    struct yaml_fsevent_iterator *fsevents = iterator;
+
+    yaml_parser_delete(&fsevents->parser);
+}
+
+static const struct rbh_iterator_operations YAML_FSEVENT_ITER_OPS = {
+    .next = hestia_fsevent_iter_next,
+    .destroy = hestia_fsevent_iter_destroy,
+};
+
+static const struct rbh_iterator HESTIA_FSEVENT_ITERATOR = {
+    .ops = &HESTIA_FSEVENT_ITER_OPS,
+};
+
+const struct rbh_iterator_operations YAML_SOURCE_ITER_OPS = {
+    .next = yaml_source_iter_next,
+    .destroy = yaml_source_iter_destroy,
+};
+
+static const struct source FILE_SOURCE = {
+    .name = "hestia",
+    .fsevents = {
+        .ops = &YAML_SOURCE_ITER_OPS,
+    },
+};
+
+struct source *
+source_from_hestia_file(FILE *file)
+{
+    struct file_source *source;
+
+    source = malloc(sizeof(*source));
+    if (source == NULL)
+        error(EXIT_FAILURE, 0, "malloc");
+
+    yaml_fsevent_init(&source->fsevents, file, HESTIA_FSEVENT_ITERATOR);
+
+    source->source = FILE_SOURCE;
+    source->file = file;
+    return &source->source;
+}
