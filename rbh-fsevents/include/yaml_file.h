05fe502c6494c8311e5fd12b582ab8538764fa49
Yoann Valeri <yoann.valeri@cea.fr>
2023-10-06 11:17:46 +0200

    file: handle yaml files more generically
    This patch modifies the "file" source file to divide it into 2, one
    specific to YAML files following the expected RobinHood fsevents
    nomenclature (the new "file" source), and the other provides more
    general functions and structures to read YAML files.
    
    This is to prepare the Hestia backend, as the provided events will
    be given in a YAML file, but not following standard fsevents
    nomenclature.
    
    Change-Id: I920a5c3d2d80e3f08ec7bb05b6659f39d4ef3096
    Signed-off-by: Yoann Valeri <yoann.valeri@cea.fr>

---
+++ rbh-fsevents/include/yaml_file.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifer: LGPL-3.0-or-later */
+
+#ifndef YAML_FILE_H
+#define YAML_FILE_H
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <errno.h>
+#include <error.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <miniyaml.h>
+#include <robinhood/fsevent.h>
+
+#include "source.h"
+
+struct yaml_fsevent_iterator {
+    struct rbh_iterator iterator;
+
+    struct rbh_fsevent fsevent;
+    yaml_parser_t parser;
+    bool exhausted;
+};
+
+struct source *
+yaml_fsevent_init(FILE *file, const struct rbh_iterator iterator,
+                  const struct source *source);
+
+const void *yaml_source_iter_next(void *iterator);
+
+void yaml_source_iter_destroy(void *iterator);
+
+#endif
