cf453ebddf55b516dc2c2e96835ef0378afcdfb8
VALERI Yoann <yoann.valeri@cea.fr>
2023-10-20 15:14:26 +0200

    hestia: ensure proper memory management
    This patch adds a way to use the source sstack in utils by the sources
    to ensure the memory is managed and freed correctly.
    
    Change-Id: I47086e9470b9595d645497b79f38a6656dd749a1
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1171055
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- rbh-fsevents/include/utils.h
+++ rbh-fsevents/include/utils.h
@@ -12,15 +12,8 @@
 
 #include <robinhood/fsevent.h>
 
-/*
- * This sstack is shared amongst the enrichers for easier use. Since some
- * enrichers use similar functions, they are defined here, and since they
- * require an sstack, we define one as a global variable.
- */
-extern __thread struct rbh_sstack *global_values;
-
 void
-flush_global_values();
+flush_source_stack();
 
 struct rbh_value_pair *
 build_pair(const char *key, struct rbh_value *(*part_builder)(void *),
@@ -33,5 +26,8 @@ struct rbh_value_map
 build_enrich_map(struct rbh_value *(*part_builder)(void *),
                  void *part_builder_arg);
 
+void *
+source_stack_alloc(const void *data, size_t size);
+
 void
-initialize_global_values(size_t stack_size);
+initialize_source_stack(size_t stack_size);
