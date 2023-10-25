db5747cfcec0411b2a93796e8bad6556546c8780
VALERI Yoann <yoann.valeri@cea.fr>
2023-10-25 09:02:00 +0200

    hestia: add test for CREATE
    Change-Id: Ib10826a52c251f03b00245890a41efe345fb6627
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-fsevents/tests/hestia/hestia_utils.bash
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# This file is part of rbh-fsevents.
+# Copyright (C) 2023 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../test_utils.bash
+
+################################################################################
+#                                DATABASE UTILS                                #
+################################################################################
+
+invoke_rbh-fsevents()
+{
+    rbh_fsevents src:hestia:$HOME/.cache/hestia/event_feed.yaml -
+}
+
+number_of_events()
+{
+    local n=$(echo "$1" | xargs | awk -F "---" "{ printf NF }")
+    echo "$((n - 1))"
+}
+
+fill_events_array()
+{
+    local entry="$1"
+    local event="$2"
+    local arr
+
+    events=()
+
+    entry=$(echo "$entry" | sed "s/---/§/")
+    IFS='§' read -d '' -a arr <<< "$entry" || true
+
+    local n=${#arr[@]}
+
+    for i in $(seq 0 $((n - 1))); do
+        if echo "${arr[$i]}" | grep "$event" > /dev/null; then
+            events+=("${arr[$i]}")
+        fi
+    done
+}
+
+hestia_setup()
+{
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/hestia
+    declare -a events
+}
+
+hestia_teardown()
+{
+    rm -rf $HOME/.cache/hestia/
+}
---
+++ rbh-fsevents/tests/hestia/test_create.bash
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+# This file is part of rbh-fsevents
+# Copyright (C) 2023 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../test_utils.bash
+. $test_dir/hestia_utils.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_create()
+{
+    hestia object create blob
+
+    local output=$(invoke_rbh-fsevents)
+
+    if [[ $(number_of_events "$output") != 4 ]]; then
+        error "There should be 4 fsevents created (one for link," \
+              "inode_xattrs, upsert of statx and upsert of enrichment tag)," \
+              "found '$(number_of_events "$output")'"
+    fi
+
+    if ! echo "$output" | xargs | cut -d'.' -f1 | grep "blob" | \
+         grep "link"; then
+        error "There should be a link event on the object 'blob' in '$output'"
+    fi
+
+    fill_events_array "$output" "link"
+
+    if [[ ${#events[@]} != 1 ]]; then
+        error "There should be a single 'link' event in '$output'"
+    fi
+
+    if ! echo "${events[0]}" | grep "blob"; then
+        error "The 'link' event should be on the object 'blob'"
+    fi
+
+    fill_events_array "$output" "inode_xattr"
+
+    if [[ ${#events[@]} != 1 ]]; then
+        error "There should be a single 'inode_xattr' event in '$output'"
+    fi
+
+    if ! echo "${events[0]}" | grep "tiers" | grep "\[\]"; then
+        error "The 'inode_xattr' event should have 'tiers' as an empty array"
+    fi
+
+    if ! echo "${events[0]}" | grep "size" | grep "0"; then
+        error "The 'inode_xattr' event should have 'size' set to '0'"
+    fi
+
+    # We check the changelog time since the enrichment of the times isn't ready
+    # yet
+    local changelog_time=$(cat ~/.cache/hestia/event_feed.yaml | grep "time" |
+                           cut -d' ' -f2)
+
+    fill_events_array "$output" "upsert"
+
+    if [[ ${#events[@]} != 2 ]]; then
+        error "There should be two events 'upsert' in '$output'"
+    fi
+
+    if ! echo "${events[0]}" | grep "$changelog_time"; then
+        error "One 'upsert' event should set timestamp '$changelog_time'"
+    fi
+
+    if ! echo "$output" | grep "$changelog_time"; then
+        error "The timestamp '$changelog_time' should appear in '$output'"
+    fi
+
+    local n=$(echo "${events[0]}" | grep "$changelog_time" | wc -l)
+
+    if [[ $n != 4 ]]; then
+        error "'$changelog_time' should be set for the 4 times, but seen '$n'"
+    fi
+
+    if ! echo "${events[1]}" | grep "rbh-fsevents"; then
+        error "One 'upsert' event should set the enrichment tag"
+    fi
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_create)
+
+run_tests hestia_setup hestia_teardown ${tests[@]}
