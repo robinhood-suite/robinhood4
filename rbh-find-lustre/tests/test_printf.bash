fcbe553705e56b0a58cd2cfb0355e9fd79765496
VALERI Yoann <yoann.valeri@cea.fr>
2024-02-13 16:46:29 +0100

    lfind: add '%I' directive
    This patch adds the '%I' directive to print the rbh_id. This will be
    useful for the retention script as we plan of creating a special event
    which will be given to rbh-fsevents. For this we need the ID of the
    entry to modify, which we don't have without this patch and without
    doing a mongo request.
    
    Change-Id: I4f32f3fe44a4eb2a360c00459aaa4f54f8a3ffaa
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-find-lustre/tests/test_printf.bash
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# This file is part of the RobinHood Library
+# Copyright (C) 2023 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+set -xe
+
+if ! command -v rbh-sync &> /dev/null; then
+    echo "This test requires rbh-sync to be installed" >&2
+    exit 1
+fi
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/test_utils.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_id()
+{
+    touch fileA
+    touch fileB
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    local IDA=$(rbh_lfind "rbh:mongo:$testdb" -name fileA -printf "%I\n")
+    local IDB=$(rbh_lfind "rbh:mongo:$testdb" -name fileB -printf "%I\n")
+
+    local countA="$(mongo --quiet "$testdb" --eval \
+                    "db.entries.find({\"_id\": BinData(0, \"$IDA\")}).count()")"
+    local countB="$(mongo --quiet "$testdb" --eval \
+                    "db.entries.find({\"_id\": BinData(0, \"$IDB\")}).count()")"
+
+    if [[ "$countA" != "1" ]]; then
+        error "Couldn't find entry with ID '$IDA'"
+    fi
+
+    if [[ "$countB" != "1" ]]; then
+        error "Couldn't find entry with ID '$IDB'"
+    fi
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_id)
+
+LUSTRE_DIR=/mnt/lustre/
+cd "$LUSTRE_DIR"
+
+tmpdir=$(mktemp --directory --tmpdir=$LUSTRE_DIR)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
