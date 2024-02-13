a388e1887b0e812fea86a9e156c3ae8812cc0d70
VALERI Yoann <yoann.valeri@cea.fr>
2024-02-13 10:15:30 +0100

    retention: keep the user xattrs in the database
    This patch changes the behaviour of the retention mechanism to keep in
    the database:
     - the expiration date
     - the expiration attribute set by the user, as a string
    
    Before this patch, we would fetch the expiration attribute, and then
    change it to the expiration date, so we wouldn't keep the attribute the
    user set.
    
    Change-Id: I8a88229333564b749b7b9a50f0345d107369da1f
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1176821
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- librobinhood/src/backends/lustre/lustre.c
+++ librobinhood/src/backends/lustre/lustre.c
@@ -1014,10 +1014,11 @@ create_expiration_date_value_pair(const char *attribute_value,
     return 0;
 }
 
-static void
-xattrs_get_retention(const struct rbh_statx *statx)
+static int
+xattrs_get_retention(const struct rbh_statx *statx,
+                     struct rbh_value_pair *pairs)
 {
-    struct rbh_value_pair new_pair;
+    struct rbh_value *new_value;
 
     for (int i = 0; i < *_inode_xattrs_count; ++i) {
         char tmp[INT64_MAX_STR_LEN];
@@ -1038,11 +1039,22 @@ xattrs_get_retention(const struct rbh_statx *statx)
                _inode_xattrs[i].value->binary.size);
         tmp[_inode_xattrs[i].value->binary.size] = 0;
 
-        create_expiration_date_value_pair(tmp, statx, &new_pair);
+        if (create_expiration_date_value_pair(tmp, statx, &pairs[0]))
+            return 0;
 
-        _inode_xattrs[i] = new_pair;
-        break;
+        new_value = rbh_sstack_push(_values, NULL, sizeof(*new_value));
+        if (new_value == NULL)
+            return 0;
+
+        new_value->type = RBH_VT_STRING;
+        new_value->string = rbh_sstack_push(_values, tmp, strlen(tmp) + 1);
+
+        _inode_xattrs[i].value = new_value;
+
+        return 1;
     }
+
+    return 0;
 }
 
 static int
@@ -1071,7 +1083,9 @@ _get_attrs(struct entry_info *entry_info,
         count += subcount;
     }
 
-    xattrs_get_retention(statx);
+    subcount = xattrs_get_retention(statx, &pairs[count]);
+
+    count += subcount;
 
     return count;
 }
