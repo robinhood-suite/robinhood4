81d87a68e9bef970700220431d9782f4bfa71ce8
VALERI Yoann <yoann.valeri@cea.fr>
2024-02-15 15:29:13 +0100

    sync: change "mode" to "statx" in namespace xattrs callback
    This patch changes the callback for retrieving namespace xattrs from the
    mode of the entry to check to the statx of the entry.
    
    Change-Id: Ie29019cb751ea14ed090b2ee04a1e1641db014d2
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1176940
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- librobinhood/src/backends/posix/posix.c
+++ librobinhood/src/backends/posix/posix.c
@@ -325,7 +325,8 @@ free_ns_data(void)
 
 static struct rbh_fsentry *
 fsentry_from_ftsent(FTSENT *ftsent, int statx_sync_type, size_t prefix_len,
-                    int (*ns_xattrs_callback)(const int, const uint16_t,
+                    int (*ns_xattrs_callback)(const int,
+                                              const struct rbh_statx *,
                                               struct rbh_value_pair *,
                                               ssize_t *,
                                               struct rbh_value_pair *,
@@ -479,7 +480,7 @@ fsentry_from_ftsent(FTSENT *ftsent, int statx_sync_type, size_t prefix_len,
     ns_xattrs.count = 1;
 
     if (ns_xattrs_callback != NULL) {
-        ns_count = ns_xattrs_callback(fd, statxbuf.stx_mode, pairs, &count,
+        ns_count = ns_xattrs_callback(fd, &statxbuf, pairs, &count,
                                       &ns_pairs[ns_xattrs.count], ns_values);
         if (ns_count == -1) {
             if (errno != ENOMEM) {
