941993c73ed45fd575f2ec9e65256e6151cf8f1c
VALERI Yoann <yoann.valeri@cea.fr>
2024-02-19 14:19:48 +0100

    retention: update expiration date when using rbh-fsevents
    This patch adds the management of the retention when using the
    rbh-fsevents command. This is done by retrieving the xattr on the entry,
    calculating the date, and then storing it in the fsevent.
    
    This will ensure the expiration date is kept up to date with what
    happens on the system and the different modifications to the files,
    without requiring a new complete synchronization.
    
    Change-Id: Iad5a8b1413451e2c6f93a5cde9e5948de2869e06
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-fsevents/tests/lustre/test_retention.bash
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2023 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../test_utils.bash
+. $test_dir/lustre_utils.bash
+
+__rbh_lfind=$(PATH="$PWD:$PATH" which rbh-lfind)
+rbh_lfind()
+{
+    "$__rbh_lfind" "$@"
+}
+
+difflines()
+{
+    diff -y - <([ $# -eq 0 ] && printf '' || printf '%s\n' "$@")
+}
+
+find_attribute()
+{
+    old_IFS=$IFS
+    IFS=','
+    local output="$*"
+    IFS=$old_IFS
+    local res=$(mongo $testdb --eval "db.entries.count({$output})")
+    [[ "$res" == "1" ]] && return 0 ||
+        error "Expected 1 entry with filter '{ $output }', got '$res'"
+}
+
+retention_teardown()
+{
+    # Since the test changes the system's date and time, it must be reset at the
+    # end of it, which is what hwclock does
+    hwclock --hctosys
+    lustre_teardown
+}
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_retention()
+{
+    local entry="test_entry"
+
+    touch $entry
+
+    local path_without_mount="$(realpath $entry)"
+    path_without_mount="${path_without_mount#"$LUSTRE_DIR"}"
+
+    setfattr -n user.ccc_expires -v +5 $entry
+
+    invoke_rbh-fsevents
+
+    clear_changelogs "$LUSTRE_MDT" "$userid"
+
+    verify_lustre "$entry"
+
+    local exp_time="$(( $(stat -c %X $entry) + 5))"
+    find_attribute '"xattrs.user.ccc_expiration_date": NumberLong('$exp_time')'\
+                   '"ns.name": "'$entry'"'
+
+    rbh_lfind "rbh:mongo:$testdb" -expired | sort | difflines
+
+    date --set="@$(( $(stat -c %X $entry) + 6))"
+
+    rbh_lfind "rbh:mongo:$testdb" -expired | sort |
+        difflines "/$path_without_mount"
+
+    truncate -s 300 $entry
+
+    invoke_rbh-fsevents
+
+    verify_lustre "$entry"
+
+    exp_time="$(( $(stat -c %Y $entry) + 5))"
+    find_attribute '"xattrs.user.ccc_expiration_date": NumberLong('$exp_time')'\
+                   '"ns.name": "'$entry'"'
+
+    rbh_lfind "rbh:mongo:$testdb" -expired | sort | difflines
+
+    date --set="@$(( $(stat -c %Y $entry) + 6))"
+
+    rbh_lfind "rbh:mongo:$testdb" -expired | sort |
+        difflines "/$path_without_mount"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_retention)
+
+LUSTRE_DIR=/mnt/lustre/
+cd "$LUSTRE_DIR"
+
+LUSTRE_MDT=lustre-MDT0000
+userid="$(start_changelogs "$LUSTRE_MDT")"
+
+tmpdir=$(mktemp --directory --tmpdir=$LUSTRE_DIR)
+lfs setdirstripe -D -i 0 $tmpdir
+trap -- "rm -rf '$tmpdir'; stop_changelogs '$LUSTRE_MDT' '$userid'" EXIT
+cd "$tmpdir"
+
+run_tests lustre_setup retention_teardown "${tests[@]}"
