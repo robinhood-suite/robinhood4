1584e8a480a5bb1cf45ce29b862df8bbf8266dec
Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
2024-04-11 15:32:29 +0200

    lustre-mpi: -o with rbh-sync iterate only the root before syncing
    The lustre-mpi backend uses now fts_read with rbh-sync when the option
    --one is set. It avoid traversing the entire tree like mpifileutils
    does.
    
    Change-Id: Ia195fb5e9b6e7a6eca478650ee51a860e8adea9c
    Signed-off-by: Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>

--- librobinhood/include/robinhood/backends/lustre_internal.h
+++ librobinhood/include/robinhood/backends/lustre_internal.h
@@ -15,6 +15,9 @@
  |                             lustre_operations                              |
  *----------------------------------------------------------------------------*/
 
+struct rbh_mut_iterator *
+lustre_iterator_new(const char *root, const char *entry, int statx_sync_type);
+
 int
 lustre_inode_xattrs_callback(const int fd, const struct rbh_statx *statx,
                              struct rbh_value_pair *inode_xattrs,
