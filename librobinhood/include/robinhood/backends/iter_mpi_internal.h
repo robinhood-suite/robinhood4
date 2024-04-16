f55cb825a75a075fb9e251ada568bb6df1cc4a57
Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
2024-04-16 14:25:25 +0200

    mpi-iter: move the mpi iterator/backend functions
    This patch moves the mpi iterator/backend functions originally placed
    in the lustre-mpi backend in a folder containing all the functions
    useful for mpi. As is was before, we had to have Lustre installed and
    the lustre-mpi backend to have the posix-mpi backend. Also, this adds
    better generic management of the mpi backends.
    
    Change-Id: I18ed9353e2344a54f9d210aaa238ad194215d9eb
    Signed-off-by: Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>

--- librobinhood/include/robinhood/backends/iter_mpi_internal.h
+++ librobinhood/include/robinhood/backends/iter_mpi_internal.h
@@ -6,8 +6,8 @@
  * SPDX-License-Identifer: LGPL-3.0-or-later
  */
 
-#ifndef RBH_LUSTRE_MPI_H
-#define RBH_LUSTRE_MPI_H
+#ifndef RBH_ITER_MPI_H
+#define RBH_ITER_MPI_H
 
 /**
  * @file
@@ -17,11 +17,9 @@
  */
 
 #include "mfu.h"
-#include "robinhood/iterator.h"
+
+#include "robinhood/backend.h"
 #include "robinhood/sstack.h"
-#include "robinhood/value.h"
-#include "robinhood/id.h"
-#include "robinhood/statx.h"
 
 #include "common.h"
 
@@ -60,6 +58,16 @@ struct mpi_iterator {
 };
 
 /*----------------------------------------------------------------------------*
+ |                       mpi_iterator operations                              |
+ *----------------------------------------------------------------------------*/
+
+struct rbh_mut_iterator *
+mpi_iterator_new(const char *root, const char *entry, int statx_sync_type);
+
+void *
+mpi_iter_next(void *iterator);
+
+/*----------------------------------------------------------------------------*
  |                               mpi_file_info                                |
  *----------------------------------------------------------------------------*/
 
@@ -81,11 +89,21 @@ struct mpi_file_info {
 };
 
 /*----------------------------------------------------------------------------*
- |                       lustre_mpi operations                                |
+ |                       mpi_backend operations                               |
  *----------------------------------------------------------------------------*/
 
-struct rbh_backend *
-lustre_mpi_backend_branch(void *backend, const struct rbh_id *id,
-                          const char *path);
+struct rbh_mut_iterator *
+mpi_backend_filter(void *backend, const struct rbh_filter *filter,
+                   const struct rbh_filter_options *options);
+
+struct rbh_mut_iterator *
+mpi_branch_backend_filter(void *backend, const struct rbh_filter *filter,
+                          const struct rbh_filter_options *options);
+
+/**
+ * Release the memory associated with the plugin
+ */
+void
+rbh_mpi_plugin_destroy();
 
 #endif
