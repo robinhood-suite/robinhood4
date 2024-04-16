cceccf73feff4afac06fe33fc5e58ca66abe73ef
Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
2024-04-16 14:50:55 +0200

    posix-mpi: add branches to posix-mpi backend
    This patch adds the branch to the posix-mpi backend. It is done the
    same way as for the lustre-mpi backend. We use the
    "posix_branch_backend" structure but by overloading it.
    
    Change-Id: I0540d2fcae3dca8e40b8d3b025c16cac5318ef0b
    Signed-off-by: Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1193356
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>
    Tested-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/backends/posix_mpi/posix_mpi.c
+++ librobinhood/src/backends/posix_mpi/posix_mpi.c
@@ -101,6 +101,7 @@ posix_mpi_backend_root(void *backend,
     static const struct rbh_backend_operations POSIX_MPI_ROOT_BACKEND_OPS = {
         .get_option = posix_mpi_backend_get_option,
         .set_option = posix_mpi_backend_set_option,
+        .branch = posix_mpi_backend_branch,
         .root = posix_mpi_backend_root,
         .filter = posix_backend_filter,
         .destroy = posix_mpi_backend_destroy,
@@ -114,12 +115,55 @@ posix_mpi_backend_root(void *backend,
 }
 
     /*--------------------------------------------------------------------*
+     |                          branch()                                  |
+     *--------------------------------------------------------------------*/
+
+static struct rbh_mut_iterator *
+posix_mpi_branch_backend_filter(void *backend, const struct rbh_filter *filter,
+                                const struct rbh_filter_options *options)
+{
+    return mpi_branch_backend_filter(backend, filter, options);
+}
+
+static const struct rbh_backend_operations POSIX_MPI_BRANCH_BACKEND_OPS = {
+    .root = posix_mpi_backend_root,
+    .branch = posix_mpi_backend_branch,
+    .filter = posix_mpi_branch_backend_filter,
+    .destroy = posix_mpi_backend_destroy,
+};
+
+static const struct rbh_backend POSIX_MPI_BRANCH_BACKEND = {
+    .name = RBH_POSIX_MPI_BACKEND_NAME,
+    .ops = &POSIX_MPI_BRANCH_BACKEND_OPS,
+};
+
+struct rbh_backend *
+posix_mpi_backend_branch(void *backend, const struct rbh_id *id,
+                         const char *path)
+{
+    struct posix_backend *posix_mpi = backend;
+    struct posix_branch_backend *branch;
+
+    branch = (struct posix_branch_backend *)
+              posix_backend_branch(backend, id, path);
+    if (branch == NULL)
+        return NULL;
+
+    branch->posix.statx_sync_type = posix_mpi->statx_sync_type;
+    branch->posix.backend = POSIX_MPI_BRANCH_BACKEND;
+    branch->posix.iter_new = posix_mpi_iterator_new;
+
+    return &branch->posix.backend;
+}
+
+    /*--------------------------------------------------------------------*
      |                          backend()                                 |
      *--------------------------------------------------------------------*/
 
 static const struct rbh_backend_operations POSIX_MPI_BACKEND_OPS = {
     .get_option = posix_mpi_backend_get_option,
     .set_option = posix_mpi_backend_set_option,
+    .branch = posix_mpi_backend_branch,
     .root = posix_mpi_backend_root,
     .filter = posix_mpi_backend_filter,
     .destroy = posix_mpi_backend_destroy,
