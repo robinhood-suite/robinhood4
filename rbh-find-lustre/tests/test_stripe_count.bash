77ba910cf1cfae407c8e557aace85bdf1aa03022
VALERI Yoann <yoann.valeri@cea.fr>
2024-04-17 12:04:31 +0200

    stripe-count: add stripe-count tests
    This patch creates the stripe-count test. For now, it is expected to
    fail, but the next patches will make it pass.
    
    Change-Id: I5b0a2952191d3290d3683defa77bf2c54657e45c
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://gerrithub.io/c/robinhood-suite/robinhood4/+/1193310
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Gauthier Evraerd <gauthier.evraerd.ocre@cea.fr>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>

---
+++ rbh-find-lustre/tests/test_stripe_count.bash
@@ -0,0 +1,156 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+if ! command -v rbh-sync &> /dev/null; then
+    echo "This test requires rbh-sync to be installed" >&2
+    exit 1
+fi
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/test_utils.bash
+
+LUSTRE_DIR=/mnt/lustre/
+cd "$LUSTRE_DIR"
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_invalid()
+{
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count $(echo 2^64 | bc) &&
+        error "find with an ost index too big should have failed"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count 42blob &&
+        error "find with an invalid ost index should have failed"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count invalid &&
+        error "find with an invalid ost index should have failed"
+
+    return 0
+}
+
+test_default()
+{
+    local dir=test_default_dir
+
+    local default_stripe_count="$(lfs getstripe -c $LUSTRE_DIR | xargs)"
+    mkdir $dir
+
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count default | sort |
+        difflines "/" "/$dir"
+
+    lfs setstripe -c $((default_stripe_count + 1)) $dir
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count default | sort |
+        difflines "/"
+
+    lfs setstripe -c $((default_stripe_count)) $dir
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count default | sort |
+        difflines "/"
+}
+
+test_stripe_count()
+{
+    local dir=test_stripe_count_dir
+    local file=test_stripe_count
+
+    lfs mkdir $dir
+
+    lfs setstripe -c 1 $dir/$file
+    lfs setstripe -c 2 $dir
+    lfs setstripe -c 3 .
+
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count 1 | sort |
+        difflines "/$dir/$file"
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count 2 | sort |
+        difflines "/$dir"
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count 3 | sort |
+        difflines "/"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count -4 | sort |
+        difflines "/" "/$dir" "/$dir/$file"
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count -3 | sort |
+        difflines "/$dir" "/$dir/$file"
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count -2 | sort |
+        difflines "/$dir/$file"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count +0 | sort |
+        difflines "/" "/$dir" "/$dir/$file"
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count +1 | sort |
+        difflines "/" "/$dir"
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count +2 | sort |
+        difflines "/"
+
+    lfs migrate -c 3 $dir/$file
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count 3 | sort |
+        difflines "/" "/$dir/$file"
+
+    lfs setstripe -c 3 $dir
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count 3 | sort |
+        difflines "/" "/$dir" "/$dir/$file"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count +2 | sort |
+        difflines "/" "/$dir" "/$dir/$file"
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count -4 | sort |
+        difflines "/" "/$dir" "/$dir/$file"
+}
+
+test_default_stripe_count()
+{
+    local dir=test_default_stripe_count_dir
+    local file=test_default_stripe_count
+
+    lfs mkdir $dir
+    lfs setstripe -c 4 $file
+
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" \
+        -stripe-count $(lfs getstripe -c $LUSTRE_DIR | xargs) | sort |
+        difflines
+
+    lfs setstripe -c 3 $LUSTRE_DIR
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count 3 | sort |
+        difflines "/" "/$dir"
+
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count -3 | sort |
+        difflines
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count -5 | sort |
+        difflines "/" "/$file" "/$dir"
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count +3 | sort |
+        difflines "/$file"
+    rbh_lfind "rbh:mongo:$testdb" -stripe-count +2 | sort |
+        difflines "/" "/$file" "/$dir"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_invalid test_default test_stripe_count
+                  test_default_stripe_count)
+
+tmpdir=$(mktemp --directory --tmpdir=$LUSTRE_DIR)
+trap "rm -r $tmpdir; lfs setstripe -c 0 $LUSTRE_DIR" EXIT
+
+cd "$tmpdir"
+
+run_tests "${tests[@]}"
