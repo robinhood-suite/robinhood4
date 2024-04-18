e6822031f13abd0f08fb8127eb9171b832abccbf
VALERI Yoann <yoann.valeri@cea.fr>
2024-04-18 10:38:46 +0200

    lfind: allow `stripe-count [+|-]n` to match default values
    This patch modifies the stripe count filter in the case of greater or
    lower matching. In that case, we match the entries which have a default
    stripe count if the stripe count of the mount is:
      - lower than the value given by the user, in the case of a filter on
      `-n`
      - larger than the value given by the user, in the case of a filter on
      `+n`
    
    Also change the stripe count test to now be expected to succeed.
    
    Change-Id: I23413c3d50b13a5501ce0d5fac27273515b00612
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-find-lustre/src/filters.c
+++ rbh-find-lustre/src/filters.c
@@ -292,9 +292,22 @@ stripe_count2filter(const char *stripe_count)
         error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
                       "stripe_count2filter");
 
-    if (filter->op != RBH_FOP_EQUAL ||
-        filter->compare.value.uint64 != pair.value->uint64)
-        return filter_and(filter, filter_not(default_filter));
+    switch (filter->op) {
+    case RBH_FOP_STRICTLY_LOWER:
+        if (pair.value->uint64 < filter->compare.value.uint64)
+            return filter_or(filter, default_filter);
+        break;
+    case RBH_FOP_STRICTLY_GREATER:
+        if (pair.value->uint64 > filter->compare.value.uint64)
+            return filter_or(filter, default_filter);
+        break;
+    case RBH_FOP_EQUAL:
+        if (pair.value->uint64 == filter->compare.value.uint64)
+            return filter_or(filter, default_filter);
+        break;
+    default:
+        __builtin_unreachable();
+    }
 
-    return filter_or(filter, default_filter);
+    return filter_and(filter, filter_not(default_filter));
 }
