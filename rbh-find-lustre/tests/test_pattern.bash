2994eed9ccb4f78537e9d5b5b1ce687524efde18
VALERI Yoann <yoann.valeri@cea.fr>
2024-04-22 16:44:35 +0200

    lfind: add pattern tests
    This patch creates the pattern tests. For now, it is expected to
    fail, but the next patches will make it pass.
    
    Change-Id: I8c219d712402bc741ed922360765e47dcd11ab58
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-find-lustre/tests/test_pattern.bash
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+if ! command -v rbh-sync &> /dev/null; then
+    echo "This test requires rbh-sync to be installed" >&2
+    exit 1
+fi
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/test_utils.bash
+
+LUSTRE_DIR=/mnt/lustre/
+cd "$LUSTRE_DIR"
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_invalid()
+{
+    rbh_lfind "rbh:mongo:$testdb" -pattern 42 &&
+        error "find with an invalid pattern should have failed"
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern invalid &&
+        error "find with an invalid pattern should have failed"
+
+    return 0
+}
+
+test_default()
+{
+    local dir=test_default_pattern
+
+    mkdir $dir
+
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern default | sort |
+        difflines "/" "/$dir"
+
+    lfs setstripe -L mdt -E 1M $dir
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern default | sort |
+        difflines "/"
+
+    lfs setstripe -L raid0 $dir
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern default | sort |
+        difflines "/"
+}
+
+test_pattern()
+{
+    local dir=test_pattern_dir
+    local file=test_pattern
+
+    mkdir $dir
+
+    lfs setstripe -L raid0 -c 1 $dir/$file
+    lfs setstripe -L raid0 -c 1 .
+    lfs setstripe -L mdt -E 1M $dir
+
+    lfs getstripe -v .
+
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern raid0 | sort |
+        difflines "/" "/$dir/$file"
+    rbh_lfind "rbh:mongo:$testdb" -pattern mdt | sort |
+        difflines "/$dir"
+
+    lfs migrate -L mdt -E 1M $dir/$file
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern raid0 | sort |
+        difflines "/"
+    rbh_lfind "rbh:mongo:$testdb" -pattern mdt | sort |
+        difflines "/$dir" "/$dir/$file"
+
+    lfs setstripe -L mdt -E 1M .
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern raid0 | sort |
+        difflines
+    rbh_lfind "rbh:mongo:$testdb" -pattern mdt | sort |
+        difflines "/" "/$dir" "/$dir/$file"
+}
+
+test_default_pattern()
+{
+    local dir=test_default_pattern_dir
+    local file=test_default_pattern
+
+    mkdir $dir
+    lfs setstripe -L raid0 -c 1 $file
+
+    rbh-sync "rbh:lustre:." "rbh:mongo:$testdb"
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern mdt | sort |
+        difflines
+
+    lfs setstripe -L mdt -E 1M $LUSTRE_DIR
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern mdt | sort |
+        difflines "/" "/$dir"
+    rbh_lfind "rbh:mongo:$testdb" -pattern raid0 | sort |
+        difflines "/$file"
+
+    lfs setstripe -L raid0 -c 1 $LUSTRE_DIR
+
+    rbh_lfind "rbh:mongo:$testdb" -pattern mdt | sort |
+        difflines
+    rbh_lfind "rbh:mongo:$testdb" -pattern raid0 | sort |
+        difflines "/" "/$file" "/$dir"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_invalid test_default test_pattern
+                  test_default_pattern)
+
+tmpdir=$(mktemp --directory --tmpdir=$LUSTRE_DIR)
+trap "rm -r $tmpdir; lfs setstripe -L raid0 $LUSTRE_DIR" EXIT
+
+cd "$tmpdir"
+
+run_tests "${tests[@]}"
