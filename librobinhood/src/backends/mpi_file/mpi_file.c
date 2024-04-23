ae5aadd006853503595ef196fb1c0240a4dfb7f9
Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
2024-04-23 10:07:15 +0200

    mpi-file : add the mpi-file backend
    This patch adds the new mpi-file backend. This backend will be used to
    create a mpifileutils file with all the information recover while
    syncing another backend (update).
    
    Moreover, this backend will be able to to synchronize a mpifileutils
    file with another backend (filter).
    
    All operations described will be added in future patches.
    
    Finally, to use it you have to give the path where is the mpifileutils
    file or where to create it in the RBH URI: rbh:mpi-file:path.
    
    Change-Id: I49e30cc208df4ad77d6a0c023f8c8d3846dc5895
    Signed-off-by: Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>

---
+++ librobinhood/src/backends/mpi_file/mpi_file.c
@@ -0,0 +1,106 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <error.h>
+#include <errno.h>
+#include <string.h>
+
+#include "mfu.h"
+
+#include "robinhood/backends/mpi_file.h"
+
+/*----------------------------------------------------------------------------*
+ |                          mpi_file_backend                                  |
+ *----------------------------------------------------------------------------*/
+
+struct mpi_file_backend {
+    struct rbh_backend backend;
+    /*
+     * Path of the mpi-file
+     */
+    const char *path;
+    mfu_flist flist;
+};
+
+    /*--------------------------------------------------------------------*
+     |                          destroy()                                 |
+     *--------------------------------------------------------------------*/
+
+static void
+mpi_file_backend_destroy(void *backend)
+{
+    struct mpi_file_backend *mpi_file = backend;
+
+    mfu_flist_free(&mpi_file->flist);
+    free((char *)mpi_file->path);
+    free(mpi_file);
+}
+
+    /*--------------------------------------------------------------------*
+     |                          backend()                                 |
+     *--------------------------------------------------------------------*/
+
+static const struct rbh_backend_operations MPI_FILE_BACKEND_OPS = {
+    .destroy = mpi_file_backend_destroy,
+};
+
+static const struct rbh_backend MPI_FILE_BACKEND = {
+    .id = RBH_BI_MPI_FILE,
+    .name = RBH_MPI_FILE_BACKEND_NAME,
+    .ops = &MPI_FILE_BACKEND_OPS,
+};
+
+static void
+mpi_file_backend_init(struct mpi_file_backend *mpi_file)
+{
+    int flag;
+
+    MPI_Initialized(&flag);
+    if (!flag) {
+        MPI_Init(NULL, NULL);
+        mfu_init();
+    }
+
+    mpi_file->flist = mfu_flist_new();
+    if (mpi_file->flist == NULL)
+        error(EXIT_FAILURE, errno, "malloc flist");
+}
+
+struct rbh_backend *
+rbh_mpi_file_backend_new(const char *path)
+{
+    struct mpi_file_backend *mpi_file;
+    int save_errno;
+
+    mpi_file = malloc(sizeof(*mpi_file));
+    if (mpi_file == NULL)
+        error(EXIT_FAILURE, errno, "malloc mpi_file_backend struct");
+
+    if (*path == '\0') {
+        save_errno = errno;
+        free(mpi_file);
+        errno = save_errno;
+        return NULL;
+    }
+
+    mpi_file->path = strdup(path);
+    if (mpi_file->path == NULL) {
+        save_errno = errno;
+        free(mpi_file);
+        errno = save_errno;
+        return NULL;
+    }
+
+    mpi_file_backend_init(mpi_file);
+    mpi_file->backend = MPI_FILE_BACKEND;
+
+    return &mpi_file->backend;
+}
