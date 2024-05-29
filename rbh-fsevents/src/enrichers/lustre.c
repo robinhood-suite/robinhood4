ea3deeb8b67a5e53fe114270a584fd43c21aeb71
VALERI Yoann <yoann.valeri@cea.fr>
2024-05-29 14:14:05 +0200

    enrich: skip entry on error
    This patch adds the same behaviour when enriching entries in
    rbh-fsevents as when using rbh-sync: if enriching an entry fails, we
    skip it and continue the command.
    
    Change-Id: I95106c8b5a6683549733fb03e524604fca72fda6
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-fsevents/src/enrichers/lustre.c
+++ rbh-fsevents/src/enrichers/lustre.c
@@ -230,14 +230,20 @@ static const void *
 lustre_enricher_iter_next(void *iterator)
 {
     struct enricher *enricher = iterator;
-    const void *fsevent;
+    const struct rbh_fsevent *fsevent;
+    int rc;
 
+skip:
     fsevent = rbh_iter_next(enricher->fsevents);
     if (fsevent == NULL)
         return NULL;
 
-    if (enrich(enricher, fsevent))
-        return NULL;
+    rc = enrich(enricher, fsevent);
+    if (rc) {
+        fprintf(stderr, "Failed to enrich entry '%s', skipping it: %s (%d)\n",
+                fsevent->link.name, strerror(errno), errno);
+        goto skip;
+    }
 
     return &enricher->fsevent;
 }
