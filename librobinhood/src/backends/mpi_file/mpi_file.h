b1eb22b3cb6352909dc1da501aba4a884db01e67
Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
2024-05-30 09:51:05 +0200

    mpi-file: create mfu_pred from rbh_filter
    This patch converts a rbh_filter in a list of mfu_pred. Only the
    logical operator AND in the rbh_filter is supported.
    
    Change-Id: Ie1cd13448a85c4c8288a1f399f81466f6950c42e
    Signed-off-by: Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>

---
+++ librobinhood/src/backends/mpi_file/mpi_file.h
@@ -0,0 +1,23 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_MPI_FILE_H
+#define RBH_MPI_FILE_H
+
+#include <stdbool.h>
+
+#include "mfu.h"
+#include "robinhood/filter.h"
+
+bool
+convert_rbh_filter(mfu_pred *pred, mfu_pred_times *now,
+                   struct rbh_filter *filter);
+
+mfu_pred *
+rbh_filter2mfu_pred(struct rbh_filter *filter);
+
+#endif
