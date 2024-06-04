b0231b92b64345572c55a11827e09db51a934ff0
VALERI Yoann <yoann.valeri@cea.fr>
2024-06-04 09:23:42 +0200

    fsevents: add "no-skip" option
    This patch adds an option "no-skip" that will make the command stop if
    an error is detected when enriching Lustre information.
    
    Change-Id: I6d37aa2c01939016a11b4cc4211c78fd42569b58
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-fsevents/rbh-fsevents.c
+++ rbh-fsevents/rbh-fsevents.c
@@ -312,6 +312,8 @@ destroy_enrich_point(void)
         rbh_backend_destroy(enrich_point);
 }
 
+static bool skip_error = true;
+
 static void
 feed(struct sink *sink, struct source *source,
      struct enrich_iter_builder *builder, bool allow_partials,
@@ -332,7 +334,7 @@ feed(struct sink *sink, struct source *source,
             break;
 
         if (builder != NULL)
-            fsevents = build_enrich_iter(builder, fsevents);
+            fsevents = build_enrich_iter(builder, fsevents, skip_error);
         else if (!allow_partials)
             fsevents = iter_no_partial(fsevents);
 
@@ -396,6 +398,10 @@ main(int argc, char *argv[])
             .val = 'h',
         },
         {
+            .name = "no-skip",
+            .val = 'n',
+        },
+        {
             .name = "raw",
             .val = 'r',
         },
@@ -407,7 +413,8 @@ main(int argc, char *argv[])
     char c;
 
     /* Parse the command line */
-    while ((c = getopt_long(argc, argv, "b:e:hlr", LONG_OPTIONS, NULL)) != -1) {
+    while ((c = getopt_long(argc, argv, "b:e:hnr",
+                            LONG_OPTIONS, NULL)) != -1) {
         switch (c) {
         case 'b':
             if (!str2size_t(optarg, &dedup_opts.batch_size))
@@ -422,6 +429,9 @@ main(int argc, char *argv[])
         case 'h':
             usage();
             return 0;
+        case 'n':
+            skip_error = false;
+            break;
         case 'r':
             /* Ignore errors on close */
             mount_fd_exit();
