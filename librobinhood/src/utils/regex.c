fc71f74683e5291f8e670a19d5320a26a619ba95
Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
2024-06-06 09:33:10 +0200

    find: move shell2pcre to librobinhood
    This patch moves the function shell2pcre from rbh-find to
    librobinhood. Initially, rbh-find was converting the shell pattern
    from the name/path predicate into a Perl Compatible Regex (PCRE) for
    the Mongo backend, but the mpifile backend didn't support it. Now,
    it is no longer rbh-find but the backend itself that handles and
    converts it if needed.
    
    Change-Id: I093bf5469cec829f09c4398aafd5eaa9ecc614f6
    Signed-off-by: Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
    Reviewed-on: https://gerrithub.io/c/robinhood-suite/robinhood4/+/1195891
    Tested-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

---
+++ librobinhood/src/utils/regex.c
@@ -0,0 +1,133 @@
+/* This file is part of the RobinHood Library
+ * Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "robinhood/utils.h"
+
+char *
+shell2pcre(const char *shell)
+{
+    char *pcre;
+    size_t len = 0, i = 0, j = 0, k = 0;
+    bool escaped = false, end = false;
+
+    /* Compute the size of the destination string */
+    while (!end) {
+        switch (shell[i++]) {
+            case '\0':
+                end = true;
+                break;
+            case '\\':
+                escaped = !escaped;
+                break;
+            case '*':
+            case '?':
+            case '|':
+            case '+':
+            case '(':
+            case ')':
+            case '{':
+            case '}':
+                if (!escaped)
+                    len++;
+                escaped = false;
+                break;
+            case '[':
+            case ']':
+                escaped = false;
+                break;
+            default:
+                if (escaped)
+                    len--;
+                escaped = false;
+                break;
+        }
+        len++;
+    }
+
+    /*                                     `len'
+     *                                    vvvvvvv
+     * Allocate the destination string: "^<regex>(?!\n)$"
+     *                                   ^       ^^^^^^^^
+     *                                   1       234 5678
+     */
+    pcre = malloc(len + 8);
+    if (pcre == NULL) {
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    len = i - 1; /* len = strlen(shell) */
+    pcre[k++] = '^';
+    escaped = false;
+    for (i = 0; i < len; i++) {
+        switch (shell[i]) {
+        case '\\':
+            escaped = !escaped;
+            break;
+        case '*':
+            if (!escaped) {
+                strncpy(&pcre[k], &shell[j], i - j);
+                k += i - j;
+                j = i;
+                pcre[k++] = '.';
+            }
+            escaped = false;
+            break;
+        case '?':
+            if (!escaped) {
+                strncpy(&pcre[k], &shell[j], i - j);
+                k += i - j;
+                j = i + 1;
+                pcre[k++] = '.';
+            }
+            escaped = false;
+            break;
+        case '.':
+        case '|':
+        case '+':
+        case '(':
+        case ')':
+        case '{':
+        case '}':
+            if (!escaped) {
+                /* Those characters need to be escaped */
+                strncpy(&pcre[k], &shell[j], i - j);
+                k += i - j;
+                j = i;
+                pcre[k++] = '\\';
+            }
+            escaped = false;
+            break;
+        case '[':
+        case ']':
+            escaped = false;
+            break;
+        default:
+            if (escaped) {
+                /* Skip the escape char, it is meaningless */
+                strncpy(&pcre[k], &shell[j], i - j - 1);
+                k += i - j - 1;
+                j = i;
+            }
+            escaped = false;
+            break;
+        }
+    }
+    strncpy(&pcre[k], &shell[j], i - j);
+    k += i - j;
+    snprintf(&pcre[k], 7, "(?!\n)$");
+
+    return pcre;
+}
