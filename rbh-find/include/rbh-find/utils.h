9bba9ac265427b287a6e8e36f644d58c10385aaa
Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
2024-06-06 09:33:10 +0200

    find: move shell2pcre to librobinhood
    This patch moves the function shell2pcre from rbh-find to
    librobinhood. Initially, rbh-find was converting the shell pattern
    from the name/path predicate into a Perl Compatible Regex (PCRE) for
    the Mongo backend, but the mpifile backend didn't support it. Now,
    it is no longer rbh-find but the backend itself that handles and
    converts it if needed.
    
    Change-Id: I093bf5469cec829f09c4398aafd5eaa9ecc614f6
    Signed-off-by: Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>

--- rbh-find/include/rbh-find/utils.h
+++ rbh-find/include/rbh-find/utils.h
@@ -8,23 +8,6 @@
 #ifndef RBH_FIND_UTILS_H
 #define RBH_FIND_UTILS_H
 
-/**
- * shell2pcre - translate a shell pattern into a Perl Compatible regex
- *
- * @param shell the shell pattern to convert
- *
- * @return      a pointer to the newly allocated perl compatible regex
- *
- * The conversion prefixes any '*' and '?' with a dot unless it is escaped.
- * A leading '^' and a trailing '(?!\n)$' are added to the pattern.
- * The '(?!\n)' part is meant as a workaround to PCRE's specification that
- * allows '$' to match either before or after the last trailing newline.
- *
- * The returned pointer must be freed by the caller.
- */
-char *
-shell2pcre(const char *shell);
-
 enum time_unit {
     TU_SECOND,
     TU_MINUTE,
