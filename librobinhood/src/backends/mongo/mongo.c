e5938c2221335eaadef0fac7b6b2ddf2c059a636
VALERI Yoann <yoann.valeri@cea.fr>
2024-06-12 12:44:53 +0200

    config: change return type of `rbh_config_find`
    This patch changes the return type of `rbh_config_find` to an `enum
    key_parse_result`, as is used internally. This allows for easier usage
    by the caller to differentiate when the key is found and when it's not.
    
    Change-Id: Ibe604f70852fe8cb1d43df13d0a0f757bd492998
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1196215
    Reviewed-by: Gauthier Evraerd <gauthier.evraerd.ocre@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- librobinhood/src/backends/mongo/mongo.c
+++ librobinhood/src/backends/mongo/mongo.c
@@ -1381,14 +1381,14 @@ static char *
 get_mongo_addr()
 {
     struct rbh_value value = { 0 };
+    enum key_parse_result rc;
     char *addr;
-    int rc;
 
     rc = rbh_config_find(MONGODB_ADDRESS_KEY, &value);
-    if (rc)
+    if (rc == KPR_ERROR)
         return NULL;
 
-    if (value.type != -1) {
+    if (rc == KPR_FOUND) {
         if (value.type != RBH_VT_STRING) {
             // TODO: add a conversion value_type to string
             fprintf(stderr,
