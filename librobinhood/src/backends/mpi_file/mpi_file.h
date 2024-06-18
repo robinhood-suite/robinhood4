8f1bc285733e35df15ffa13e5a14eece34eb7864
Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>
2024-06-18 12:00:00 +0200

    mpi-file: modify creation of mfu_pred
    This patchs modifies the way the mfu_pred are created from a
    rbh_filter in order to support the OR and NOT logical operators.
    
    The structure of the rbh_filter is reproduced with mfu_pred structure.
    Like the rbh_filter, there are logical mfu_pred and comparison
    mfu_pred. Each mfu_pred takes a function and an argument. The function
    determine if it's a logical or comparison mfu_pred.
    
    The logical mfu_pred are combinations of other mfu_pred.
    The functions used for the logic are _MFU_PRED_AND and _MFU_PRED_NULL.
    It takes as an argument another mfu_pred containing a combination of
    other mfu_pred.
    
    The comparison mfu_pred represents a single predicate.
    The functions used are _MFU_PRED_SIZE, _MFU_PRED_PATH, and all the
    functions from mpiFileUtils. They takes as an argument the predicate's
    value.
    
    Change-Id: I4985ef9b59dfb82b1ade8282b0068c4356738eea
    Signed-off-by: Gauthier EVRAERD <gauthier.evraerd.ocre@cea.fr>

--- librobinhood/src/backends/mpi_file/mpi_file.h
+++ librobinhood/src/backends/mpi_file/mpi_file.h
@@ -13,6 +13,45 @@
 #include "mfu.h"
 #include "robinhood/filter.h"
 
+/**
+ * The structure of the rbh_filter is reproduced with mfu_pred structure.
+ * Like the rbh_filter, there are logical mfu_pred and comparison mfu_pred.
+ * Each mfu_pred takes a function and an argument. The function determines if
+ * it's a logical or comparison mfu_pred.
+ *
+ * Logical mfu_pred:
+ *
+ * Logical mfu_pred are combinations of other mfu_pred.
+ *
+ * The logical mfu_pred functions are _MFU_PRED_AND and _MFU_PRED_NULL.
+ *
+ * It takes as an argument a mfu_pred which is the first element of a linked
+ * list of mfu_pred.
+ *
+ * Comparison mfu_pred:
+ *
+ * The comparison mfu_pred represents a single predicate.
+ *
+ * The comparison mfu_pred functions are _MFU_PRED_SIZE, _MFU_PRED_PATH,...,
+ * and all the functions from mpiFileUtils.
+ *
+ * It takes as an argument the predicate's value.
+ *
+ * Example:
+ * -name file -and -type f
+ *  <=>
+ *               mfu_pred
+ *  function: _MFU_PRED_AND
+ *                  |             next
+ *  argument:    mfu_pred ---------------------> mfu_pred
+                    |                                |
+ *  function: MFU_PRED_NAME          function: MFU_PRED_TYPE
+ *                  |                                |
+ *  argument:     value               argument:    value
+ *                 "f"                              "f"
+ */
+
+
 /*----------------------------------------------------------------------------*
  |                                PRED FUNCTION                               |
  *----------------------------------------------------------------------------*/
@@ -27,8 +66,19 @@ int
 _MFU_PRED_PATH(mfu_flist flist, uint64_t idx, void *arg);
 
 /*----------------------------------------------------------------------------*
+ |                          PRED LOGICAL FUNCTION                             |
+ *---------------------------------------------------------------------------*/
+
+int
+_MFU_PRED_AND(mfu_flist flist, uint64_t idx, void *arg);
+
+int
+_MFU_PRED_NULL(mfu_flist flist, uint64_t idx, void *arg);
+
+/*----------------------------------------------------------------------------*
  |                                  FILTER                                    |
  *----------------------------------------------------------------------------*/
+
 bool
 convert_rbh_filter(mfu_pred *pred, mfu_pred_times *now, int prefix_len,
                    const struct rbh_filter *filter);
@@ -36,4 +86,8 @@ convert_rbh_filter(mfu_pred *pred, mfu_pred_times *now, int prefix_len,
 mfu_pred *
 rbh_filter2mfu_pred(const struct rbh_filter *filter, int prefix_len,
                     mfu_pred_times *now);
+
+void
+_mfu_pred_free(mfu_pred *pred);
+
 #endif
