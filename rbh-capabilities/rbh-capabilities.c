b16e1c3314054e5e2ba5f153547133b57c0025e6
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2024-07-04 14:35:20 +0200

    Capabilities: Add a new command to show capabilities of backend.
    Add a new command rbh-capabilities to show capabilities of backend among filter,
    update and branch. To do this we add one function that load a backend to recover
    capabilities and one other that translate binary capabilities indicator to human
    readable text. We modify all backend structure to add the capabilities binary indicator
    
    Change-Id: Ib14650e5f6d77800d8df286f03b654cd5816bb7e
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-capabilities/rbh-capabilities.c
@@ -0,0 +1,106 @@
+/* This file is part of Robinhood 4
+ * Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+#include <dlfcn.h>
+#include <errno.h>
+#include <error.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sysexits.h>
+#include <getopt.h>
+
+#include <robinhood.h>
+#include <robinhood/utils.h>
+#include <robinhood/config.h>
+
+static const struct rbh_backend_plugin *
+import_backend_plugin(const char *name)
+{
+    const struct rbh_backend_plugin *plugin;
+    plugin = rbh_backend_plugin_import(name);
+
+    if (plugin != NULL){
+        return plugin;
+    } else {
+        printf("This backend does not exist\n\n");
+        return NULL;
+    };
+}
+
+static int
+capabilities_translate(const struct rbh_backend_plugin *plugin)
+{
+
+    const uint8_t capabilities = plugin->capabilities;
+    printf("Capabilities of %s :\n\n",plugin->plugin.name);
+    if (capabilities & RBH_FILTER_OPS)
+    {
+        printf("- filter\n");
+    }
+    if (capabilities & RBH_UPDATE_OPS)
+    {
+        printf("- update\n");
+    }
+    if (capabilities & RBH_BRANCH_OPS)
+    {
+        printf("- branch\n");
+    }
+    printf("\n");
+    return 0;
+}
+
+static int
+help()
+{
+    const char *message =
+        "Usage:\n"
+        "  %s <name of backend>   Show capabilities of the given backend"
+        "name\n"
+        "  --help                 Show this message and exit\n"
+    return printf(message, program_invocation_short_name);
+}
+
+int
+main(int argc, char **argv)
+{
+    const struct option LONG_OPTIONS[] = {
+        {
+            .name = "help",
+            .val = 'h'
+        },
+        {}
+    };
+    int option;
+
+    if (argc == 1){
+        printf("No backend name given, Please give a backend name\n");
+        help();
+        return EINVAL;
+    }
+
+    while ((option = getopt_long(argc, argv, "h", LONG_OPTIONS,
+                                 NULL)) != -1) {
+        switch (option) {
+        case 'h':
+            help();
+            return 0;
+        default :
+            fprintf(stderr, "Unrecognized option\n\n");
+            help();
+            return EINVAL;
+        }
+    }
+
+    const char *arg = argv[optind];
+    const struct rbh_backend_plugin *plugin = import_backend_plugin(arg);
+    if (plugin == NULL) {
+        return EINVAL;
+    } else {
+        capabilities_translate(plugin);
+        return 0;
+    }
+}
