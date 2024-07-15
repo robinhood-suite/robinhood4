4aeae21747c1e1dbf8a636c036ff68919b05a367
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2024-07-15 10:06:50 +0200

    Capabilities: Add listing of installed backends.
    We add a -l argument to show the backends installed list. We do this
    by looking the names of the installed library in different folders on
    the system.
    
    Change-Id: I5703bfd04c221605b1dbe4a51b5743211c6ce836
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- rbh-capabilities/rbh-capabilities.c
+++ rbh-capabilities/rbh-capabilities.c
@@ -10,12 +10,57 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <sysexits.h>
 #include <getopt.h>
 
 #include <robinhood.h>
 #include <robinhood/utils.h>
+#include <robinhood/list.h>
 #include <robinhood/config.h>
+
+struct rbh_node_capabilities {
+    char *name;
+    struct rbh_list_node list;
+};
+
+static int
+add_list(struct rbh_list_node *head, const char *name)
+{
+    struct rbh_node_capabilities *new_node = malloc(sizeof(*new_node));
+
+    if (new_node == NULL) {
+        perror("malloc");
+        return 1;
+    }
+    new_node->name = strdup(name);
+    if (new_node->name == NULL) {
+        perror("strdup");
+        return 1;
+    }
+    rbh_list_add_tail(head, &new_node->list);
+    return 0;
+}
+
+int
+name_in_list(struct rbh_list_node *head, const char *name)
+{
+    struct rbh_node_capabilities *node;
+
+    if (rbh_list_empty(head)) {
+        return 0;
+    }
+
+    rbh_list_foreach(head, node, list) {
+        if (strcmp(node->name, name) == 0)
+            return 1;
+    }
+    return 0;
+}
 
 static int
 capabilities_translate(const struct rbh_backend_plugin *plugin)
@@ -42,8 +87,127 @@ help()
         "Usage:"
         "  %s <name of backend>   Show capabilities of the given backend"
         " name\n"
-        "  -h --help                 Show this message and exit\n";
+        "Arguments:\n"
+        "  -h --help                 Show this message and exit\n"
+        "  -l --list                 Show the list of backends\n";
+>>>>>>> 22c32d1 (Capabilities: Add listing of installed backends.)
     return printf(message, program_invocation_short_name);
+}
+
+static int
+search_library(const char *dir, const char *pattern, struct rbh_list_node *head)
+{
+    DIR *dp = opendir(dir);
+    struct dirent *entry;
+    struct stat statbuf;
+
+    if (dp == NULL)
+        return 0;
+
+    while ((entry = readdir(dp)) != NULL) {
+        char path[1024];
+        snprintf(path, sizeof(path),"%s/%s", dir, entry->d_name);
+
+        if (stat(path, &statbuf) == -1)
+            continue;
+
+        if (S_ISREG(statbuf.st_mode) && strstr(entry->d_name, pattern))
+            add_list(head, entry->d_name);
+    }
+    closedir(dp);
+    return 0;
+}
+
+static struct rbh_list_node *
+extract_names(struct rbh_list_node *head)
+{
+    struct rbh_list_node *new_list = malloc(sizeof(*new_list));
+    struct rbh_node_capabilities *node;
+    struct rbh_node_capabilities *tmp;
+
+    if (new_list == NULL) {
+        perror("malloc");
+        return NULL;
+    }
+
+    rbh_list_init(new_list);
+
+    rbh_list_foreach_safe(head, node, tmp, list) {
+        char *prefix_backend_name = strchr(node->name, '-');
+        if (prefix_backend_name)
+            prefix_backend_name++;
+        else
+            continue;
+
+        char *suffix_backend_name = strchr(prefix_backend_name, '.');
+        if (suffix_backend_name)
+            *suffix_backend_name = '\0';
+        else
+            continue;
+
+        if (name_in_list(new_list, prefix_backend_name) == 0){
+            if (add_list(new_list, prefix_backend_name) != 0) {
+                free(new_list);
+                return NULL;
+            }
+        }
+    }
+
+    return new_list;
+}
+
+static int
+rbh_backend_list()
+{
+    const char *library_dirs[] = {
+        "/lib",
+        "/usr/lib",
+        "/usr/local/lib",
+        "/lib64",
+        "/usr/lib64",
+        "/usr/local/lib64",
+    };
+    int len_library = sizeof(library_dirs) / sizeof(library_dirs[0]);
+    struct rbh_list_node *backends_list;
+    struct rbh_node_capabilities *node;
+    struct rbh_node_capabilities *tmp;
+    const char *prefix = "librbh-";
+    struct rbh_list_node head;
+
+    rbh_list_init(&head);
+
+    for (int i = 0; i < len_library; i++) {
+        struct stat statbuf;
+        if (lstat(library_dirs[i], &statbuf) == -1)
+            continue;
+
+        if (S_ISLNK(statbuf.st_mode))
+            continue;
+
+        search_library(library_dirs[i], prefix, &head);
+    }
+
+    backends_list = extract_names(&head);
+    if (backends_list == NULL) {
+        return 1;
+    }
+
+    printf("List of installed backends:\n");
+
+    rbh_list_foreach_safe(backends_list, node, tmp, list) {
+        printf("- %s\n", node->name);
+        free(node->name);
+        free(node);
+    }
+
+    rbh_list_foreach_safe(&head, node, tmp, list) {
+        free(node->name);
+        free(node);
+    }
+
+    free(backends_list);
+
+    return 0;
 }
 
 int
@@ -54,6 +218,10 @@ main(int argc, char **argv)
             .name = "help",
             .val = 'h'
         },
+        {
+            .name = "list",
+            .val = 'l'
+        },
         {}
     };
     const struct rbh_backend_plugin *plugin;
@@ -61,19 +229,22 @@ main(int argc, char **argv)
     int option;
 
     if (argc == 1){
-        printf("No backend name given, Please give a backend name\n");
+        fprintf(stderr, "No backend name given, Please give a backend name\n");
         help();
         return EINVAL;
     }
 
-    while ((option = getopt_long(argc, argv, "h", LONG_OPTIONS,
+    while ((option = getopt_long(argc, argv, "hl", LONG_OPTIONS,
                                  NULL)) != -1) {
         switch (option) {
         case 'h':
             help();
             return 0;
+        case 'l':
+            rbh_backend_list();
+            return 0;
         default :
-            fprintf(stderr, "Unrecognized option\n\n");
+            fprintf(stderr, "Unrecognized option\n");
             help();
             return EINVAL;
         }
