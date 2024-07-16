c517fcef64a82620656fc710b74b07ae67d9206b
VALERI Yoann <yoann.valeri@cea.fr>
2024-07-16 14:55:54 +0200

    posix: move value creation for xattrs
    This patch moves the creation of the value when retrieving xattrs to a
    new function. This new function will call the configuration and keep a
    map of how each xattr should be interpreted and its end typing be.
    
    Change-Id: I95da5bffde11dbf53d9d770d321d517c49e307d6
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ librobinhood/src/backends/posix/xattrs_mapping.h
@@ -0,0 +1,20 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_POSIX_XATTRS_MAPPING_H
+#define RBH_POSIX_XATTRS_MAPPING_H
+
+#include <sys/types.h>
+
+#include "robinhood/sstack.h"
+#include "robinhood/value.h"
+
+struct rbh_value *
+create_value_from_xattr(const char *name, char *buffer, ssize_t length,
+                        struct rbh_sstack *xattrs);
+
+#endif
