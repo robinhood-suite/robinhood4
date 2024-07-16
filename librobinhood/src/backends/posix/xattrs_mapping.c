65493f004c4999e86b100c0d16a32d229b4ca1e0
VALERI Yoann <yoann.valeri@cea.fr>
2024-07-16 16:24:34 +0200

    posix: initialize xattrs types map
    This patch adds the initialization of the xattrs_types map in the POSIX
    backend by using the config's xattrs map number of pairs.
    
    Change-Id: I6f2ebfebd5f76a77d1b05346346466ba00c62902
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/backends/posix/xattrs_mapping.c
+++ librobinhood/src/backends/posix/xattrs_mapping.c
@@ -16,6 +16,16 @@
 #define XATTRS_MAP_ADDRESS_KEY "xattrs_map"
 
 static struct rbh_value_map *xattrs_types;
+static size_t xattrs_types_count = 1 << 7;
+static struct rbh_sstack *xattrs_types_stack;
+
+__attribute__((destructor))
+void
+free_xattrs_variables(void)
+{
+    if (xattrs_types)
+        rbh_sstack_destroy(xattrs_types_stack);
+}
 
 struct rbh_value *
 create_value_from_xattr(const char *name, char *buffer, ssize_t length,
@@ -41,14 +51,44 @@ int
 set_xattrs_types_map()
 {
     struct rbh_value value = { 0 };
+    struct rbh_value_pair *pairs;
     enum key_parse_result rc;
 
     rc = rbh_config_find(XATTRS_MAP_ADDRESS_KEY, &value, RBH_VT_MAP);
     if (rc == KPR_ERROR)
         return -1;
 
-    if (rc == KPR_NOT_FOUND)
+    if (rc == KPR_NOT_FOUND) {
         xattrs_types = NULL;
+        return 0;
+    }
+
+    xattrs_types_stack = rbh_sstack_new(xattrs_types_count);
+    if (xattrs_types_stack == NULL)
+        return false;
+
+    xattrs_types = rbh_sstack_push(xattrs_types_stack, NULL,
+                                   sizeof(*xattrs_types));
+    if (xattrs_types == NULL)
+        return -1;
+
+    pairs = rbh_sstack_push(xattrs_types_stack, NULL,
+                            value.map.count * sizeof(*pairs));
+    if (pairs == NULL)
+        return -1;
+
+    for (int i = 0; i < value.map.count; i++) {
+        struct rbh_value *_value;
+
+        pairs[i].key = value.map.pairs[i].key;
+        _value = rbh_sstack_push(xattrs_types_stack, NULL, sizeof(*_value));
+        if (_value == NULL)
+            return -1;
+
+        _value->type = RBH_VT_BINARY;
+
+        pairs[i].value = _value;
+    }
 
     return 0;
 }
