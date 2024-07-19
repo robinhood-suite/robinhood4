0ed3571f069e1ab3ae235b39ffc1c0eba2646eed
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2024-07-19 14:29:54 +0200

    Capabilities: rbh-capabilities tests
    Add some tests in a bash file test_capabilities.bash
    
    Change-Id: I81eac8c5887b07ceef7bf5c814b08603f30fe786
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-capabilities/tests/capabilities.bash
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# This file is part of rbh-capabilities.
+# Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/test_utils.bash
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+tests_backend_installed_list()
+{
+    local output=$(rbh-capabilities --list)
+    echo "$output" | grep -q "mongo"
+    echo "$output" | grep -q "posix"
+    echo "$output" | grep -q "List"
+}
+
+tests_mongo_capabilities()
+{
+    local output=$(rbh-capabilities mongo)
+    echo "$output" | grep -q "update"
+    echo "$output" | grep -q "filter"
+    echo "$output" | grep -q "synchronisation"
+    echo "$output" | grep -q "branch"
+}
+
+tests_posix_capabilities()
+{
+    local output=$(rbh-capabilities posix)
+    echo "$output" | grep -q "synchronisation"
+    echo "$output" | grep -q "branch"
+}
+
+tests_not_installed_capabilities()
+{
+    rbh-capabilities not_a_backend && 
+        error "Expected capabilities with an unknown backend to fail"
+
+    return 0
+}
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+declare -a tests=(tests_backend_installed_list tests_mongo_capabilities
+                  tests_posix_capabilities tests_not_installed_capabilities)
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+run_tests ${tests[@]}
