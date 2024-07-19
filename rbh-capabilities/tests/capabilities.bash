a4c1b98bc78fb9ebed78ebeb277b98888c8d5603
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2024-07-19 14:29:54 +0200

    Capabilities: rbh-capabilities tests
    Add some tests in a bash files test_capabilities.bash
    
    Change-Id: I81eac8c5887b07ceef7bf5c814b08603f30fe786
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-capabilities/tests/capabilities.bash
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# This file is part of rbh-capabilities.
+# Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/test_utils.bash
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+tests_backend_installed_list()
+{
+    local output=$(rbh-capabilities --list)
+    echo "$output" | grep -q "mongo"
+    echo "$output" | grep -q "posix"
+    echo "$output" | grep -q "List"
+}
+
+tests_mongo_capabilities()
+{
+    local output=$(rbh-capabilities mongo)
+    echo "$output" | grep -q "update"
+    echo "$output" | grep -q "filter"
+    echo "$output" | grep -q "branch"
+}
+
+tests_posix_capabilities()
+{
+    local output=$(rbh-capabilities posix)
+    echo "$output" | grep -q "read"
+    echo "$output" | grep -q "branch"
+}
+
+tests_uninstalled_capabilities()
+{
+    output=$(rbh-capabilities not_a_backend 2>&1 || true)
+    echo "$output" | grep -q "backend"
+}
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+declare -a tests=(tests_backend_installed_list tests_mongo_capabilities
+                  tests_posix_capabilities tests_uninstalled_capabilities)
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+run_tests ${tests[@]}
---
+++ rbh-capabilities/tests/test_utils.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+################################################################################
+#                                  UTILITIES                                   #
+################################################################################
+
+SUITE=${BASH_SOURCE##*/}
+SUITE=${SUITE%.*}
+
+setup()
+{
+    # Create a test directory and `cd` into it
+    testdir=$PWD/$SUITE-$test
+    mkdir "$testdir"
+    cd "$testdir"
+
+    # Create test database's name
+    testdb=$SUITE-$test
+}
+
+error()
+{
+    echo "$*"
+    exit 1
+}
+
+run_tests()
+{
+    local fail=0
+
+    for test in "$@"; do
+        (set -e; trap -- teardown EXIT; setup; "$test")
+        if !(($?)); then
+            echo "$test: ✔"
+        else
+            echo "$test: ✖"
+            fail=1
+        fi
+    done
+
+    return $fail
+}
