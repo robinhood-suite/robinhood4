1d25f59a072c45266614309e1e5f089b91ae95a1
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2024-10-23 16:18:17 +0200

    capabilities: bugfix listing not loadable backend
    We remove the fetch action from backends in
    usr/local/* to be consistent with how dlopen
    works and thus prevent rbh-capabilities -l
    from displaying backends that cannot be loaded.
    (because not in the scope of dlopen)
    
    Change-Id: I9dc5c318edc4d2452b835d40a6b1dfb8f3039fe5
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- rbh-capabilities/tests/capabilities.bash
+++ rbh-capabilities/tests/capabilities.bash
@@ -83,13 +83,72 @@ tests_library_path_env_invalid()
     fi
 }
 
+tests_library_path_correctly_set()
+{
+    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/lib64"
+
+    local output=$(rbh_capabilities mongo)
+    if echo "$output" | grep -q "Capabilities of mongo:"; then
+        echo "Mongo backend imported successfully as expected"
+    else
+        error "Mongo backend import failed: Expected 'Capabilities of mongo:'
+            but got: $output"
+    fi
+}
+
+tests_mongo_backend_detection()
+{
+    local backend_path="/home/$SUDO_USER/robinhood4/builddir/librobinhood/"\
+"src/backends/mongo"
+    local non_standard_path="/tmp/non_standard"
+    local TEMP=$LD_LIBRARY_PATH
+
+    sudo mkdir -p $non_standard_path
+
+    sudo rm -f /usr/lib/librbh-mongo.so
+    sudo rm -f /usr/lib/librbh-mongo.so.1
+
+    sudo rm -f /usr/lib64/librbh-mongo.so
+    sudo rm -f /usr/lib64/librbh-mongo.so.1
+
+    sudo mv $backend_path/librbh-mongo.so $non_standard_path
+    sudo cp $backend_path/librbh-mongo.so.1 $non_standard_path/
+
+    unset LD_LIBRARY_PATH
+    local output=$(rbh_capabilities mongo 2>&1)
+
+    if echo "$output" | grep -q "This backend does not exist"; then
+        echo "Mongo backend correctly not imported as expected"
+    else
+        error "Mongo backend was unexpectedly detected in default paths"
+    fi
+
+    export LD_LIBRARY_PATH=$non_standard_path
+    output=$(rbh_capabilities mongo 2>&1)
+    sudo mv $non_standard_path/librbh-mongo.so $backend_path
+
+    if echo "$output" | grep -q "Capabilities of mongo"; then
+        echo "Mongo backend imported successfully from non-standard path"
+    else
+        error "Mongo backend import failed from non-standard path: $output"
+        rm -rf $non_standard_path
+    fi
+
+    sudo cp $backend_path/librbh-mongo.so /lib64/
+    sudo cp $backend_path/librbh-mongo.so.1 /lib64/
+
+    export LD_LIBRARY_PATH=$TEMP
+    sudo rm -rf $non_standard_path
+}
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
 declare -a tests=(tests_backend_installed_list tests_mongo_capabilities
                   tests_posix_capabilities tests_not_installed_capabilities
-                  tests_not_find_backend_list)
+                  tests_not_find_backend_list tests_library_path_correctly_set
+                  tests_mongo_backend_detection)
                   # tests_library_path_env_not_exist
                   # tests_library_path_env_invalid)
 
