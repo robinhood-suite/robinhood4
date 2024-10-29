d7803635f972aa5bb079cc370d286cfe9f35327f
VALERI Yoann <yoann.valeri@cea.fr>
2024-10-29 16:30:15 +0100

    report: create directory and empty main file
    Change-Id: I7e0c25ed3d97c33b8a92542a578c8bc3a5383232
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-report/rbh-report.c
@@ -0,0 +1,21 @@
+/* This file is part of Robinhood 4
+ * Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*----------------------------------------------------------------------------*
+ |                                    cli                                     |
+ *----------------------------------------------------------------------------*/
+
+int
+main(int argc, char *argv[])
+{
+    printf("It all starts with success.\n");
+
+    return EXIT_SUCCESS;
+}
