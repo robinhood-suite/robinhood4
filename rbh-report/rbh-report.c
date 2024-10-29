9c2765b4fc31f65981a6bd7b05fcdc959b0ae751
VALERI Yoann <yoann.valeri@cea.fr>
2024-10-29 16:30:53 +0100

    report: add usage and opt loop
    Change-Id: Iafbefe7bb18ee10c8a0f819f848ac7a574c51b53
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-report/rbh-report.c
+++ rbh-report/rbh-report.c
@@ -5,17 +5,71 @@
  * SPDX-License-Identifer: LGPL-3.0-or-later
  */
 
+#include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sysexits.h>
+
+#include <robinhood/uri.h>
 
 /*----------------------------------------------------------------------------*
  |                                    cli                                     |
  *----------------------------------------------------------------------------*/
 
+    /*--------------------------------------------------------------------*
+     |                              usage()                               |
+     *--------------------------------------------------------------------*/
+
+static int
+usage(void)
+{
+    const char *message =
+        "usage: %s [-h] SOURCE\n"
+        "\n"
+        "Create a report from SOURCE's entries\n"
+        "\n"
+        "Positional arguments:\n"
+        "    SOURCE  a robinhood URI\n"
+        "\n"
+        "Optional arguments:\n"
+        "    -h,--help             show this message and exit\n"
+        "\n"
+        "A robinhood URI is built as follows:\n"
+        "    "RBH_SCHEME":BACKEND:FSNAME[#{PATH|ID}]\n"
+        "Where:\n"
+        "    BACKEND  is the name of a backend\n"
+        "    FSNAME   is the name of a filesystem for BACKEND\n"
+        "    PATH/ID  is the path/id of an fsentry managed by BACKEND:FSNAME\n"
+        "             (ID must be enclosed in square brackets '[ID]' to distinguish it\n"
+        "             from a path)\n";
+
+    return printf(message, program_invocation_short_name);
+}
+
 int
 main(int argc, char *argv[])
 {
-    printf("It all starts with success.\n");
+    const struct option LONG_OPTIONS[] = {
+        {
+            .name = "help",
+            .val = 'h',
+        },
+        {}
+    };
+    char c;
+
+    /* Parse the command line */
+    while ((c = getopt_long(argc, argv, "h", LONG_OPTIONS, NULL)) != -1) {
+        switch (c) {
+        case 'h':
+            usage();
+            return 0;
+        default:
+            /* getopt_long() prints meaningful error messages itself */
+            exit(EX_USAGE);
+        }
+    }
 
     return EXIT_SUCCESS;
 }
