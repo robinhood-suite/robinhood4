3e1796016a88d4c924f529f1b58a41b735649a71
VALERI Yoann <yoann.valeri@cea.fr>
2024-10-30 16:25:26 +0100

    report: add basic group stage to the bson pipeline
    This patch adds a basic group stage to the bson pipeline. The goal is
    mostly to have a first version that works, then makes everything generic
    for additional parameters and uses.
    
    For now, this basic group stage will simply group all the entries in the
    backend and sum their sizes.
    
    Change-Id: I18cd5219f0266b167cb1384088322de4cf2b5849
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://gerrithub.io/c/robinhood-suite/robinhood4/+/1203362
    Reviewed-by: Gauthier Evraerd <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- librobinhood/src/backends/mongo/group.c
+++ librobinhood/src/backends/mongo/group.c
@@ -13,14 +13,20 @@
 
 #include "mongo.h"
 
+/* The resulting bson will be as such:
+ * { $group: {_id: '', test: { $sum: "$statx.size" }}}
+ */
 bool
 bson_append_aggregate_group_stage(bson_t *bson, const char *key,
                                   size_t key_length)
 {
     bson_t document;
+    bson_t subdoc;
 
-    if (!bson_append_document_begin(bson, key, key_length, &document))
-        return false;
-
-    return bson_append_document_end(bson, &document);
+    return bson_append_document_begin(bson, key, key_length, &document)
+        && BSON_APPEND_NULL(&document, "_id")
+        && (BSON_APPEND_DOCUMENT_BEGIN(&document, "test", &subdoc)
+            && BSON_APPEND_UTF8(&subdoc, "$sum", "$statx.size")
+            && bson_append_document_end(&document, &subdoc))
+        && bson_append_document_end(bson, &document);
 }
