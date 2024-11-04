27b847324db933a1626aaf772d5a33fe1208d0e3
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-04 14:53:34 +0100

    report: add basic test for rbh-report
    This patch adds a basic test to verify the first pipeline works
    properly.
    
    Change-Id: Ieec50a359bb93b97d282566280e52b4e86c366b8
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-report/tests/acceptance.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_report()
+{
+    touch "empty"
+    truncate --size 1K "1K"
+    truncate --size 1025 "1K+1"
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    local dir_size="$(stat -c %s .)"
+    local size="$((1024 + 1025 + dir_size))"
+
+    rbh_report "rbh:mongo:$testdb" | difflines "$size"
+
+    truncate --size 600M "600M"
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    local size_600M="$(stat -c %s 600M)"
+    dir_size="$(stat -c %s .)"
+    size="$((1024 + 1025 + size_600M + dir_size))"
+
+    rbh_report "rbh:mongo:$testdb" | difflines "$size"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_report)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
