0f0262f7f39eac5669d18202bdac21a7132d708e
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-07 16:38:20 +0100

    report: create projection map
    This patch adds the creation of the projection map in the rbh-report
    tool.
    
    Change-Id: I2b9d03ffcbc0560be70eefb88501988014fc17c0
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-report/rbh-report.c
+++ rbh-report/rbh-report.c
@@ -15,6 +15,16 @@
 #include <robinhood/backend.h>
 #include <robinhood/uri.h>
 
+#define MIN_VALUES_SSTACK_ALLOC (1 << 6)
+static __thread struct rbh_sstack *values_sstack;
+
+static void __attribute__((destructor))
+destroy_values_sstack(void)
+{
+    if (values_sstack)
+        rbh_sstack_destroy(values_sstack);
+}
+
 static struct rbh_backend *from;
 
 static void __attribute__((destructor))
@@ -30,6 +40,32 @@ destroy_from(void)
 }
 
 static void
+create_rbh_filter_output(struct rbh_filter_output *output)
+{
+    struct rbh_value_pair *pairs;
+    struct rbh_value *value;
+
+    pairs = rbh_sstack_push(values_sstack, NULL, 1 * sizeof(*pairs));
+    if (pairs == NULL)
+        error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                      "rbh_sstack_push");
+
+    value = rbh_sstack_push(values_sstack, NULL, 1 * sizeof(*value));
+    if (value == NULL)
+        error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                      "rbh_sstack_push");
+
+    pairs[0].key = "result";
+    pairs[0].value = value;
+    value->type = RBH_VT_STRING;
+    /** XXX: this is not really generic, but it'll do for now */
+    value->string = "$test";
+    output->type = RBH_FOT_MAP;
+    output->map.pairs = pairs;
+    output->map.count = 1;
+}
+
+static void
 report()
 {
     struct rbh_filter_options options = { 0 };
@@ -37,6 +73,15 @@ report()
     struct rbh_mut_iterator *iter;
     struct rbh_filter_group group;
 
+    if (values_sstack == NULL) {
+        values_sstack = rbh_sstack_new(MIN_VALUES_SSTACK_ALLOC *
+                                       sizeof(struct rbh_value *));
+        if (values_sstack == NULL)
+            error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                          "rbh_sstack_new");
+    }
+
+    create_rbh_filter_output(&output);
     iter = rbh_backend_report(from, NULL, &group, &options, &output);
 
     if (iter == NULL)
