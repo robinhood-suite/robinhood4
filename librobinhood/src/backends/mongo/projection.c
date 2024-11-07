5292104ed1cc43e4932a4484b73abb894de41b43
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-07 15:18:52 +0100

    report: set map from rbh_filter_output
    This patch adds the conversion of the value map in the rbh_filter_output
    structure in the projection stage if it is a valid one.
    
    Currently, this map is not filled by rbh-report, so the hardcoded
    behaviour is kept.
    
    Change-Id: I44ff4827c346cddf4d27572da54797d82c7d31b4
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/backends/mongo/projection.c
+++ librobinhood/src/backends/mongo/projection.c
@@ -18,12 +18,26 @@
  */
 bool
 bson_append_aggregate_projection_stage(bson_t *bson, const char *key,
-                                       size_t key_length)
+                                       size_t key_length,
+                                       const struct rbh_filter_output *output)
 {
     bson_t document;
 
-    return bson_append_document_begin(bson, key, key_length, &document)
-        && BSON_APPEND_INT32(&document, "_id", 0)
-        && BSON_APPEND_UTF8(&document, "result", "$test")
-        && bson_append_document_end(bson, &document);
+    if (!(bson_append_document_begin(bson, key, key_length, &document) &&
+          BSON_APPEND_INT32(&document, "_id", 0)))
+        return false;
+
+    if (output->type != RBH_FOT_MAP || output->map.count == 0) {
+        if (!BSON_APPEND_UTF8(&document, "result", "$test"))
+            return false;
+    } else {
+        for (size_t i = 0; i < output->map.count; i++) {
+            const struct rbh_value_pair *pair = &output->map.pairs[i];
+
+            if (!BSON_APPEND_RBH_VALUE(&document, pair->key, pair->value))
+                return false;
+        }
+    }
+
+    return bson_append_document_end(bson, &document);
 }
