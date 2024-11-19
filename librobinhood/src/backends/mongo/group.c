7f429520621deedaf20ef1a3cca7f4e4dfb5ada8
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-19 11:51:29 +0100

    mongo: convert group value to bson
    This patch adds the function that will convert a group value to bson.
    
    Change-Id: I5194b0b199edf0a7d93213e7ac21af1122bdb16f
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/backends/mongo/group.c
+++ librobinhood/src/backends/mongo/group.c
@@ -13,6 +13,17 @@
 
 #include "mongo.h"
 
+static bool
+insert_rbh_filter_value(bson_t *bson, struct rbh_modifier_field *field)
+{
+    bson_t document;
+
+    if (!BSON_APPEND_DOCUMENT_BEGIN(bson, "test", &document))
+        return false;
+
+    return bson_append_document_end(bson, &document);
+}
+
 /* The resulting bson will be as such:
  * { $group: {_id: '', test: { $sum: "$statx.size" }}}
  */
@@ -27,11 +38,20 @@ bson_append_aggregate_group_stage(bson_t *bson, const char *key,
           BSON_APPEND_INT32(&document, "_id", 0)))
         return false;
 
-    for (size_t i = 0; i < group->map.count; i++) {
-        const struct rbh_value_pair *pair = &group->map.pairs[i];
+    if (group && group->count) {
+        for (size_t i = 0; i < group->count; i++) {
+            struct rbh_modifier_field *field = &group->fields[i];
 
-        if (!BSON_APPEND_RBH_VALUE(&document, pair->key, pair->value))
-            return false;
+            if (!insert_rbh_filter_value(&document, field))
+                return false;
+        }
+    } else {
+        for (size_t i = 0; i < group->map.count; i++) {
+            const struct rbh_value_pair *pair = &group->map.pairs[i];
+
+            if (!BSON_APPEND_RBH_VALUE(&document, pair->key, pair->value))
+                return false;
+        }
     }
 
     return bson_append_document_end(bson, &document);
