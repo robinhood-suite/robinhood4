311c3d36cc947ae9629a8ca0a70ba88ba4e336c3
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-20 16:17:19 +0100

    report: add parsing test and rename acceptance to test_sum
    Change-Id: I443f2fd280af6990e551f005897d67294e8a5609
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-report/tests/test_parsing.bash
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_parsing()
+{
+    rbh_report &&
+        error "rbh-report with no URI should have failed"
+
+    rbh_report rbh:mongo:blob &&
+        error "rbh-report with no '--output' should have failed"
+
+    rbh_report rbh:mongo:blob --output &&
+        error "rbh-report with '--output' but no string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(something" &&
+        error "rbh-report with output string missing closing parenthesis" \
+              "should have failed"
+
+    return 0
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_parsing)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
