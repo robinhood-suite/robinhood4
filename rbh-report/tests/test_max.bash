b60e056b20e655748202a480aec8ab817632efc3
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-25 14:38:11 +0100

    report: add test for the `max` accumulator
    Change-Id: Ic405236b6574e50ff833522fc88b1a522f2fc88d
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1204642
    Reviewed-by: Gauthier Evraerd <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

---
+++ rbh-report/tests/test_max.bash
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_max_size()
+{
+    touch "empty"
+    truncate --size 1K "1K"
+    truncate --size 1025 "1K+1"
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    rbh_report "rbh:mongo:$testdb" --output "max(statx.size)" |
+        difflines "1025"
+
+    truncate --size 600M "600M"
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    local size_600M="$(stat -c %s 600M)"
+
+    rbh_report "rbh:mongo:$testdb" --output "max(statx.size)" |
+        difflines "$size_600M"
+}
+
+test_max_mtime()
+{
+    touch first
+    touch -d "2 hours ago" second
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    local first_mtime="$(stat -c %Y first)"
+
+    rbh_report "rbh:mongo:$testdb" --output "max(statx.mtime.sec)" |
+        difflines "$first_mtime"
+
+    touch -d "+2 hours" third
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    local third_mtime="$(stat -c %Y third)"
+
+    rbh_report "rbh:mongo:$testdb" --output "max(statx.mtime.sec)" |
+        difflines "$third_mtime"
+}
+
+test_max_ino()
+{
+    touch first
+    touch second
+    touch third
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    local third_ino="$(stat -c %i third)"
+
+    rbh_report "rbh:mongo:$testdb" --output "max(statx.ino)" |
+        difflines "$third_ino"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_max_size test_max_mtime test_max_ino)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
