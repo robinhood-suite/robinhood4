ec3cc5dd7335057d02c77e3e9eca96363de302c6
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-26 16:30:25 +0100

    report: move output code to own file
    This patch moves around code and creates an include directory, a source
    directory and a header to better organize the code.
    
    Change-Id: Ie127c241bd7633f4f65ed18c1a28bc6f0e5c48a6
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-report/rbh-report.c
+++ rbh-report/rbh-report.c
@@ -15,8 +15,10 @@
 #include <robinhood/backend.h>
 #include <robinhood/uri.h>
 
+#include "report.h"
+
 #define MIN_VALUES_SSTACK_ALLOC (1 << 6)
-static __thread struct rbh_sstack *values_sstack;
+struct rbh_sstack *values_sstack;
 
 static void __attribute__((destructor))
 destroy_values_sstack(void)
@@ -39,146 +41,6 @@ destroy_from(void)
     }
 }
 
-static int
-count_fields(const char *str)
-{
-    /* Even if the string has no comma, it still has at least one field */
-    int count = 1;
-
-    while (*str) {
-        if (*str == ',')
-            count++;
-
-        str++;
-    }
-
-    return count;
-}
-
-static enum field_accumulator
-str2accumulator(const char *str)
-{
-    switch (*str++) {
-    case 'a': /* avg */
-        if (strcmp(str, "vg"))
-            break;
-
-        return FA_AVG;
-    case 'm':
-        switch (*str++) {
-        case 'a': /* max */
-            if (strcmp(str, "x"))
-                break;
-
-            return FA_MAX;
-        case 'i': /* min */
-            if (strcmp(str, "n"))
-                break;
-
-            return FA_MIN;
-        }
-
-        break;
-    case 's': /* sum */
-        if (strcmp(str, "um"))
-            break;
-
-        return FA_SUM;
-    }
-
-    error(EX_USAGE, 0, "invalid accumulator '%s'", str);
-    __builtin_unreachable();
-}
-
-static const struct rbh_accumulator_field
-convert_output_string_to_accumulator_field(char *output_string)
-{
-    const struct rbh_filter_field *filter_field;
-    struct rbh_accumulator_field field;
-    char *opening;
-
-    if (strlen(output_string) == 0)
-        error_at_line(EXIT_FAILURE, EINVAL, __FILE__, __LINE__,
-                      "empty field given");
-
-    opening = strchr(output_string, '(');
-    if (opening) {
-        char *closing = strchr(output_string, ')');
-
-        if (closing == NULL)
-            error_at_line(EXIT_FAILURE, EINVAL, __FILE__, __LINE__,
-                          "'%s' ill-formed, missing ')'", output_string);
-
-        *opening = '\0';
-        *closing = '\0';
-        field.accumulator = str2accumulator(output_string);
-    } else {
-        opening = output_string;
-        field.accumulator = FA_NONE;
-    }
-
-    filter_field = str2filter_field(opening + 1);
-    if (filter_field == NULL)
-        error_at_line(EXIT_FAILURE, EINVAL, __FILE__, __LINE__,
-                      "'%s' ill-formed, invalid field", output_string);
-
-    field.field = *filter_field;
-
-    return field;
-}
-
-static int
-create_rbh_filter_group_output(const char *_output_string,
-                               struct rbh_group_fields *group,
-                               struct rbh_filter_output *output)
-{
-    struct rbh_accumulator_field *fields;
-    char *output_string;
-    int count;
-
-    count = count_fields(_output_string);
-
-    fields = rbh_sstack_push(values_sstack, NULL, count * sizeof(*fields));
-    if (fields == NULL)
-        error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
-                      "rbh_sstack_push");
-
-    output_string = strdup(_output_string);
-    if (output_string == NULL)
-        error_at_line(EXIT_FAILURE, EINVAL, __FILE__, __LINE__, "strdup");
-
-    if (count == 1) {
-        fields[0] = convert_output_string_to_accumulator_field(output_string);
-    } else {
-        char *current_field;
-        int counter = 0;
-        char *token;
-
-        current_field = output_string;
-        token = strchr(current_field, ',');
-        while (token) {
-            *token = '\0';
-            fields[counter++] =
-                convert_output_string_to_accumulator_field(current_field);
-            current_field = token + 1;
-            token = strchr(current_field, ',');
-        }
-
-        fields[counter] =
-            convert_output_string_to_accumulator_field(current_field);
-    }
-
-    free(output_string);
-
-    group->fields = fields;
-    group->count = count;
-    output->type = RBH_FOT_VALUES;
-    output->output_fields.fields = fields;
-    output->output_fields.count = count;
-
-    return count;
-}
-
 static void
 report(const char *group_string, const char *output_string)
 {
@@ -198,8 +60,8 @@ report(const char *group_string, const char *output_string)
                           "rbh_sstack_new");
     }
 
-    expected_field_count = create_rbh_filter_group_output(output_string,
-                                                          &group, &output);
+    expected_field_count = fill_acc_and_output_fields(output_string,
+                                                      &group, &output);
 
     iter = rbh_backend_report(from, NULL, &group, &options, &output);
     if (iter == NULL)
