93008e64fb34d889907cc9ef3b199559bf4fd80d
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-26 16:30:25 +0100

    report: move output code to own file
    This patch moves around code and creates an include directory, a source
    directory and a header to better organize the code.
    
    Change-Id: Ie127c241bd7633f4f65ed18c1a28bc6f0e5c48a6
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-report/include/report.h
@@ -0,0 +1,20 @@
+/* This file is part of Robinhood 4
+ * Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_REPORT_H
+#define RBH_REPORT_H
+
+#include <robinhood/backend.h>
+
+extern struct rbh_sstack *values_sstack;
+
+int
+fill_acc_and_output_fields(const char *_output_string,
+                           struct rbh_group_fields *group,
+                           struct rbh_filter_output *output);
+
+#endif
