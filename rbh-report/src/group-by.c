84dc7c1825b5f0a8035fda8035fe63ecee11b65a
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-27 14:03:43 +0100

    report: count fields in group-by string
    This patch adds the counting of the fields given in the group-by string.
    
    Change-Id: I31aff0f57dd478528f9287dd5dc8eaf6cd0b887c
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-report/src/group-by.c
+++ rbh-report/src/group-by.c
@@ -19,6 +19,31 @@
 void
 fill_group_by_fields(const char *_group_by, struct rbh_group_fields *group)
 {
-    (void) _group_by;
-    (void) group;
+    struct rbh_filter_field *fields;
+    char *group_by;
+    int count;
+
+    if (_group_by == NULL) {
+        group->id_fields = NULL;
+        group->id_count = 0;
+        return;
+    }
+
+    count = count_fields(_group_by);
+
+    fields = rbh_sstack_push(values_sstack, NULL, count * sizeof(*fields));
+    if (fields == NULL)
+        error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__,
+                      "rbh_sstack_push");
+
+    group_by = strdup(_group_by);
+    if (group_by == NULL)
+        error_at_line(EXIT_FAILURE, EINVAL, __FILE__, __LINE__, "strdup");
+
+
+
+    free(group_by);
+
+    group->id_fields = NULL;
+    group->id_count = 0;
 }
