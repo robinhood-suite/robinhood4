40af49a24920924718a1a5a8180cda16b4db3e64
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2024-11-27 11:35:02 +0100

    lfind: Load configuration file
    Adds a function that will parse the command arguments to load a
    config file if provided and the default one in /etc/robinhood4 otherwise.
    
    We also remove the --config from argv and argc.
    
    Change-Id: I745f8c6bcc7d8592f6616c203b0737fbc6319de1
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- librobinhood/src/config.c
+++ librobinhood/src/config.c
@@ -10,6 +10,7 @@
 #include <miniyaml.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sysexits.h>
 
 #include "robinhood/config.h"
 #include "robinhood/serialization.h"
@@ -393,4 +394,35 @@ rbh_config_get_string(const char *key, const char *default_string)
         return default_string;
 
     return value.string;
+}
+
+static void
+handle_config_option(int argc, char *argv[], int index)
+{
+    if (index + 1 >= argc)
+        error(EX_USAGE, EINVAL, "'--config' option requires a file");
+
+    if (rbh_config_open(argv[index + 1]))
+        error(EX_USAGE, errno,
+              "Failed to open configuration file '%s'", argv[index + 1]);
+}
+
+void
+import_configuration_file(int *argc, char ***argv)
+{
+    const char* default_config = "/etc/robinhood4.d/rbh_conf.yaml";
+
+    for (int i = 0; i < *argc; i++) {
+        if (strcmp((*argv)[i], "-c") == 0 ||
+            strcmp((*argv)[i], "--config") == 0) {
+            handle_config_option(*argc, *argv, i);
+
+            return;
+        }
+    }
+
+    if (rbh_config_open(default_config)) {
+        error(EX_USAGE, errno, "Failed to open default configuration file '%s'",
+              default_config);
+    }
 }
