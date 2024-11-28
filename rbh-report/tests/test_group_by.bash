dd4876b76f3baf70d07a530909660981b22cc457
VALERI Yoann <yoann.valeri@cea.fr>
2024-11-28 16:24:25 +0100

    report: add test for multi-field group-by
    Change-Id: I3f705bc73e4e5bc1ee63a93e1a39a64d14950ec1
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-report/tests/test_group_by.bash
+++ rbh-report/tests/test_group_by.bash
@@ -67,11 +67,42 @@ test_group_by_user()
         difflines "$fake_user_size" "$main_user_size"
 }
 
+test_multi_group_by()
+{
+    mkdir first_dir
+    mkdir second_dir
+    truncate --size 1M first_dir/first_file
+    truncate --size 1G second_dir/second_file
+
+    useradd -MN fake_user
+    chown fake_user: first_dir first_dir/first_file
+
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+    userdel fake_user
+
+    local root_size="$(stat -c %s .)"
+    local first_dir_size="$(stat -c %s first_dir)"
+    local second_dir_size="$(stat -c %s second_dir)"
+    local first_file_size="$(stat -c %s first_dir/first_file)"
+    local second_file_size="$(stat -c %s second_dir/second_file)"
+
+    local main_user_dir_size="$((root_size + second_dir_size))"
+    local main_user_file_size="$((second_file_size))"
+    local fake_user_dir_size="$((first_dir_size))"
+    local fake_user_file_size="$((first_file_size))"
+
+    # The sort is necessary here as we have no guarantee over the order of the
+    # output from Mongo until the sort is implemented by rbh-report
+    rbh_report "rbh:mongo:$testdb" --group-by "statx.uid,statx.type" \
+                                   --output "sum(statx.size)" | sort -n |
+        difflines "$fake_user_dir_size" "$main_user_dir_size" "$fake_user_file_size" "$main_user_file_size"
+}
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
-declare -a tests=(test_group_by_type test_group_by_user)
+declare -a tests=(test_group_by_type test_group_by_user test_multi_group_by)
 
 tmpdir=$(mktemp --directory)
 trap -- "rm -rf '$tmpdir'" EXIT
