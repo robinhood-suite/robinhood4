50b2b7143741b2e49e7fbea120b6c1da71ffeec6
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2024-11-28 10:30:07 +0100

    lfind: Get aliases from config
    Adds the function that allows you to load aliases
    from the configuration file and displays the list
    of aliases retrieved in the terminal.
    
    Change-Id: I8c305e3b0d1ec99f4cdbb0af0fc906ff8633dfd2
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ librobinhood/include/robinhood/alias.h
@@ -0,0 +1,14 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef ROBINHOOD_ALIAS_H
+#define ROBINHOOD_ALIAS_H
+
+void
+apply_aliases(void);
+
+#endif
