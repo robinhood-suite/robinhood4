225b508d38e88de7ffb1691211e54153827b5002
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2024-11-28 13:10:55 +0100

    lfind: Resolving aliases and adding to argv, argc
    This patch modifies the operation of apply_aliases
    so that the function parses argv to resolve aliases.
    
    Change-Id: Id8c80622b1ecf55e3a86a66c01b347e86ea1514a
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- librobinhood/src/alias.c
+++ librobinhood/src/alias.c
@@ -113,8 +113,74 @@ load_aliases_from_config(void)
     return 0;
 }
 
+static int
+count_tokens(const char *str)
+{
+    char *copy = strdup(str);
+    int count = 0;
+    char *token = strtok(copy, " ");
+
+    while (token != NULL) {
+        count++;
+        token = strtok(NULL, " ");
+    }
+
+    free(copy);
+    return count;
+}
+
+static void
+alias_resolution(int *argc, char ***argv, size_t alias_index,
+                 size_t argv_alias_index)
+{
+    const char* alias_value = aliases->pairs[alias_index].value->string;
+
+    int new_argc = *argc - 2 + count_tokens(alias_value);
+    int temp_index = 0;
+    char **argv_temp;
+    char *alias_copy;
+    char *arg;
+
+    argv_temp = rbh_sstack_push(aliases_stack, NULL, new_argc * sizeof(char *));
+    if (!argv_temp) {
+        fprintf(stderr, "Failed to allocate memory for argv_temp.\n");
+        return;
+    }
+
+    for (size_t i = 0; i < argv_alias_index; i++) {
+        argv_temp[temp_index++] = (*argv)[i];
+    }
+
+    alias_copy = strdup(alias_value);
+    if (!alias_copy) {
+        fprintf(stderr, "Failed to duplicate alias_value.\n");
+        return;
+    }
+
+    arg = strtok(alias_copy, " ");
+    while (arg != NULL) {
+        argv_temp[temp_index++] = rbh_sstack_push(aliases_stack, NULL,
+                                                  strlen(arg) + 1);
+        if (!argv_temp[temp_index - 1]) {
+            fprintf(stderr, "Failed to allocate memory for token.\n");
+            free(alias_copy);
+            return;
+        }
+        strcpy(argv_temp[temp_index - 1], arg);
+        arg = strtok(NULL, " ");
+    }
+
+    for (size_t i = argv_alias_index + 2; i < *argc; i++)
+        argv_temp[temp_index++] = (*argv)[i];
+
+    *argc = new_argc;
+    *argv = argv_temp;
+
+    free(alias_copy);
+}
+
 void
-apply_aliases(void)
+apply_aliases(int *argc, char ***argv)
 {
     if (load_aliases_from_config() != 0) {
         fprintf(stderr, "Failed to load aliases from configuration.\n");
@@ -126,9 +192,37 @@ apply_aliases(void)
         return;
     }
 
-    printf("List of aliases:\n");
-    for (size_t i = 0; i < aliases->count; i++) {
-        printf("  %s : %s\n", aliases->pairs[i].key,
-               aliases->pairs[i].value->string);
+    for (int i = 0; i < *argc; i++) {
+        if (strcmp((*argv)[i], "--alias") != 0)
+            continue;
+
+        if (i + 1 >= *argc) {
+            fprintf(stderr, "No alias name provided\n");
+            return;
+        }
+
+        const char* alias_name = (*argv)[i + 1];
+        size_t alias_index = -1;
+
+        for (size_t j = 0; j < aliases->count; j++) {
+            if (strcmp(aliases->pairs[j].key, alias_name) == 0) {
+                alias_index = j;
+                break;
+            }
+        }
+
+        if (alias_index != -1) {
+            alias_resolution(argc, argv, alias_index, i);
+
+            printf("Updated argv after resolution:\n");
+            for (int k = 0; k < *argc; k++) {
+                printf("argv[%d]: %s\n", k, (*argv)[k]);
+            }
+
+            i = -1;
+        } else {
+            fprintf(stderr, "Alias '%s' not found.\n", alias_name);
+            return;
+        }
     }
 }
