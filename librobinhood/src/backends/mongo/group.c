0ca9cb37885ab0e26b6e87b25667894f2ee86861
VALERI Yoann <yoann.valeri@cea.fr>
2024-12-03 16:18:29 +0100

    mongo: set group id in another function
    Change-Id: I5fdb0a53e69725d00f64121b3ac86c2f14caeb08
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/backends/mongo/group.c
+++ librobinhood/src/backends/mongo/group.c
@@ -63,6 +63,42 @@ get_accumulator_field_strings(struct rbh_accumulator_field *accumulator_field,
 }
 
 static bool
+insert_group_id_fields(bson_t *bson, const struct rbh_group_fields *group)
+{
+    bson_t subdoc;
+
+    if (group->id_count == 0)
+        return BSON_APPEND_INT32(bson, "_id", 0);
+
+    if (!BSON_APPEND_DOCUMENT_BEGIN(bson, "_id", &subdoc))
+        return false;
+
+    for (size_t i = 0; i < group->id_count; i++) {
+        struct rbh_range_field *field = &group->id_fields[i];
+        char onstack[XATTR_ONSTACK_LENGTH];
+        char *buffer = onstack;
+        const char *tmp_field;
+        char field_str[256];
+        char field_key[256];
+
+        tmp_field = field2str(&field->field, &buffer, sizeof(onstack));
+        if (tmp_field == NULL)
+            return false;
+
+        if (sprintf(field_str, "$%s", tmp_field) <= 0 ||
+            sprintf(field_key, "%s", tmp_field) <= 0)
+            return false;
+
+        escape_field_path(field_key);
+
+        if (!BSON_APPEND_UTF8(&subdoc, field_key, field_str))
+            return false;
+    }
+
+    return bson_append_document_end(bson, &subdoc);
+}
+
+static bool
 insert_rbh_accumulator_field(bson_t *bson, struct rbh_accumulator_field *field)
 {
     char accumulator[256];
@@ -91,40 +127,8 @@ bson_append_aggregate_group_stage(bson_t *bson, const char *key,
     if (!bson_append_document_begin(bson, key, key_length, &document))
         return false;
 
-    if (group->id_count == 0) {
-        if (!BSON_APPEND_INT32(&document, "_id", 0))
-            return false;
-    } else {
-        bson_t subdoc;
-
-        if (!BSON_APPEND_DOCUMENT_BEGIN(&document, "_id", &subdoc))
-            return false;
-
-        for (size_t i = 0; i < group->id_count; i++) {
-            struct rbh_range_field *field = &group->id_fields[i];
-            char onstack[XATTR_ONSTACK_LENGTH];
-            char *buffer = onstack;
-            const char *tmp_field;
-            char field_str[256];
-            char field_key[256];
-
-            tmp_field = field2str(&field->field, &buffer, sizeof(onstack));
-            if (tmp_field == NULL)
-                return false;
-
-            if (sprintf(field_str, "$%s", tmp_field) <= 0 ||
-                sprintf(field_key, "%s", tmp_field) <= 0)
-                return false;
-
-            escape_field_path(field_key);
-
-            if (!BSON_APPEND_UTF8(&subdoc, field_key, field_str))
-                return false;
-        }
-
-        if (!bson_append_document_end(&document, &subdoc))
-            return false;
-    }
+    if (!insert_group_id_fields(&document, group))
+        return false;
 
     for (size_t i = 0; i < group->acc_count; i++) {
         struct rbh_accumulator_field *field = &group->acc_fields[i];
