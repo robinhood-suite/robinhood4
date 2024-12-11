c11adba9140a275082c211af002dec4831b5332d
VALERI Yoann <yoann.valeri@cea.fr>
2024-12-11 11:18:49 +0100

    tests: add test for the report count
    Change-Id: I8a418f63330c85dab142a5e69ba6a30baf523bf9
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1205555
    Reviewed-by: Gauthier Evraerd <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

---
+++ rbh-report/tests/test_count.bash
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+regular=32768
+directory=16384
+
+test_count()
+{
+    for i in {0..25}; do
+        mkdir dir$i
+        truncate --size $i dir$i/file$i
+    done
+
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    rbh_report "rbh:mongo:$testdb" --output "count()" |
+        difflines "53"
+
+    rbh_report "rbh:mongo:$testdb" --group-by "statx.type" \
+                                   --output "count()" |
+        difflines "$directory: 27" "$regular: 26"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_count)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
