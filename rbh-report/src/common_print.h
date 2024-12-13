be11fd4b1622e0fec9deb1b074abb0d9b53610b6
VALERI Yoann <yoann.valeri@cea.fr>
2024-12-13 10:59:24 +0100

    report: change dump functions to write to buffer
    This patch changes the `dump_value` function to instead write to a
    buffer. This will allow either printing the value or getting its size,
    which will be necessary for pretty printing.
    
    Moreover, we separate the `dump_value` function from the other ones, as
    the latter are more CSV oriented, while the former is more generic and
    will be used by other types of printer.
    
    Change-Id: Id52c794c09f0e0ea6f991ddc04cb1022710deed0
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1205869
    Reviewed-by: Gauthier Evraerd <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

---
+++ rbh-report/src/common_print.h
@@ -0,0 +1,16 @@
+/* This file is part of Robinhood 4
+ * Copyright (C) 2024 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_REPORT_COMMON_PRINT_H
+#define RBH_REPORT_COMMON_PRINT_H
+
+#include <robinhood/value.h>
+
+int
+dump_value(const struct rbh_value *value, char *buffer);
+
+#endif
