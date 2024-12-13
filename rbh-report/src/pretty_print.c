f276a4d1ce81d0dd4cc35140e27a4aee557a5791
VALERI Yoann <yoann.valeri@cea.fr>
2024-12-13 13:52:35 +0100

    report: pretty print the headers
    This patch adds the pretty printing of the headers by using the columns
    structure.
    
    Change-Id: I40ff266be0905edd332eb3f4013251a766f012c4
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-report/src/pretty_print.c
+++ rbh-report/src/pretty_print.c
@@ -12,6 +12,57 @@
 #include "columns.h"
 #include "report.h"
 
+static int
+pretty_print_padded_value(int max_length, const struct rbh_value *value)
+{
+    (void) max_length;
+    (void) value;
+
+    return 0;
+}
+
+static int
+pretty_print_headers(struct result_columns *columns, bool print_id)
+{
+    int written = 0;
+
+    if (!print_id)
+        goto skip_id;
+
+    for (int i = 0; i < columns->id_count; i++) {
+        struct rbh_value value = {
+            .type = RBH_VT_STRING,
+            .string = columns->id_columns[i].header,
+        };
+
+        written += pretty_print_padded_value(columns->id_columns[i].length,
+                                             &value);
+
+        if (i < columns->id_count - 1)
+            written += printf("|");
+    }
+
+    written += printf("||");
+
+skip_id:
+    for (int i = 0; i < columns->output_count; i++) {
+        struct rbh_value value = {
+            .type = RBH_VT_STRING,
+            .string = columns->output_columns[i].header,
+        };
+
+        written += pretty_print_padded_value(columns->output_columns[i].length,
+                                             &value);
+
+        if (i < columns->output_count - 1)
+            written += printf("|");
+    }
+
+    printf("\n");
+
+    return written;
+}
+
 void
 pretty_print_results(struct rbh_value_map *result_maps, int count_results,
                      const struct rbh_group_fields group,
@@ -34,4 +85,6 @@ pretty_print_results(struct rbh_value_map *result_maps, int count_results,
 
         check_columns_lengthes(id_map, group, output_map, columns);
     }
+
+    pretty_print_headers(columns, id_map != NULL);
 }
