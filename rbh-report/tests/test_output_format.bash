50ae61158a07af5521e20dc4a262789c0da220eb
VALERI Yoann <yoann.valeri@cea.fr>
2024-12-17 10:54:42 +0100

    test: add test for report's pretty print
    Change-Id: I485767d3aa97027b1fca4ef34942c9c7a9aafb41
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-report/tests/test_output_format.bash
+++ rbh-report/tests/test_output_format.bash
@@ -15,7 +15,7 @@ test_dir=$(dirname $(readlink -e $0))
 
 main_user_id="$(id -u)"
 
-test_format_multi_group_and_rsort()
+complete_setup()
 {
     mkdir first_user_dir
     cd first_user_dir
@@ -38,9 +38,14 @@ test_format_multi_group_and_rsort()
     cd ..
 
     useradd -MN fake_user || true
-    local fake_user_id="$(id -u fake_user)"
+    fake_user_id="$(id -u fake_user)"
     chown -R fake_user: second_user_dir
     userdel fake_user || true
+}
+
+test_format_multi_group_and_rsort()
+{
+    complete_setup
 
     rbh_sync "rbh:posix:first_user_dir" "rbh:mongo:$testdb"
     rbh_sync "rbh:posix:second_user_dir" "rbh:mongo:$testdb"
@@ -81,11 +86,66 @@ test_format_multi_group_and_rsort()
                   "$main_user_id,fifo: 0"
 }
 
+check_output_has_line()
+{
+    local output="$1"
+    local line_number="$2"
+    shift 2
+
+    # Get the n-th line from the complete output and remove every sequence of
+    # more than 1 '|'
+    local line="$(echo "$output" | sed -n "$line_number{p;q}" | tr -s '|')"
+    local count=0
+
+    # Then check for each argument is that argument is equal to a part of the
+    # line, in order
+    for string in "$@"; do
+        count=$(( count + 1 ))
+
+        echo "$line" | cut -d"|" -f$count | grep "$string"
+    done
+}
+
+test_format_pretty_print()
+{
+    complete_setup
+
+    rbh_sync "rbh:posix:first_user_dir" "rbh:mongo:$testdb"
+    rbh_sync "rbh:posix:second_user_dir" "rbh:mongo:$testdb"
+
+    local first_root_size="$(stat -c %s first_user_dir)"
+    local second_root_size="$(stat -c %s second_user_dir)"
+    local first_symlink_size="$(stat -c %s first_user_dir/first_slink)"
+    local second_symlink_size="$(stat -c %s second_user_dir/second_slink)"
+
+    local output="$(rbh_report "rbh:mongo:$testdb" --pretty-print \
+                        --group-by "statx.uid,statx.type" \
+                        --output "sum(statx.size)")"
+
+    check_output_has_line "$output" 1 "statx.uid" "statx.type" "sum(statx.size)"
+    check_output_has_line "$output" 3 "$main_user_id" "fifo" "0"
+    check_output_has_line "$output" 4 "$main_user_id" "char" "0"
+    check_output_has_line "$output" 5 "$main_user_id" "directory" \
+                                    "$first_root_size"
+    check_output_has_line "$output" 6 "$main_user_id" "block" "0"
+    check_output_has_line "$output" 7 "$main_user_id" "file" "0"
+    check_output_has_line "$output" 8 "$main_user_id" "link" \
+                                    "$first_symlink_size"
+    check_output_has_line "$output" 9 "$fake_user_id" "fifo" "0"
+    check_output_has_line "$output" 10 "$fake_user_id" "char" "0"
+    check_output_has_line "$output" 11 "$fake_user_id" "directory" \
+                                    "$second_root_size"
+    check_output_has_line "$output" 12 "$fake_user_id" "block" "0"
+    check_output_has_line "$output" 13 "$fake_user_id" "file" "0"
+    check_output_has_line "$output" 14 "$fake_user_id" "link" \
+                                    "$second_symlink_size"
+}
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
-declare -a tests=(test_format_multi_group_and_rsort)
+declare -a tests=(test_format_multi_group_and_rsort test_format_pretty_print)
 
 tmpdir=$(mktemp --directory)
 trap -- "rm -rf '$tmpdir'" EXIT
