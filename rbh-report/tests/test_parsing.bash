914bb315d552196fbcf0ee3c278d0fb10da7052b
VALERI Yoann <yoann.valeri@cea.fr>
2024-12-18 09:46:32 +0100

    tests: add more parsing tests for rbh-report
    Change-Id: Ic67290c900722cf1f3352e0b3d724b4002ba7831
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-report/tests/test_parsing.bash
+++ rbh-report/tests/test_parsing.bash
@@ -21,29 +21,120 @@ test_parsing()
     rbh_report rbh:mongo:blob &&
         error "rbh-report with no '--output' should have failed"
 
+    return 0
+}
+
+test_parsing_output()
+{
     rbh_report rbh:mongo:blob --output &&
         error "rbh-report with '--output' but no string should have failed"
 
     rbh_report rbh:mongo:blob --output "," &&
         error "rbh-report with invalid output string should have failed"
 
-    rbh_report rbh:mongo:blob --output "sum(something" &&
+    rbh_report rbh:mongo:blob --output "sum" &&
+        error "rbh-report with output string missing field should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(" &&
         error "rbh-report with output string missing closing parenthesis" \
               "should have failed"
 
-    rbh_report rbh:mongo:blob --output "blob(statx.size)" &&
-        error "rbh-report with output string and invalid accumulator" \
+    rbh_report rbh:mongo:blob --output "sum(something" &&
+        error "rbh-report with output string missing closing parenthesis" \
               "should have failed"
 
     rbh_report rbh:mongo:blob --output "sum(something)" &&
         error "rbh-report with output string and invalid field" \
               "should have failed"
 
+    rbh_report rbh:mongo:blob --output "blob(statx.size)" &&
+        error "rbh-report with output string and invalid accumulator" \
+              "should have failed"
+
     rbh_report rbh:mongo:blob --output "sum(statx.size)," &&
+        error "rbh-report with invalid output string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size,)" &&
         error "rbh-report with invalid output string should have failed"
 
     rbh_report rbh:mongo:blob --output ",sum(statx.size)" &&
         error "rbh-report with invalid output string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size,statx.type)" &&
+        error "rbh-report with invalid output string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size),statx.type" &&
+        error "rbh-report with two output components but no accumulator on" \
+              "the second one should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size),blob(statx.type)" &&
+        error "rbh-report with two output components but invalid accumulator" \
+              "on the second one should have failed"
+
+    return 0
+}
+
+test_parsing_group()
+{
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" --group &&
+        error "rbh-report with '--group' but no string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" --group "," &&
+        error "rbh-report with invalid group string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" --group "blob" &&
+        error "rbh-report with invalid field in group string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type," &&
+        error "rbh-report with invalid comma in group string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group ",statx.type" &&
+        error "rbh-report with invalid comma in group string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type,blob" &&
+        error "rbh-report with invalid field in group string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type,statx.uid," &&
+        error "rbh-report with invalid comma in group string should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type[,statx.uid" &&
+        error "rbh-report with missing closing bracket should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type[],statx.uid" &&
+        error "rbh-report with empty brackets should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type[,statx.uid,]" &&
+        error "rbh-report with brackets containing invalid values" \
+              "should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type[statx.uid]" &&
+        error "rbh-report with brackets containing invalid values" \
+              "should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type[;],statx.uid" &&
+        error "rbh-report with brackets containing semi-colon but no values" \
+              "should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type[10;],statx.uid" &&
+        error "rbh-report with invalid values in brackets should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type[;10],statx.uid" &&
+        error "rbh-report with invalid values in brackets should have failed"
+
+    rbh_report rbh:mongo:blob --output "sum(statx.size)" \
+                              --group "statx.type[10;blob],statx.uid" &&
+        error "rbh-report with invalid values in brackets should have failed"
 
     return 0
 }
@@ -52,7 +143,7 @@ test_parsing()
 #                                     MAIN                                     #
 ################################################################################
 
-declare -a tests=(test_parsing)
+declare -a tests=(test_parsing test_parsing_group test_parsing_output)
 
 tmpdir=$(mktemp --directory)
 trap -- "rm -rf '$tmpdir'" EXIT
