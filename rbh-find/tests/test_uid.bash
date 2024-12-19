a6033841589599903f3f129c2abd85e2986ab1ca
Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
2024-12-19 13:27:33 +0100

    find: add `-uid` filter
    This filter allows one to select files following their owner UIDs.
    
    Change-Id: I0173e5fbdf74474dc75b5f9e228e8fc47b5d85e0
    Signed-off-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

---
+++ rbh-find/tests/test_uid.bash
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_equal()
+{
+    touch "my_file"
+    touch "your_file"
+
+    useradd -MU you
+    my_id=$(id -u)
+    your_id=$(id -u you)
+    chown you "your_file"
+    userdel you
+
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    rbh_find "rbh:mongo:$testdb" -uid $my_id | sort | difflines "/" "/my_file"
+    rbh_find "rbh:mongo:$testdb" -uid $your_id | sort | difflines "/your_file"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_equal)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
