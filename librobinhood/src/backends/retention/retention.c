ae4e00b46c2e6e9e52c5d54390b7abfc7d3df405
Guillaume Courrier <guillaume.courrier@cea.fr>
2024-12-21 09:09:25 -0600

    retention: add the retention plugin as a posix extension
    This patch only creates the empty retention plugin that just declares
    enough to be a loadable extension.
    
    Change-Id: I090d8cc3459844db134e4d352035da6c11b82263
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>

---
+++ librobinhood/src/backends/retention/retention.c
@@ -0,0 +1,14 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#include "retention_internals.h"
+
+int
+rbh_retention_enrich(void)
+{
+    return 0;
+}
