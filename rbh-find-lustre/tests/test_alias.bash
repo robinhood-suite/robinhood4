af19d05820d700afc942ef8b3ce01b10eaee3e51
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-01-10 10:21:34 +0100

    lfind: Added test for aliases
    This patch adds tests for alias resolution in rbh-find-lustre.
    
    Change-Id: Ie3babaf6a6ca603d23fb492a2e6f495b8eda112a
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-find-lustre/tests/test_alias.bash
@@ -0,0 +1,129 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_config_specified()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "-pool namer -layout-pattern default -stripe-count default"
+EOF
+
+    command_output=$(rbh-lfind --config test_conf.yaml --dry-run \
+                    "rbh:mongo:$testdb" --alias a1)
+    pattern="\-pool namer \-layout-pattern default \-stripe-count default"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "The command output does not match the expected output."
+    fi
+}
+
+test_config_default()
+{
+    mkdir -p /etc/robinhood4.d
+    cat > /etc/robinhood4.d/rbh_conf.yaml <<EOF
+alias:
+   a1: "-pool namer -layout-pattern default -stripe-count default"
+EOF
+
+    command_output=$(rbh-lfind --dry-run "rbh:mongo:$testdb" --alias a1)
+    pattern="\-pool namer \-layout-pattern default \-stripe-count default"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "The command failed when using the default config file."
+    fi
+    rm -rf /etc/robinhood4.d/rbh_conf.yaml
+}
+
+test_multiple_alias()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "-pool namer -layout-pattern default"
+   a2: "-print"
+EOF
+
+    command_output=$(rbh-lfind --config test_conf.yaml --dry-run \
+                    "rbh:mongo:$testdb" --alias a2 --alias a1 --alias a2)
+    pattern="\-print \-pool namer \-layout-pattern default \-print"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "Multiple alias resolution in a single command failed."
+    fi
+}
+
+test_recursive_alias()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "--alias a2"
+   a2: "--alias a3"
+   a3: "-print"
+EOF
+
+    command_output=$(rbh-lfind --config test_conf.yaml --dry-run \
+                    "rbh:mongo:$testdb" --alias a1)
+    pattern="\-print"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "Recursive alias resolution failed."
+    fi
+}
+
+test_recursive_alias_loop()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "--alias a2"
+   a2: "--alias a1"
+EOF
+
+    command_output=$(rbh-lfind --config test_conf.yaml --dry-run \
+                    "rbh:mongo:$testdb" --alias a1 2>&1 || true)
+    pattern="\[ERROR\] Infinite loop detected for alias 'a1'.Execution stopped."
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "Alias loop was not detected."
+    fi
+}
+
+test_alias_repeated()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "-pool namer -print"
+EOF
+
+    command_output=$(rbh-lfind --config test_conf.yaml --dry-run \
+                    "rbh:mongo:$testdb" --alias a1 --alias a1)
+    pattern="\-pool namer \-print \-pool namer \-print"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "The command failed when using the same alias twice."
+    fi
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_config_specified test_config_default test_multiple_alias
+                  test_recursive_alias test_recursive_alias_loop
+                  test_alias_repeated)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'; rm -rf /etc/robinhood4" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
