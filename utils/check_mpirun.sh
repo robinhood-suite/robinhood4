1b60a4ae8e780b8b5c9fd8fbc12187498d7d9901
Guillaume Courrier <guillaume.courrier@cea.fr>
2025-01-18 09:46:47 -0600

    tests: allow mpi tests to be run with less than 4 cores
    Some tests were enforcing 4 tasks per MPI jobs. This make
    the tests fail on machine with less than 4 cores. Remove
    this restriction to let mpirun use all the availables cores.
    
    Change-Id: Id3f24694a07308917b4e0cb5af8a264cf913755b
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- utils/check_mpirun.sh
+++ utils/check_mpirun.sh
@@ -7,14 +7,18 @@
 # SPDX-License-Identifer: LGPL-3.0-or-later
 
 if ! type -t module ; then
+    echo "'module' not found"
     exit 1
 fi
 
 module load mpi/openmpi-x86_64
 if [[ $? -eq 1 ]]; then
+    echo "failed to load openmpi module"
     exit 1
 fi
+
 if ! which mpirun ; then
+    echo "'mpirun' not found"
     exit 1
 fi
 
