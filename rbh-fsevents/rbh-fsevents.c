84594e4c810b4e0beafd46ff960b8e33f8ad4a84
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-02-17 12:02:51 +0100

    fsevents: Add --alias and --dry-run to fsevents command
    This patch adds the functionality to use aliases
    in the rbh-fsevents command and his associated tests.
    
    Change-Id: I7da9cc88a63f4e4133f441835ba874dbc0828863
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- rbh-fsevents/rbh-fsevents.c
+++ rbh-fsevents/rbh-fsevents.c
@@ -20,6 +20,7 @@
 #include <robinhood/uri.h>
 #include <robinhood/utils.h>
 #include <robinhood/config.h>
+#include <robinhood/alias.h>
 
 #include "deduplicator.h"
 #include "enricher.h"
@@ -67,6 +68,8 @@ usage(void)
         "    -h, --help      print this message and exit\n"
         "    -l, --lustre    consider SOURCE is an MDT name\n"
         "    -r, --raw       do not enrich changelog records (default)\n"
+        "    --alias NAME    specify an alias for the operation.\n"
+        "    --dry-run   displays the command after alias management\n"
         "\n"
         "Note that uploading raw records to a RobinHood backend will fail, they have to\n"
         "be enriched first.\n";
@@ -424,14 +427,22 @@ main(int argc, char *argv[])
             .name = "raw",
             .val = 'r',
         },
+        {
+            .name = "dry-run",
+            .has_arg = no_argument,
+            .val = 'x',
+        },
         {}
     };
     struct deduplicator_options dedup_opts = {
         .batch_size = DEFAULT_BATCH_SIZE,
     };
     char *dump_file = NULL;
-    int rc;
     char c;
+
+    /* Alias resolution */
+    import_configuration_file(&argc, &argv);
+    apply_aliases(&argc, &argv);
 
     /* Parse the command line */
     while ((c = getopt_long(argc, argv, "b:c:d:e:hnr", LONG_OPTIONS,
@@ -443,12 +454,6 @@ main(int argc, char *argv[])
 
             break;
         case 'c':
-            rc = rbh_config_open(optarg);
-            if (rc) {
-                fprintf(stderr, "Failed to open configuration file '%s'\n",
-                        optarg);
-                exit(errno);
-            }
             break;
         case 'd':
             dump_file = strdup(optarg);
@@ -471,6 +476,9 @@ main(int argc, char *argv[])
             mount_fd_exit();
             mount_fd = -1;
             break;
+        case 'x':
+            display_resolved_argv(NULL, &argc, &argv);
+            return EXIT_SUCCESS;
         case '?':
         default:
             /* getopt_long() prints meaningful error messages itself */
---
+++ rbh-fsevents/tests/lustre/test_alias.bash
@@ -0,0 +1,166 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_config_specified()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "--enrich rbh:lustre:test"
+EOF
+
+    command_output=$(rbh-fsevents --config test_conf.yaml --dry-run \
+                    --alias a1 src:lustre:test -)
+    pattern="\--enrich rbh:lustre:test"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "The command output does not match the expected output."
+    fi
+}
+
+test_config_default()
+{
+    mkdir -p /etc/robinhood4.d
+    cat > /etc/robinhood4.d/rbh_conf.yaml <<EOF
+alias:
+   a1: "--enrich rbh:lustre:test"
+EOF
+
+    command_output=$(rbh-fsevents --dry-run --alias a1 src:lustre:test -)
+    pattern="\--enrich rbh:lustre:test"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "The command failed when using the default config file."
+    fi
+    rm -rf /etc/robinhood4.d/rbh_conf.yaml
+}
+
+test_multiple_alias()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "--enrich rbh:lustre:test"
+   a2: "-r"
+EOF
+
+    command_output=$(rbh-fsevents --config test_conf.yaml --dry-run \
+                    --alias a2 --alias a1 --alias a2 src:lustre:test -)
+    pattern="\-r \--enrich rbh:lustre:test \-r"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "Multiple alias resolution in a single command failed."
+    fi
+}
+
+test_recursive_alias()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "--alias a2"
+   a2: "--alias a3"
+   a3: "--enrich rbh:lustre:test"
+EOF
+
+    command_output=$(rbh-fsevents --config test_conf.yaml --dry-run \
+                    --alias a1 src:lustre:test -)
+    pattern="\--enrich rbh:lustre:test"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "Recursive alias resolution failed."
+    fi
+}
+
+test_recursive_alias_loop()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "--alias a2"
+   a2: "--alias a1"
+EOF
+
+    command_output=$(rbh-fsevents --config test_conf.yaml --dry-run \
+                    --alias a1 src:lustre:test - 2>&1 || true)
+    pattern="\[ERROR\] Infinite loop detected for alias 'a1'.Execution stopped."
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "Alias loop was not detected."
+    fi
+}
+
+test_alias_repeated()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "-r"
+EOF
+
+    command_output=$(rbh-fsevents --config test_conf.yaml --dry-run \
+                    --alias a1 --alias a1 src:lustre:test -)
+    pattern="\-r \-r"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "The command failed when using the same alias twice."
+    fi
+}
+
+test_aliases_in_single_argument()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "-r"
+   a2: "-r"
+   a3: "-r"
+EOF
+
+    command_output=$(rbh-fsevents --config test_conf.yaml --dry-run \
+                    --alias a1,a2,a3 src:lustre:test -)
+    pattern="\-r \-r \-r"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "The command failed when using aliases separated commas"
+    fi
+}
+
+test_aliases_in_single_argument_recursive()
+{
+    cat > test_conf.yaml <<EOF
+alias:
+   a1: "-r --alias a2,a3"
+   a2: "-r"
+   a3: "-r"
+EOF
+
+    command_output=$(rbh-fsevents --config test_conf.yaml --dry-run \
+                    --alias a1 src:lustre:test -)
+    pattern="\-r \-r \-r"
+
+    if ! echo "$command_output" | grep -q "$pattern"; then
+        error "The command failed when using aliases separated commas"
+    fi
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_config_specified test_config_default test_multiple_alias
+                  test_recursive_alias test_recursive_alias_loop
+                  test_alias_repeated test_aliases_in_single_argument
+                  test_aliases_in_single_argument_recursive)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'; rm -rf /etc/robinhood4" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
