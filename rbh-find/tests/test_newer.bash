77db9211b9237394e8ec0be51182d40a2df5deae
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-02-25 10:42:42 +0100

    find: add `anewer` filter
    This filter allows one to filter entries with an atime greater than
    the mtime of a specific entry.
    
    Also, add a field to the get filter to know which value to retrieve
    from the entry targeted.
    
    Change-Id: Id66c8255b542e60794e3efa7246889615ae08c08
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- rbh-find/tests/test_newer.bash
+++ rbh-find/tests/test_newer.bash
@@ -28,7 +28,7 @@ test_newer()
         difflines "/" "/fileA"
 
     rbh_find "rbh:mongo:$testdb" -newer /fileA | sort |
-        difflines "/"
+            difflines "/"
     rbh_find "rbh:mongo:$testdb" -not -newer /fileA | sort |
         difflines "/fileA" "/fileB" "/fileC"
 
@@ -40,14 +40,43 @@ test_newer()
         difflines "/" "/fileA" "/fileB"
 
     # /file doesn't exist, it should return nothing
-    rbh_find "rbh:mongo:$testdb" -newer /file | sort | difflines
+    rbh_find "rbh:mongo:$testdb" "$1" /file | sort | difflines
+}
+
+test_anewer()
+{
+    touch fileA
+    sleep 1
+    touch -d "5 minutes ago" fileB
+    touch -d "10 minutes ago" fileC
+
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
+
+    rbh_find "rbh:mongo:$testdb" -anewer /fileC | sort |
+        difflines "/" "/fileA" "/fileB"
+    rbh_find "rbh:mongo:$testdb" -anewer /fileB | sort |
+        difflines "/" "/fileA"
+
+    rbh_find "rbh:mongo:$testdb" -anewer /fileA | sort | difflines
+    rbh_find "rbh:mongo:$testdb" -not -anewer /fileA | sort |
+        difflines "/" "/fileA" "/fileB" "/fileC"
+
+    rbh_find "rbh:mongo:$testdb" -anewer / | sort | difflines
+
+    rbh_find "rbh:mongo:$testdb" -anewer /fileB -a -anewer /fileC | sort |
+        difflines "/" "/fileA"
+    rbh_find "rbh:mongo:$testdb" -anewer /fileB -o -anewer /fileC | sort |
+        difflines "/" "/fileA" "/fileB"
+
+    # /file doesn't exist, it should return nothing
+    rbh_find "rbh:mongo:$testdb" -anewer /file | sort | difflines
 }
 
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
-declare -a tests=(test_newer)
+declare -a tests=(test_newer test_anewer)
 
 tmpdir=$(mktemp --directory)
 trap -- "rm -rf '$tmpdir'"  EXIT
