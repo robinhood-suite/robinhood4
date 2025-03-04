5d6d462d0b61db6632df206a74b5a158ea92b491
Karl ENGELBRECHT <karl.engelbrecht@cea.fr>
2025-03-04 10:59:35 +0100

    rbh-info: now displays size of mongo
    adding get_collection_size function inside mongo backend that displays
    the size of collection "entries" (later the displaying method will be
    generic).
    
    Change-Id: Ib7c4556d66fb910bbfc1b88cc12db73540d96957
    Signed-off-by: Karl ENGELBRECHT <karl.engelbrecht@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1210411
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>
    Tested-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/backends/mongo/mongo.c
+++ librobinhood/src/backends/mongo/mongo.c
@@ -792,11 +792,53 @@ mongo_backend_report(void *backend, const struct rbh_filter *filter,
      |                              get_info()                            |
      *--------------------------------------------------------------------*/
 
+int32_t
+get_collection_size(const struct mongo_backend *mongo) {
+    bson_error_t error;
+    bson_iter_t iter;
+    bson_t *command;
+    bson_t reply;
+    int32_t size;
+
+    command = BCON_NEW("collStats", BCON_UTF8("entries"));
+
+    if (mongoc_collection_command_simple(mongo->entries, command, NULL, &reply,
+                                         &error)) {
+        if (bson_iter_init(&iter, &reply) &&
+            bson_iter_find(&iter, "size")) {
+            if (BSON_ITER_HOLDS_INT32(&iter)) {
+                size = bson_iter_int32(&iter);
+                if (size >= 1073741824) //GB
+                    printf("Entries size: %.2d GB\n", size / 1073741824);
+                else if (size >= 1048576) //MB
+                    printf("Entries size: %.2d MB\n", size / 1048576);
+                else if (size >= 1024) //KB
+                    printf("Entries size: %.2d KB\n", size / 1024);
+                else {
+                    printf("Entries size: %d bytes\n", size);
+                }
+            }
+        }
+    }
+
+    bson_destroy(command);
+    bson_destroy(&reply);
+    return size;
+}
+
+
 void
-mongo_backend_get_info(__attribute__((unused)) void *backend,
-                       __attribute__((unused)) enum rbh_info info)
+mongo_backend_get_info(void *backend, enum rbh_info info)
 {
-    return;
+    struct mongo_backend *mongo = backend;
+
+    switch (info){
+    case RBH_INFO_SIZE:
+        get_collection_size(mongo);
+        break;
+    default:
+        return;
+    }
 }
 
     /*--------------------------------------------------------------------*
