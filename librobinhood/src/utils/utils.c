4ac40c030bbad2543fcfdd52d8abb4e094b5934e
Karl Engelbrecht <karl.engelbrecht@cea.fr>
2025-03-07 10:53:39 +0100

    utils: adding a human readable size_printer
    This patch  provide a tool that takes an int representing the size and
    prints it in PB,TB,GB,etc., depending on its value.
    
    Change-Id: I83bcf641b4b335712c5ab89f47548547b5c73e0a
    Signed-off-by: Karl Engelbrecht <karl.engelbrecht@cea.fr>

--- librobinhood/src/utils/utils.c
+++ librobinhood/src/utils/utils.c
@@ -47,7 +47,8 @@ str2uint64_t(const char *input, uint64_t *result)
     return 0;
 }
 
-int count_char_separated_values(const char *str, char character)
+int
+count_char_separated_values(const char *str, char character)
 {
     int count;
 
@@ -72,4 +73,30 @@ int count_char_separated_values(const char *str, char character)
     }
 
     return count;
+}
+
+void
+size_printer(int64_t size)
+{
+    const int64_t EB = 1LL << 60;
+    const int64_t PB = 1LL << 50;
+    const int64_t TB = 1LL << 40;
+    const int64_t GB = 1LL << 30;
+    const int64_t MB = 1LL << 20;
+    const int64_t KB = 1LL << 10;
+
+    if (size >= EB)
+        printf("Size: %.2f EB\n", (double)size / EB);
+    else if (size >= PB)
+        printf("Size: %.2f PB\n", (double)size / PB);
+    else if (size >= TB)
+        printf("Size: %.2f TB\n", (double)size / TB);
+    else if (size >= GB)
+        printf("Size: %.2f GB\n", (double)size / GB);
+    else if (size >= MB)
+        printf("Size: %.2f MB\n", (double)size / MB);
+    else if (size >= KB)
+        printf("Size: %.2f KB\n", (double)size / KB);
+    else
+        printf("Size: %ld bytes\n", size);
 }
