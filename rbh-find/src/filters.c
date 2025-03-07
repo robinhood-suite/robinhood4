cd17b5d9c946624fbc3e90ff8b42052c19f55115
VALERI Yoann <yoann.valeri@cea.fr>
2025-03-07 16:39:12 +0100

    find: add `-inum` filter
    This filter allows one to filter entries based on a given inode number.
    
    Change-Id: I3b341c376c665c27150e0f91d7754818685b4a83
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-find/src/filters.c
+++ rbh-find/src/filters.c
@@ -48,6 +48,7 @@ static const struct rbh_filter_field predicate2filter_field[] = {
     [PRED_CTIME]    = {.fsentry = RBH_FP_STATX, .statx = RBH_STATX_CTIME_SEC},
     [PRED_ILNAME]   = {.fsentry = RBH_FP_SYMLINK},
     [PRED_INAME]    = {.fsentry = RBH_FP_NAME},
+    [PRED_INUM]     = {.fsentry = RBH_FP_STATX, .statx = RBH_STATX_INO},
     [PRED_GID]      = {.fsentry = RBH_FP_STATX, .statx = RBH_STATX_GID},
     [PRED_GROUP]    = {.fsentry = RBH_FP_STATX, .statx = RBH_STATX_GID},
     [PRED_LINKS]    = {.fsentry = RBH_FP_STATX, .statx = RBH_STATX_NLINK},
