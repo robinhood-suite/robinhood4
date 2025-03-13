ca56910c0af2face7f8e3721c946b16a3e562cb8
Alban RIO <Alban.RIO@cea.fr>
2025-03-13 14:38:47 +0000

    s3: add s3 wrapper
    Add the wrapper library to be able to call the aws sdk cpp API from
    the rbh s3 backend
    
    Change-Id: I627a0352992f968254a580a491b2d0ff8f010345
    Signed-off-by: Alban RIO <Alban.RIO@cea.fr>

---
+++ librobinhood/src/backends/s3/s3_wrapper.cpp
@@ -0,0 +1,43 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#include <iostream>
+#include <aws/core/Aws.h>
+#include <aws/s3/S3Client.h>
+#include <aws/core/auth/AWSCredentialsProvider.h>
+
+#include <memory>
+#include <string>
+
+#include "s3_wrapper.h"
+
+#define SIGNING_POLICY Aws::Client::AWSAuthV4Signer::PayloadSigningPolicy
+#define S3CLIENT Aws::S3::S3Client
+
+std::unique_ptr<S3CLIENT> s3_client_ptr = nullptr;
+
+void
+s3_init_api(const char *address, const char *username, const char *password)
+{
+    Aws::SDKOptions options;
+    Aws::InitAPI(options);
+    Aws::S3::S3ClientConfiguration config;
+    config.endpointOverride = address;
+    config.scheme = Aws::Http::Scheme::HTTPS;
+    Aws::Auth::AWSCredentials credentials(username, password);
+    s3_client_ptr = std::unique_ptr<S3CLIENT>(new S3CLIENT(credentials,
+                                              config, SIGNING_POLICY::Never,
+                                              false));
+}
+
+void
+s3_destroy_api()
+{
+    s3_client_ptr.reset();
+    Aws::SDKOptions options;
+    Aws::ShutdownAPI(options);
+}
