1b0c927c650349deb81edce885e93ed70bf4edaa
Alban RIO <Alban.RIO@cea.fr>
2025-03-13 14:16:40 +0000

    s3: Add backend basic functions
    Add the s3 backend basic functions such as backend_new and
    plugin_destroy
    
    Change-Id: If2c78ab11f27e16e340aa00318c388a4b863d2be
    Signed-off-by: Alban RIO <Alban.RIO@cea.fr>

---
+++ librobinhood/src/plugins/s3/s3.c
@@ -0,0 +1,87 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <error.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "robinhood/backends/s3.h"
+#include "robinhood/config.h"
+
+#include "s3_wrapper.h"
+
+/*----------------------------------------------------------------------------*
+ |                                  s3_backend                                |
+ *----------------------------------------------------------------------------*/
+
+struct s3_backend {
+    struct rbh_backend backend;
+};
+
+    /*--------------------------------------------------------------------*
+     |                             destroy()                              |
+     *--------------------------------------------------------------------*/
+
+static void
+s3_backend_destroy(void *backend)
+{
+    struct s3_backend *s3 = backend;
+
+    free(s3);
+    s3_destroy_api();
+}
+
+static const struct rbh_backend_operations S3_BACKEND_OPS = {
+    .destroy = s3_backend_destroy,
+};
+
+static const struct rbh_backend S3_BACKEND = {
+    .name = RBH_S3_BACKEND_NAME,
+    .ops = &S3_BACKEND_OPS,
+};
+
+struct rbh_backend *
+rbh_s3_backend_new(__attribute__((unused)) const char *path,
+                   struct rbh_config *config)
+{
+    struct rbh_value value = { 0 };
+    struct s3_backend *s3;
+    const char *password;
+    const char *address;
+    const char *user;
+
+    s3 = malloc(sizeof(*s3));
+    if (s3 == NULL)
+        return NULL;
+
+    load_rbh_config(config);
+
+    rbh_config_find("RBH_S3/S3_PASSWORD", &value, RBH_VT_STRING);
+    password = value.string;
+    rbh_config_find("RBH_S3/S3_ADDRESS", &value, RBH_VT_STRING);
+    address = value.string;
+    rbh_config_find("RBH_S3/S3_USER", &value, RBH_VT_STRING);
+    user = value.string;
+
+    if (!address || !user || !password){
+        rbh_backend_error_printf("could not retrieve the s3 setup variables from
+                                 the config file");
+        return NULL;
+    }
+
+    s3_init_api(address, user, password);
+    s3->backend = S3_BACKEND;
+
+    return &s3->backend;
+}
