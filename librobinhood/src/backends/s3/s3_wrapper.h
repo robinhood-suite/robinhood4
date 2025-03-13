7803c202c7af82bed87f3d22dec26148ed6bcaa3
Alban RIO <Alban.RIO@cea.fr>
2025-03-13 14:38:47 +0000

    s3: add s3 wrapper
    Add the wrapper library to be able to call the aws sdk cpp API from
    the rbh s3 backend
    
    Change-Id: I627a0352992f968254a580a491b2d0ff8f010345
    Signed-off-by: Alban RIO <Alban.RIO@cea.fr>

---
+++ librobinhood/src/backends/s3/s3_wrapper.h
@@ -0,0 +1,47 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef S3_WRAPPER_H_
+#define S3_WRAPPER_H_
+
+/**
+ * Since the AWS SDK API for s3 has been written in cpp, this wrapper has
+ * been made to make it compatible with the rbh backend in c
+ */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /**
+     * Initialize the aws client
+     *
+     * Init_API is called to initialize the AWS API and generate a client using
+     * the credentials given
+     *
+     * @param address   the s3 server address (ex: 127.0.0.1:9000)
+     *
+     * @param username  the s3 username
+     *
+     * @param password  the s3 password
+     */
+    void
+    Init_API(const char* address, const char* username,
+             const char* password);
+
+    /**
+     * Shutdown the AWS API
+     */
+    void
+    Destroy_API();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
