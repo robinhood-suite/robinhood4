5d18e1703fec28a2a19afbeb627fd69d9b104124
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-03-20 10:32:48 +0100

    plugin: add new operations in plugin to build filters
    This patch adds two operations in the plugin. The first,
    "rbh_plugin_check_valid_token" is to check if a plugin can handle a
    token. And the second, "rbh_plugin_build_filter" is to build a filter
    from a token.
    
    Change-Id: Ib1701a42237994f6bb7d341a995acb94e3b2737d
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- librobinhood/include/robinhood/filter.h
+++ librobinhood/include/robinhood/filter.h
@@ -87,6 +87,13 @@
 #include "robinhood/fsentry.h"
 #include "robinhood/value.h"
 
+enum rbh_parser_token {
+    RBH_TOKEN_ERROR,
+    RBH_TOKEN_UNKNOWN,
+    RBH_TOKEN_PREDICATE,
+    RBH_TOKEN_ACTION,
+};
+
 /**
  * Filter operator
  *
