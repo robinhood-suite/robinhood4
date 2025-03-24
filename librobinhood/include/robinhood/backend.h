75f574fb4420587ca983100afacf6d2a75606e9f
Karl ENGELBRECHT <karl.engelbrecht@cea.fr>
2025-03-24 15:54:30 +0100

    backend: new backend_call, insert_source
    By adding this new backend call, we later will be able to
    retrieve the name of the backent d source (for example,
    during a rbh-sync) and store it inside mongo's info collection.
    
    Change-Id: Ia4e02be6b328651c9d9c79ddefbdb73873f25f8e
    Signed-off-by: Karl ENGELBRECHT <karl.engelbrecht@cea.fr>

--- librobinhood/include/robinhood/backend.h
+++ librobinhood/include/robinhood/backend.h
@@ -260,6 +260,10 @@ struct rbh_backend_operations {
             void *backend,
             struct rbh_iterator *fsevents
             );
+    int (*insert_source)(
+            void *backend,
+            const struct rbh_value *backend_source
+            );
     struct rbh_backend *(*branch)(
             void *backend,
             const struct rbh_id *id,
@@ -480,6 +484,27 @@ rbh_backend_update(struct rbh_backend *backend, struct rbh_iterator *fsevents)
     }
 
     return backend->ops->update(backend, fsevents);
+}
+
+/**
+ * Insert the source backends in the target backend
+ *
+ * @param backend         the backend in which to insert the source
+ * @param backend_source  a rbh_value sequence containing the source backend
+ *                        names
+ *
+ * @return                0 on success, -1 on error
+ */
+static inline int
+rbh_backend_insert_source(struct rbh_backend *backend,
+                          const struct rbh_value *backend_source)
+{
+    if (backend->ops->insert_source == NULL) {
+        errno = ENOTSUP;
+        return -1;
+    }
+
+    return backend->ops->insert_source(backend, backend_source);
 }
 
 /**
