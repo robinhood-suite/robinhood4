3d5cd839a9784470d8b54e8c38929bede8c87f70
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-03-25 13:19:41 +0100

    posix: add a callback to build filters in posix plugin
    This patch adds the callback to build filter in the posix plugin. The
    old function in rbh-find is kept temporarily until rbh-find can handle
    plugins.
    
    Change-Id: I549bd51024fee7068b2d9c281c5175fbfdd54363
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- librobinhood/include/robinhood/plugins/backend.h
+++ librobinhood/include/robinhood/plugins/backend.h
@@ -10,6 +10,7 @@
 
 #include <assert.h>
 #include <error.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <errno.h>
 
@@ -214,7 +215,6 @@ rbh_plugin_check_valid_token(const struct rbh_backend_plugin *plugin,
  *                       by the plugin if necessary to skip optionnal values
  * @param need_prefetch  boolean value set by the plugin to indicate if a filter
  *                       needs to be completed
-
  *
  * @return               a pointer to newly allocated struct rbh_filter on
  *                       success, NULL on error and errno is set appropriately
