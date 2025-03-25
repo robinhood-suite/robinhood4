519cc5be3c105469d730d3cde83ee335bb9e51e9
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-03-25 13:06:48 +0100

    find: move posix filters to posix plugin
    This patch moves the posix filters from rbh-find to the posix plugin.
    The filters are temporarily keep in rbh-find until it can handle
    plugins.
    
    Change-Id: I12fba1f7ee3a92a61f635a2501ad565d0eef826c
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- rbh-find/src/utils.c
+++ rbh-find/src/utils.c
@@ -1,5 +1,5 @@
 /* This file is part of RobinHood 4
- * Copyright (C) 2019 Commissariat a l'energie atomique et aux energies
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
  *                    alternatives
  *
  * SPDX-License-Identifer: LGPL-3.0-or-later
@@ -19,35 +19,6 @@
 #include <time.h>
 
 #include "rbh-find/utils.h"
-
-const unsigned long TIME_UNIT2SECONDS[] = {
-    [TU_SECOND] = 1,
-    [TU_MINUTE] = 60,
-    [TU_HOUR] = 3600,
-    [TU_DAY] = 86400,
-};
-
-unsigned long
-str2seconds(enum time_unit unit, const char *string)
-{
-    unsigned long delta;
-    char *endptr;
-
-    delta = strtoul(string, &endptr, 10);
-    if ((errno == ERANGE && delta == ULONG_MAX) || (errno != 0 && delta == 0))
-        return delta;
-    if (*endptr != '\0') {
-        errno = EINVAL;
-        return 0;
-    }
-
-    if (ULONG_MAX / TIME_UNIT2SECONDS[unit] < delta) {
-        errno = ERANGE;
-        return ULONG_MAX;
-    }
-
-    return delta * TIME_UNIT2SECONDS[unit];
-}
 
 const char *
 time_from_timestamp(const time_t *time)
