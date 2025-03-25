025c563cd9fdf7d4eb7db76650b8b548ed4a9477
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-03-25 13:06:48 +0100

    find: move posix filters to posix plugin
    This patch moves the posix filters from rbh-find to the posix plugin.
    The filters are temporarily keep in rbh-find until it can handle
    plugins.
    
    Change-Id: I12fba1f7ee3a92a61f635a2501ad565d0eef826c
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- rbh-find/include/rbh-find/utils.h
+++ rbh-find/include/rbh-find/utils.h
@@ -1,5 +1,5 @@
 /* This file is part of RobinHood 4
- * Copyright (C) 2019 Commissariat a l'energie atomique et aux energies
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
  *                    alternatives
  *
  * SPDX-License-Identifer: LGPL-3.0-or-later
@@ -26,27 +26,6 @@
  */
 char *
 shell2pcre(const char *shell);
-
-enum time_unit {
-    TU_SECOND,
-    TU_MINUTE,
-    TU_HOUR,
-    TU_DAY,
-};
-
-extern const unsigned long TIME_UNIT2SECONDS[];
-
-/**
- * str2seconds - convert a string into a number of seconds
- *
- * @param unit      a time unit in which to interpret \p string
- * @param string    a string representing an unsigned long
- *
- * @return          an unsigned long representing \p string converted into
- *                  seconds
- */
-unsigned long
-str2seconds(enum time_unit unit, const char *string);
 
 /**
  * time_from_timestamp - convert a timestamp to a human-readable string using
