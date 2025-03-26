a1e664c812be3199b05124a1bfde286350350fdb
VALERI Yoann <yoann.valeri@cea.fr>
2025-03-26 12:18:35 +0100

    find: remove callbacks from find and find-lustre
    This patch removes the `parse_predicate` and `predicate_or_action`
    callbacks from rbh-find, and the associated function from both rbh-find
    and rbh-find-lustre.
    
    Change-Id: I4a0605e535e4bca5872eafd00bb3370f7f677b76
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-find/include/rbh-find/find_cb.h
+++ rbh-find/include/rbh-find/find_cb.h
@@ -1,5 +1,5 @@
 /* This file is part of RobinHood 4
- * Copyright (C) 2022 Commissariat a l'energie atomique et aux energies
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
  *                    alternatives
  *
  * SPDX-License-Identifer: LGPL-3.0-or-later
@@ -41,23 +41,5 @@ find_exec_action(struct find_context *ctx,
 void
 find_post_action(struct find_context *ctx, const int index,
                  const enum action action, const size_t count);
-
-/**
- * Find parse_predicate function, see `parse_predicate_callback` in `struct
- * find_context` for more information.
- *
- * Called by rbh-find and implement GNU-find like behaviour.
- */
-struct rbh_filter *
-find_parse_predicate(struct find_context *ctx, int *arg_idx);
-
-/**
- * Find predicate_or_action function, see `pred_or_action_callback` in `struct
- * find_context` for more information.
- *
- * Called by rbh-find and implement GNU-find like behaviour.
- */
-enum command_line_token
-find_predicate_or_action(const char *string);
 
 #endif
