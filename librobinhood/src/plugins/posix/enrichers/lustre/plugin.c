d0d4769de5ba152d8eb92744687ccce8b3e52b5d
VALERI Yoann <yoann.valeri@cea.fr>
2025-03-26 15:19:37 +0100

    plugins: implement `delete_entry` for posix
    This patch implements the `delete_entry` callback for POSIX, and set it
    to NULL for Lustre, as Lustre won't do anything more than what POSIX
    does, so might as well use POSIX's callback.
    
    Change-Id: I8f8ef0f0ead5dc4e534b5f0d951232fac9f7bf23
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/plugins/posix/enrichers/lustre/plugin.c
+++ librobinhood/src/plugins/posix/enrichers/lustre/plugin.c
@@ -24,6 +24,7 @@ const struct rbh_posix_extension RBH_BACKEND_EXTENDS(POSIX, LUSTRE) = {
         .max_version = RBH_POSIX_BACKEND_VERSION,
         .check_valid_token = rbh_lustre_check_valid_token,
         .build_filter = rbh_lustre_build_filter,
+        .delete_entry = NULL,
     },
     .enrich = rbh_lustre_enrich,
 };
