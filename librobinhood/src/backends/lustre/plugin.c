333d59996773c5c8884a920495707ac2dec6e99a
VALERI Yoann <yoann.valeri@cea.fr>
2025-03-27 10:42:09 +0100

    lustre: copy print_directive function from rbh-lfind
    This patch copies the `print_directive` callback from rbh-find-lustre to
    the Lustre and set it as the plugin's callback.
    
    Change-Id: Ibebc1ff3ccd80b0f57071991ebac2194d0f8b422
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/backends/lustre/plugin.c
+++ librobinhood/src/backends/lustre/plugin.c
@@ -19,6 +19,7 @@ static const struct rbh_backend_plugin_operations LUSTRE_BACKEND_PLUGIN_OPS = {
     .new = rbh_lustre_backend_new,
     .check_valid_token = rbh_lustre_check_valid_token,
     .build_filter = rbh_lustre_build_filter,
+    .fill_entry_info = rbh_lustre_fill_entry_info,
     .delete_entry = rbh_lustre_delete_entry,
 };
 
