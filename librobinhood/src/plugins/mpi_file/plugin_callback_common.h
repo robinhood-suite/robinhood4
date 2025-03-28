ac11dc9d82f79153dc803bd430913d4af02fe6fd
VALERI Yoann <yoann.valeri@cea.fr>
2025-03-28 14:31:31 +0100

    mpi-file: add posix plugin as global variable
    This patch adds a posix plugin as a global variable to be used by the
    plugin callbacks for rbh-find. Overall, regarding those callbacks, the
    mpi-file plugin will behave exactly like the POSIX plugin, except only a
    subset of filter are allowed. Therefore, the mpi-file plugin will import
    the POSIX one, and use its (almost) all of its operations.
    
    Change-Id: I7fdc30cdfe579f9e596a336ba0c382b2ccc99ff7
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1212479
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ librobinhood/src/plugins/mpi_file/plugin_callback_common.h
@@ -0,0 +1,16 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_MPI_FILE_PLUGIN_CALLBACK_COMMON_H
+#define RBH_MPI_FILE_PLUGIN_CALLBACK_COMMON_H
+
+extern const struct rbh_backend_plugin *posix_plugin;
+
+int
+import_posix_plugin();
+
+#endif
