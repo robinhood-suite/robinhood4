94fc7f44e9105b3a06c7955702b64b4478e66628
VALERI Yoann <yoann.valeri@cea.fr>
2025-04-02 15:26:36 +0200

    common ops: add wrappers for each common_ops
    This patch adds wrappers for each function in the common_ops structure.
    
    Change-Id: Ib161ea040bf29ac09ba15f2eed784cf679a9e2c1
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/include/robinhood/plugins/common_ops.h
+++ librobinhood/include/robinhood/plugins/common_ops.h
@@ -72,4 +72,69 @@ struct rbh_pe_common_operations {
     int (*delete_entry)(struct rbh_fsentry *fsentry);
 };
 
+/**
+ * The following functions are wrappers around the functions defined in the
+ * common_ops structure defined above. They will check the function requested
+ * is defined and call them if needed, or return an error otherwise.
+ *
+ * They all use the same arguments as the common_ops functions, though they
+ * add on argument, which is the common_ops structure to call.
+ *
+ * For each of their documentation, check the documentation of the common_ops
+ * functions.
+ */
+static inline enum rbh_parser_token
+rbh_pe_common_ops_check_valid_token(
+    const struct rbh_pe_common_operations *common_ops,
+    const char *token
+)
+{
+    if (common_ops->check_valid_token)
+        return common_ops->check_valid_token(token);
+
+    errno = ENOTSUP;
+    return RBH_TOKEN_ERROR;
+}
+
+static inline struct rbh_filter *
+rbh_pe_common_ops_build_filter(
+    const struct rbh_pe_common_operations *common_ops,
+    const char **argv, int argc, int *index, bool *need_prefetch
+)
+{
+    if (common_ops->build_filter)
+        return common_ops->build_filter(argv, argc, index, need_prefetch);
+
+    errno = ENOTSUP;
+    return NULL;
+}
+
+static inline int
+rbh_pe_common_ops_fill_entry_info(
+    const struct rbh_pe_common_operations *common_ops,
+    char *output, int max_length, const struct rbh_fsentry *fsentry,
+    const char *directive, const char *backend
+)
+{
+    if (common_ops->fill_entry_info)
+        return common_ops->fill_entry_info(output, max_length, fsentry,
+                                            directive, backend);
+
+    errno = ENOTSUP;
+    return -1;
+}
+
+static inline int
+rbh_pe_common_ops_delete_entry(
+    const struct rbh_pe_common_operations *common_ops,
+    struct rbh_fsentry *fsentry
+)
+{
+    if (common_ops->delete_entry)
+        return common_ops->delete_entry(fsentry);
+
+    errno = ENOTSUP;
+    return -1;
+}
+
 #endif
