b1ad03b3a514b01ad497823417f7e22c6d9ef179
VALERI Yoann <yoann.valeri@cea.fr>
2025-04-02 17:34:06 +0200

    librobinhood: remove duplicate callbacks from plugins and extensions
    Change-Id: I1979d8994fc7eeae275f14cf2716b65a686f63ab
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/plugins/posix/enrichers/lustre/plugin.c
+++ librobinhood/src/plugins/posix/enrichers/lustre/plugin.c
@@ -30,10 +30,6 @@ const struct rbh_posix_extension RBH_BACKEND_EXTENDS(POSIX, LUSTRE) = {
         .min_version = RBH_POSIX_BACKEND_VERSION,
         .max_version = RBH_POSIX_BACKEND_VERSION,
         .common_ops  = &LUSTRE_EXTENSION_COMMON_OPS,
-        .check_valid_token = rbh_lustre_check_valid_token,
-        .build_filter = rbh_lustre_build_filter,
-        .fill_entry_info = rbh_lustre_fill_entry_info,
-        .delete_entry = NULL,
     },
     .enrich = rbh_lustre_enrich,
 };
