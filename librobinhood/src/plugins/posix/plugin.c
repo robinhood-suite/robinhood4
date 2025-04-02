0d2fc486f9f4719a97014fa77d9a7b21876bf8e9
VALERI Yoann <yoann.valeri@cea.fr>
2025-04-02 17:34:06 +0200

    librobinhood: remove duplicate callbacks from plugins and extensions
    Change-Id: I1979d8994fc7eeae275f14cf2716b65a686f63ab
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1212786
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- librobinhood/src/plugins/posix/plugin.c
+++ librobinhood/src/plugins/posix/plugin.c
@@ -22,10 +22,6 @@ static const struct rbh_pe_common_operations POSIX_BACKEND_PLUGIN_COMMON_OPS = {
 
 static const struct rbh_backend_plugin_operations POSIX_BACKEND_PLUGIN_OPS = {
     .new = rbh_posix_backend_new,
-    .check_valid_token = rbh_posix_check_valid_token,
-    .build_filter = rbh_posix_build_filter,
-    .fill_entry_info = rbh_posix_fill_entry_info,
-    .delete_entry = rbh_posix_delete_entry,
 };
 
 const struct rbh_backend_plugin RBH_BACKEND_PLUGIN_SYMBOL(POSIX) = {
