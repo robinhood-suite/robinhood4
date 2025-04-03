203736b628087fae5f783654156478f6121415d2
Karl ENGELBRECHT <karl.engelbrecht@cea.fr>
2025-04-03 14:23:03 +0200

    rbh-info: rbh-info mongo-mongo backend_source test added
    Following previous patches behaviour, this test checks if
    rbh-info -b matches what stored inside mongodb.
    
    Change-Id: I0619ff39587eef9a023197eb938b5f6403306d33
    Signed-off-by: Karl ENGELBRECHT <karl.engelbrecht@cea.fr>

--- rbh-info/tests/get_info.bash
+++ rbh-info/tests/get_info.bash
@@ -9,6 +9,13 @@
 test_dir=$(dirname $(readlink -e $0))
 . $test_dir/../../utils/tests/framework.bash
 
+setup_double_db()
+{
+    # Create test databases' names
+    testdb1=$SUITE-1$test
+    testdb2=$SUITE-2$test
+}
+
 ################################################################################
 #                                    TESTS                                     #
 ################################################################################
@@ -78,16 +85,41 @@ test_collection_backend_source_posix()
     fi
 }
 
+test_collection_backend_source_mongo()
+{
+    rbh_sync "rbh:posix:." "rbh:mongo:$testdb1"
+    rbh_sync "rbh:mongo:$testdb1" "rbh:mongo:$testdb2"
+
+    local rbh_info_backend_source=$(rbh_info "rbh:mongo:$testdb2" -b)
+    local -a info_array
+
+    while IFS= read -r line; do
+        info_array+=("$line")
+    done <<< "$rbh_info_backend_source"
+
+    local mongo_backend_source=$(mongo "$testdb2" --eval " db.info.find({},
+                               {backend_source:1}).toArray()[0].backend_source")
+
+    mongo_backend_source=$(echo "$mongo_backend_source" | tr -d '[:space:][]""')
+    IFS=',' read -r -a mongo_array <<< "$mongo_backend_source"
+
+    if [[ "${info_array[@]}" != "${mongo_array[@]}" ]]; then
+        error "sources retrieved from info does not match mongo ones"
+    fi
+}
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
 declare -a tests=(test_collection_size test_collection_count
                   test_collection_avg_obj_size
-                  test_collection_backend_source_posix)
+                  test_collection_backend_source_posix
+                  test_collection_backend_source_mongo)
 
 tmpdir=$(mktemp --directory)
 trap -- "rm -rf '$tmpdir'" EXIT
 cd "$tmpdir"
 
+sub_setup=setup_double_db
 run_tests ${tests[@]}
