07ee0e1e8dde87f18f23f5791bee7d0298bbdc03
VALERI Yoann <yoann.valeri@cea.fr>
2025-04-03 10:07:23 +0200

    extension: move action.c file from lustre to retention
    This patch moves the action.c file from the Lustre extension to the
    retention extension, as the `fill_entry_info` function of the Lustre
    extension currently only defines retention directives.
    
    Change-Id: I6b484f7166a6a8fd845cabebdc5b26fcdb1370d8
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/plugins/posix/enrichers/retention/plugin.c
+++ librobinhood/src/plugins/posix/enrichers/retention/plugin.c
@@ -12,6 +12,10 @@
 #include "robinhood/backends/posix_extension.h"
 #include "robinhood/plugins/backend.h"
 
+static const struct rbh_pe_common_operations RETENTION_EXTENSION_COMMON_OPS = {
+    .fill_entry_info = rbh_retention_fill_entry_info,
+};
+
 const struct rbh_posix_extension RBH_BACKEND_EXTENDS(POSIX, RETENTION) = {
     .extension = {
         .super       = RBH_POSIX_BACKEND_NAME,
@@ -19,19 +23,7 @@ const struct rbh_posix_extension RBH_BACKEND_EXTENDS(POSIX, RETENTION) = {
         .version     = RBH_RETENTION_PLUGIN_VERSION,
         .min_version = RBH_POSIX_BACKEND_VERSION,
         .max_version = RBH_POSIX_BACKEND_VERSION,
-    },
-    .enrich         = rbh_retention_enrich,
-    .setup_enricher = rbh_retention_setup,
-};
-
-/* For backward compatibility, make the retention extend the Lustre plugin */
-const struct rbh_posix_extension RBH_BACKEND_EXTENDS(LUSTRE, RETENTION) = {
-    .extension = {
-        .super       = RBH_LUSTRE_BACKEND_NAME,
-        .name        = RBH_RETENTION_PLUGIN_NAME,
-        .version     = RBH_RETENTION_PLUGIN_VERSION,
-        .min_version = RBH_POSIX_BACKEND_VERSION,
-        .max_version = RBH_POSIX_BACKEND_VERSION,
+        .common_ops  = &RETENTION_EXTENSION_COMMON_OPS,
     },
     .enrich         = rbh_retention_enrich,
     .setup_enricher = rbh_retention_setup,
