a291121f95944d7bcf76f7a838a1b463130a3839
Karl ENGELBRECHT <karl.engelbrecht@cea.fr>
2025-04-03 11:13:59 +0200

    rbh-info: addind a test to check --backend_source
    This test checks if the backends retrieved by rbh-info -b
    command match the ones stored inside mongo.
    
    nb: this test is only for Lustre backend. Also, I assumed that
    the backends retrieved by rbh-info are in the same order as
    those in mongo. Keeping this in mind, I only check if the
    arrays match, rather than comparing arrays elemement by element.
    
    Change-Id: I06f4400d404d54ba86edc19c81fbfdd1a12ff286
    Signed-off-by: Karl ENGELBRECHT <karl.engelbrecht@cea.fr>

--- rbh-info/tests/get_info.bash
+++ rbh-info/tests/get_info.bash
@@ -56,12 +56,35 @@ test_collection_avg_obj_size()
     fi
 }
 
+test_collection_backend_source_lustre()
+{
+    rbh_sync "rbh:lustre:/mnt/lustre" "rbh:mongo:$testdb"
+
+    local rbh_info_backend_source=$(rbh_info "rbh:mongo:$testdb" -b)
+    local -a info_array
+
+    while IFS= read -r line; do
+        info_array+=("$line")
+    done <<< "$rbh_info_backend_source"
+
+    local mongo_backend_source=$(mongo "$testdb" --eval " db.info.find({},
+                               {backend_source:1}).toArray()[0].backend_source")
+
+    mongo_backend_source=$(echo "$mongo_backend_source" | tr -d '[:space:][]""')
+    IFS=',' read -r -a mongo_array <<< "$mongo_backend_source"
+
+    if [[ "${info_array[@]}" != "${mongo_array[@]}" ]]; then
+        error "sources retrieved from info does not match mongo ones"
+    fi
+}
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
 declare -a tests=(test_collection_size test_collection_count
-                  test_collection_avg_obj_size)
+                  test_collection_avg_obj_size
+                  test_collection_backend_source_lustre)
 
 tmpdir=$(mktemp --directory)
 trap -- "rm -rf '$tmpdir'" EXIT
