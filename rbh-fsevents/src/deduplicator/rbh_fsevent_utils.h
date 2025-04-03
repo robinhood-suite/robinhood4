919324eeb027e2cc6244eab350c4e50242824aae
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-04-03 13:15:47 +0200

    fsevents: update the number of children of directories
    This patch updates the number of children of directories with the
    changelog of lustre. When a file, symlink, ... is created, deleted or
    renamed the counter is incremented or decremented by generating a
    fsevent with the id of the directory.
    
    Change-Id: I60505e4de8bcaa7997cb7fc51d8a6e08d3a43806
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- rbh-fsevents/src/deduplicator/rbh_fsevent_utils.h
+++ rbh-fsevents/src/deduplicator/rbh_fsevent_utils.h
@@ -52,4 +52,8 @@ rbh_fsevent_deep_copy(struct rbh_fsevent *dst,
                       const struct rbh_fsevent *src,
                       struct rbh_sstack *stack);
 
+int
+rbh_value_deep_copy(struct rbh_value *dest, const struct rbh_value *src,
+                    struct rbh_sstack *stack);
+
 #endif
