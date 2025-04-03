db2a3e760d31a8a1f9f54ed3e4296def58c6c3be
Karl ENGELBRECHT <karl.engelbrecht@cea.fr>
2025-04-03 11:13:59 +0200

    rbh-info: adding a test to check --backend_source
    This test checks if the backends retrieved by rbh-info -b
    command match the ones stored inside mongo.
    
    nb: this test is only for Lustre backend. Also, I assumed that
    the backends retrieved by rbh-info are in the same order as
    those in mongo. Keeping this in mind, I only check if the
    arrays match, rather than comparing arrays elemement by element.
    
    Change-Id: I06f4400d404d54ba86edc19c81fbfdd1a12ff286
    Signed-off-by: Karl ENGELBRECHT <karl.engelbrecht@cea.fr>

--- rbh-info/tests/test_lustre_mongo_backend_source.bash
+++ rbh-info/tests/test_lustre_mongo_backend_source.bash
@@ -13,49 +13,6 @@ test_dir=$(dirname $(readlink -e $0))
 #                                    TESTS                                     #
 ################################################################################
 
-test_collection_size()
-{
-    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
-
-    local rbh_info_size=$(rbh_info "rbh:mongo:$testdb" -s)
-    local mongo_size=$(mongo "$testdb" --eval " db.entries.stats().size")
-
-    if [ "$rbh_info_size" != "$mongo_size" ]; then
-        error "sizes are not matching\n"
-    fi
-}
-
-test_collection_count()
-{
-    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
-
-    local rbh_info_count=$(rbh_info "rbh:mongo:$testdb" -c)
-
-    if (( $(mongo_version) < $(version_code 5.0.0) )); then
-        local mongo_count=$(mongo "$testdb" --eval "db.entries.count()")
-    else
-        local mongo_count=$(mongo "$testdb" --eval
-                            "db.entries.countDocuments()")
-    fi
-
-    if [ "$rbh_info_count" != "$mongo_count" ]; then
-        error "count are not matching\n"
-    fi
-}
-
-test_collection_avg_obj_size()
-{
-    rbh_sync "rbh:posix:." "rbh:mongo:$testdb"
-
-    local rbh_info_avg_obj_size=$(rbh_info "rbh:mongo:$testdb" -a)
-    local mongo_avg_obj_size=$(mongo "$testdb" --eval "
-                               db.entries.stats().avgObjSize")
-
-    if [ "$rbh_info_avg_obj_size" != "$mongo_avg_obj_size" ]; then
-        error "average objects size are not matching\n"
-    fi
-}
-
 test_collection_backend_source_lustre()
 {
     rbh_sync "rbh:lustre:/mnt/lustre" "rbh:mongo:$testdb"
@@ -77,13 +34,12 @@ test_collection_backend_source_lustre()
         error "sources retrieved from info does not match mongo ones"
     fi
 }
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
-declare -a tests=(test_collection_size test_collection_count
-                  test_collection_avg_obj_size
-                  test_collection_backend_source_lustre)
+declare -a tests=(test_collection_backend_source_lustre)
 
 tmpdir=$(mktemp --directory)
 trap -- "rm -rf '$tmpdir'" EXIT
