aa7d67ece1dfb14c23bdc1647e4482a5a6f68883
VALERI Yoann <yoann.valeri@cea.fr>
2025-04-07 11:38:37 +0200

    info: add `backend_source` as available info
    This patch adds a `-b` option to the rbh-info CLI that will retrieve the
    list of source backends in the given URI and print them.
    
    Change-Id: I23a770b2749f85df225363e4c04cdb55f7b53c91
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-info/rbh-info.c
+++ rbh-info/rbh-info.c
@@ -109,17 +109,19 @@ info_translate(const struct rbh_backend_plugin *plugin)
                plugin->plugin.name);
         return;
     }
+
     printf("Available info for backend '%s': \n", plugin->plugin.name);
-    if (info & RBH_INFO_AVG_OBJ_SIZE) {
-        printf("- a: give the average size of objects inside entries ");
-        printf("collection\n");
-    }
-    if (info & RBH_INFO_COUNT) {
-        printf("- c: retrieve the amount of document inside entries ");
-        printf("collection\n");
-    }
+    if (info & RBH_INFO_AVG_OBJ_SIZE)
+        printf("- a: give the average size of objects inside entries collection");
+
+    if (info & RBH_INFO_BACKEND_SOURCE)
+        printf("- b: give the backend sources of the backend\n");
+
+    if (info & RBH_INFO_COUNT)
+        printf("- c: retrieve the amount of document inside entries collection");
+
     if (info & RBH_INFO_SIZE)
-        printf("-s: size of entries collection\n");
+        printf("- s: size of entries collection\n");
 }
 
 static int
@@ -131,6 +133,8 @@ help()
         "URI arguments:\n"
         "  -a --avg_obj_size         Show the average size of objects inside "
         "a given backend\n"
+        "  -b --backend_source       Show the backend used as source for past "
+        "rbh-syncs\n"
         "  -c --count                Show the amount of document inside a "
         "given backend\n"
         "  -s --size                 Show the size of entries collection\n\n"
@@ -280,10 +284,20 @@ _get_collection_count(const struct rbh_value *value)
     printf("%ld\n", value->int64);
 }
 
+static void
+_get_backend_source(const struct rbh_value *value)
+{
+    assert(value->type == RBH_VT_SEQUENCE);
+
+    for (size_t i = 0 ; i < value->sequence.count ; i++)
+        printf("%s\n", value->sequence.values[i].string);
+}
+
 static struct rbh_info_fields INFO_FIELDS[] = {
     { "average_object_size", _get_collection_avg_obj_size },
     { "count", _get_collection_count },
     { "size", _get_collection_size },
+    { "backend_source", _get_backend_source},
 };
 
 void
@@ -293,7 +307,7 @@ print_info_fields(int flags)
     size_t field_count = sizeof(INFO_FIELDS) / sizeof(INFO_FIELDS[0]);
 
     if (info_map == NULL) {
-        printf("Failed to retrieve backend info");
+        printf("Failed to retrieve backend info.\n");
         return;
     }
 
@@ -333,12 +347,8 @@ main(int argc, char **argv)
             .val = 'a',
         },
         {
-            .name = "first_sync",
-            .val = 'f',
-        },
-        {
-            .name = "last_sync",
-            .val = 'y',
+            .name = "backend_source",
+            .val = 'b',
         },
         {}
     };
@@ -351,9 +361,18 @@ main(int argc, char **argv)
         return EINVAL;
     }
 
-    while ((option = getopt_long(argc, argv, "hlsca", LONG_OPTIONS,
+    while ((option = getopt_long(argc, argv, "hlscab", LONG_OPTIONS,
                                  NULL)) != -1) {
         switch (option) {
+        case 'a':
+            flags |= RBH_INFO_AVG_OBJ_SIZE;
+            break;
+        case 'b':
+            flags |= RBH_INFO_BACKEND_SOURCE;
+            break;
+        case 'c':
+            flags |= RBH_INFO_COUNT;
+            break;
         case 'h':
             help();
             return 0;
@@ -362,12 +381,6 @@ main(int argc, char **argv)
             return 0;
         case 's':
             flags |= RBH_INFO_SIZE;
-            break;
-        case 'c':
-            flags |= RBH_INFO_COUNT;
-            break;
-        case 'a':
-            flags |= RBH_INFO_AVG_OBJ_SIZE;
             break;
         default :
             fprintf(stderr, "Unrecognized option\n");
