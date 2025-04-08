6955f1728111b30ebb57c71060b261771b268507
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-04-08 16:22:38 +0200

    WIP rbh-policy: Provide initial code and example for policy engine
    This patch introduces a Python file containing the initial
    implementation of the policy engine command and another
    Python file that serves as a sample configuration.
    The goal is to highlight the engine's early functionality and
    clarify the interaction between the command implementation and
    a Python-based configuration in the design phase.
    
    Change-Id: I1c4d69f21abdd889f9e995596eb8add63d72f803
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-policy/config.py
@@ -0,0 +1,31 @@
+declare_fileclass(
+    name="recent_files",
+    condition=(Size >= 10) & (Last_Access > 180)
+)
+
+declare_fileclass(
+    name="large_files",
+    condition=(Size >= 50)
+)
+
+declare_policy(
+    name="archive_large_files",
+    target=(Size > 100),
+    action=archive_files,
+    parameters={"compression": "gzip"},
+    rules=[
+        {
+            "name": "very_large_files",
+            "target": (Size > 500),
+            "action": delete_files,
+            "parameters": {}
+        }
+    ]
+)
+
+declare_policy(
+    name="delete_old_files",
+    target=(Last_Access > 365) & (Size < 5),
+    action=delete_files,
+    parameters={}
+)
