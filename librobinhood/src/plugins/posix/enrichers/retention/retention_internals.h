65a667b552d5ab7abf79d204491fd1566b2208db
VALERI Yoann <yoann.valeri@cea.fr>
2025-04-16 16:49:20 +0200

    retention: implement helper callback
    This patch implements the helper callback for the retention extension,
    using part of rbh-find-lustre's helper.
    
    Change-Id: Ib594ed83857bf9c20b189d1789c090c8c0ee1acd
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/src/plugins/posix/enrichers/retention/retention_internals.h
+++ librobinhood/src/plugins/posix/enrichers/retention/retention_internals.h
@@ -17,6 +17,8 @@
 
 #include <robinhood/fsentry.h>
 
+#include <robinhood/config.h>
+
 struct entry_info;
 struct rbh_value_pair;
 struct rbh_sstack;
@@ -29,6 +31,16 @@ rbh_retention_enrich(struct entry_info *einfo, uint64_t flags,
                      struct rbh_value_pair *pairs,
                      size_t pairs_count,
                      struct rbh_sstack *values);
+
+/**
+ * The following functions are implementations of the different callbacks of the
+ * `rbh_pe_common_operations` structure. Their documentation is the same as the
+ * one given for the structure's callbacks.
+ */
+void
+rbh_retention_helper(__attribute__((unused)) const char *backend,
+                     __attribute__((unused)) struct rbh_config *config,
+                     char **predicate_helper, char **directive_helper);
 
 enum rbh_parser_token
 rbh_retention_check_valid_token(const char *token);
