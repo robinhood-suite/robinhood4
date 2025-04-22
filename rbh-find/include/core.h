abf7b71f091c28f297b52071b33b68171e1b5e2b
VALERI Yoann <yoann.valeri@cea.fr>
2025-04-22 15:08:15 +0200

    find: remove action callbacks from find's context
    Change-Id: I0344e0bcdefb878518ebdee386631a30f6fc0266
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1213866
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- rbh-find/include/core.h
+++ rbh-find/include/core.h
@@ -70,45 +70,6 @@ struct find_context {
 
     /** The command to execute on each entry and its arguments */
     char **exec_command;
-
-    /**
-     * Callback to prepare an action's execution
-     *
-     * @param ctx      find's context for this execution
-     * @param index    the command line index of the action token
-     * @param action   the type of action to prepare for
-     *
-     * @return         int corresponding to how many command line tokens were
-     *                 consumed
-     */
-    int (*pre_action_callback)(struct find_context *ctx, const int index,
-                               const enum action action);
-
-    /**
-     * Callback for executing an action
-     *
-     * @param ctx            find's context for this execution
-     * @param backend_index  which backend is being queried
-     * @param action         the type of action to execute
-     * @param fsentry        the fsentry to act on
-     *
-     * @return         1 if the action is ACT_COUNT, 0 otherwise.
-     */
-    int (*exec_action_callback)(struct find_context *ctx,
-                                size_t backend_index,
-                                enum action action,
-                                struct rbh_fsentry *fsentry);
-
-    /**
-     * Callback to finish an action's execution
-     *
-     * @param ctx      find's context for this execution
-     * @param index    the command line index of the token after the action
-     * @param action   the type of action to prepare for
-     * @param count    number of entries found with this action
-     */
-    void (*post_action_callback)(struct find_context *ctx, const int index,
-                                 const enum action action, const size_t count);
 };
 
 /**
