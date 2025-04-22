82ba2e663062856e00399cb461dc6281c2350f45
VALERI Yoann <yoann.valeri@cea.fr>
2025-04-22 15:08:15 +0200

    find: remove action callbacks from find's context
    Change-Id: I0344e0bcdefb878518ebdee386631a30f6fc0266
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-find/include/actions.h
+++ rbh-find/include/actions.h
@@ -7,6 +7,8 @@
 
 #include <robinhood/fsentry.h>
 
+#include "parser.h"
+
 struct find_context;
 
 void
@@ -15,5 +17,19 @@ fsentry_print_ls_dils(FILE *file, const struct rbh_fsentry *fsentry);
 void
 fsentry_printf_format(struct find_context *ctx, size_t backend_index,
                       const struct rbh_fsentry *fsentry);
+
+int
+find_pre_action(struct find_context *ctx, const int index,
+                const enum action action);
+
+int
+find_exec_action(struct find_context *ctx,
+                 size_t backend_index,
+                 enum action action,
+                 struct rbh_fsentry *fsentry);
+
+void
+find_post_action(struct find_context *ctx, const int index,
+                 const enum action action, const size_t count);
 
 #endif
