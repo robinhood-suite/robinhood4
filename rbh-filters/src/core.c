a6ac1c29d845a399b236b63cd50b255cdf6c692b
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-04-30 14:52:07 +0200

    filters: add the filters_context structure
    This patch introduces a new structure that contains the plugins and
    extensions used to parse and build filters. It also stores all information
    related to the filters.
    
    Change-Id: I8034a705c4392db48ec46817c3d73a393fe27ed8
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ rbh-filters/src/core.c
@@ -0,0 +1,18 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "rbh-filters/core.h"
+
+void
+filters_ctx_finish(struct filters_context *ctx)
+{
+    free(ctx->info_pe);
+}
