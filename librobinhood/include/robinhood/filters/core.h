244add5ec4a11b7a85ceda762c903c4f9f1f8d34
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-04-30 14:52:07 +0200

    filters: add the filters_context structure
    This patch introduces a new structure that contains the plugins and
    extensions used to parse and build filters. It also stores all
    information related to the filters.
    
    Change-Id: I8034a705c4392db48ec46817c3d73a393fe27ed8
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ librobinhood/include/robinhood/filters/core.h
@@ -0,0 +1,50 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef ROBINHOOD_CORE_FILTERS_H
+#define ROBINHOOD_CORE_FILTERS_H
+
+#include "robinhood/plugins/backend.h"
+
+struct rbh_plugin_or_extension {
+    bool is_plugin;
+    union {
+        const struct rbh_backend_plugin *plugin;
+        const struct rbh_plugin_extension *extension;
+    };
+};
+
+static inline const struct rbh_pe_common_operations *
+get_common_operations(struct rbh_plugin_or_extension *pe)
+{
+    if (pe->is_plugin)
+        return pe->plugin->common_ops;
+    else
+        return pe->extension->common_ops;
+}
+
+/**
+ * Filters library context
+ */
+struct filters_context {
+    /** The type of information stored in the backends */
+    struct rbh_plugin_or_extension *info_pe;
+    size_t info_pe_count;
+
+    /** If a filter needs to be completed by an information of the backend */
+    bool need_prefetch;
+};
+
+/**
+ * Destroy the `struct filters_context`
+ *
+ * @param ctx      filters context
+ */
+void
+filters_ctx_finish(struct filters_context *ctx);
+
+#endif
