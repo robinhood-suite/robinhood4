ef3bb734a03d406c53526f8c33a6ae8932382716
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-05-05 09:53:52 +0200

    filters: move parse_expression function
    This patch moves the parse_expression function to librobinhood. For
    compatibility, the function is temporarily kept in rbh-find.
    
    The function can also accept a callback as an argument to customize
    the handling of actions and sort predicates. This is necessary because
    rbh-report does not support actions, and rbh-report and rbh-find
    handle sort predicates differently.
    
    Change-Id: I5f8600f0d98573f7f8a6f1b3d55b1f547daad108
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
    Reviewed-on: https://gerrithub.io/c/robinhood-suite/robinhood4/+/1214497
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>

--- librobinhood/include/robinhood/filters/parser.h
+++ librobinhood/include/robinhood/filters/parser.h
@@ -39,4 +39,31 @@ enum command_line_token
 str2command_line_token(struct filters_context *ctx, const char *string,
                        int *pe_index);
 
+typedef void (*parse_callback)(struct filters_context *ctx,
+                               int *arg_idx, const struct rbh_filter *filter,
+                               struct rbh_filter_sort **sorts,
+                               size_t *sort_count,
+                               enum command_line_token token, void *param);
+
+/**
+ * parse_expression - parse an expression (predicates / operators / actions)
+ *
+ * @param ctx           filters context
+ * @param arg_idx       a pointer to the index of argv to start parsing at
+ * @param _filter       a filter (the part of the cli already parsed)
+ * @param sorts         an array of filtering options
+ * @param sorts_count   the size of \p sorts
+ * @param cb            parsing callback to parse specific token
+ * @param cb_param      argument for the parsing callback
+ *
+ * @return              a filter that represents the parsed expression
+ *
+ * Note this function is recursive
+ */
+struct rbh_filter *
+parse_expression(struct filters_context *ctx, int *arg_idx,
+                 const struct rbh_filter *_filter,
+                 struct rbh_filter_sort **sorts, size_t *sorts_count,
+                 parse_callback cb, void *cb_param);
+
 #endif
