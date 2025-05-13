6b83b162d2e40fe2b8e8360b6cdbef51adfafaaf
VALERI Yoann <yoann.valeri@cea.fr>
2025-05-13 14:05:06 +0200

    retention: add tests for get_retention
    Change-Id: I1fb727b8625bf36f064d5b26ef0f6eb1a2d4a4fe
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1214881
    Reviewed-by: Arnaud Gautheret <arnaud.gautheret@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- retention/test_retention.bash
+++ retention/test_retention.bash
@@ -74,11 +74,65 @@ function test_set_retention
     fi
 }
 
+function test_get_retention
+{
+    local dir="dir"
+    local file="file"
+    local symlink="symlink"
+
+    touch $file
+    mkdir $dir
+    ln -s $file $symlink
+
+    local date="$(date -d "now - 5 minutes" +%s)"
+    setfattr -n user.expires -v "$date" $dir
+    local attr="$(rbh_retention $dir)"
+    local date2attr="$(date -d "@$date")"
+    if [[ "$attr" != *"$date2attr"* ]]; then
+        error "'$date' value should have been converted to '$date2attr' for '$dir'"
+    fi
+
+    date="$(($(date -d "10 minutes" +%s) - $(date +%s)))"
+    setfattr -n user.expires -v "+$date" $dir
+    local attr="$(rbh_retention $dir)"
+    if [[ "$attr" != *"10 minute(s)"* ]]; then
+        error "'$date' value should have been converted to '10 minutes' for '$dir'"
+    fi
+
+    date="$(($(date -d "12 hours" +%s) - $(date +%s)))"
+    setfattr -n user.expires -v "+$date" $file
+    local attr="$(rbh_retention $file)"
+    if [[ "$attr" != *"12 hour(s)"* ]]; then
+        error "'$date' value should have been converted to '12 hours' for '$file'"
+    fi
+
+    date="$(($(date -d "2 days" +%s) - $(date +%s)))"
+    setfattr -n user.expires -v "+$date" $symlink
+    local attr="$(rbh_retention $symlink)"
+    if [[ "$attr" != *"2 day(s)"* ]]; then
+        error "'$date' value should have been converted to '2 days' for '$symlink'"
+    fi
+
+    date="$(($(date -d "2 days + 1 hour" +%s) - $(date +%s)))"
+    setfattr -n user.expires -v "+$date" $file
+    local attr="$(rbh_retention $file)"
+    if [[ "$attr" != *"2 day(s) and 1 hour(s)"* ]]; then
+        error "'$date' value should have been converted to '2 days and 1 hours' for '$file'"
+    fi
+
+    date="$(($(date -d "2 days + 2 hours + 5 minutes" +%s) - $(date +%s)))"
+    setfattr -n user.expires -v "+$date" $file
+    local attr="$(rbh_retention $file)"
+    if [[ "$attr" != *"2 day(s), 2 hour(s) and 5 minute(s)"* ]]; then
+        error "'$date' value should have been converted to '2 days, 2 hours and 5 minutes' for '$file'"
+    fi
+}
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
-declare -a tests=(test_parsing test_set_retention)
+declare -a tests=(test_parsing test_set_retention test_get_retention)
 
 tmpdir=$(mktemp --directory)
 trap "rm -r $tmpdir" EXIT
