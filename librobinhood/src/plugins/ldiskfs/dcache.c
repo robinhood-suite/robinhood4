98e8a053ca779b3244d1b276bca38fc31fc672ab
Guillaume Courrier <guillaume.courrier@cea.fr>
2025-06-09 08:44:49 +0200

    ldiskfs: basic iterator
    Implement the basic .next method that will simply go through
    all the elements under the 'ROOT' directory and convert them
    to a rbh_fsentry.
    
    Change-Id: I55ee57106fd0fb002f8a89f20701f91b13216d38
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- librobinhood/src/plugins/ldiskfs/dcache.c
+++ librobinhood/src/plugins/ldiskfs/dcache.c
@@ -6,6 +6,7 @@
  */
 
 #include "dcache.h"
+#include <robinhood/utils.h>
 
 #include <assert.h>
 
@@ -78,7 +79,7 @@ glist_name_cmp(gconstpointer a, gconstpointer b)
     const struct rbh_dentry *dentry = a;
     const char *name = b;
 
-    return !strncmp(dentry->name, name, dentry->namelen);
+    return strncmp(dentry->name, name, dentry->namelen);
 }
 
 static struct rbh_dentry *
