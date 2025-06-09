bcec9f3ecb6852ca2b49c7ed8cd6f9bc55e506fa
Guillaume Courrier <guillaume.courrier@cea.fr>
2025-06-09 08:44:49 +0200

    ldiskfs: add dentry cache
    This patch introduces a dentry cache that will be filled
    as the device is scanned.
    
    Change-Id: I4aa2caf0ba9371a6529e844442e6e986d1c4c531
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>

---
+++ librobinhood/src/plugins/ldiskfs/dcache.h
@@ -0,0 +1,56 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef ROBINHOOD_DCACHE_H
+#define ROBINHOOD_DCACHE_H
+
+#include <ext2fs/ext2fs.h>
+#include <glib.h>
+
+#include <robinhood/fsentry.h>
+
+struct rbh_dcache {
+    GHashTable *dentries;
+};
+
+struct rbh_dentry_xattr {
+    char *name;
+    void *value;
+    size_t value_len;
+};
+
+struct rbh_dentry {
+    ext2_ino_t ino;
+    struct ext2_inode *inode;
+    const char *name;
+    size_t namelen;
+    struct rbh_dentry *parent;
+    GList *children;
+    GList *xattrs;
+};
+
+struct rbh_dentry *
+rbh_dentry_new(ext2_ino_t ino);
+
+struct rbh_dcache *
+rbh_dcache_new(void);
+
+void
+rbh_dcache_destroy(struct rbh_dcache *dcache);
+
+struct rbh_dentry *
+rbh_dcache_find(struct rbh_dcache *dcache, ext2_ino_t ino);
+
+struct rbh_dentry *
+rbh_dcache_find_or_create(struct rbh_dcache *dcache, ext2_ino_t ino);
+
+typedef void (*rbh_dcache_cb_t)(struct rbh_fsentry *fsentry, void *udata);
+
+void
+rbh_dcache_foreach(struct rbh_dcache *dcache, rbh_dcache_cb_t cb, void *udata);
+
+#endif
