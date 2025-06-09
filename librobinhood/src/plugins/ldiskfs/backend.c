b800b2056c705f0cfda70c2f6338d9e8a48d88b0
Guillaume Courrier <guillaume.courrier@cea.fr>
2025-06-09 08:44:49 +0200

    ldiskfs: implement new/destroy operation
    For now, this only allocates and frees the struct
    ldiskfs_backend.
    
    Change-Id: Ida23b76ed8dbdb568f83ad967af04daaaa14d386
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- librobinhood/src/plugins/ldiskfs/backend.c
+++ librobinhood/src/plugins/ldiskfs/backend.c
@@ -7,16 +7,36 @@
 
 #include "internals.h"
 
+static const struct rbh_backend_operations LDISKFS_BACKEND_OPS = {
+};
+
+static const struct rbh_backend LDISKFS_BACKEND = {
+    .id   = RBH_BI_LDISKFS,
+    .name = RBH_LDISKFS_BACKEND_NAME,
+    .ops  = &LDISKFS_BACKEND_OPS,
+};
+
 struct rbh_backend *
 rbh_ldiskfs_backend_new(const struct rbh_backend_plugin *self,
                         const struct rbh_uri *uri,
                         struct rbh_config *config,
                         bool read_only)
 {
-    return NULL;
+    struct ldiskfs_backend *ldiskfs;
+
+    ldiskfs = calloc(1, sizeof(*ldiskfs));
+    if (!ldiskfs)
+        return NULL;
+
+    ldiskfs->backend = LDISKFS_BACKEND;
+
+    return &ldiskfs->backend;
 }
 
 void
 ldiskfs_backend_destroy(void *backend)
 {
+    struct ldiskfs_backend *ldiskfs = backend;
+
+    free(ldiskfs);
 }
