31beb03a1c21800b958923afc7769fac37265559
Guillaume Courrier <guillaume.courrier@cea.fr>
2025-06-09 12:42:11 +0200

    sqlite: implement .insert_metadata method
    For now, we only insert the backend source.
    
    Change-Id: I3c6b013592e399990a8ef90b67c94743ff70f860
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- librobinhood/src/plugins/sqlite/info.c
+++ librobinhood/src/plugins/sqlite/info.c
@@ -7,11 +7,151 @@
 
 #include "internals.h"
 
+static bool
+insert_source(struct sqlite_backend *sqlite,
+              const char *plugin,
+              const char **extensions,
+              size_t n_extensions)
+{
+    const char *query =
+        "insert or replace into info (id, plugin, extensions) values (1, ?, ?)";
+    const char *extensions_array;
+    struct sqlite_cursor cursor;
+    int save_errno;
+    json_t *array;
+    bool res;
+
+    array = sqlite_list2array(extensions, n_extensions);
+    if (!array)
+        return false;
+
+    extensions_array = json_dumps(array, JSON_COMPACT);
+    json_decref(array);
+    if (!extensions_array)
+        return false;
+
+    res = sqlite_cursor_setup(sqlite, &cursor) &&
+        sqlite_setup_query(&cursor, query) &&
+        sqlite_cursor_bind_string(&cursor, plugin) &&
+        sqlite_cursor_bind_string(&cursor, extensions_array) &&
+        sqlite_cursor_exec(&cursor);
+
+    save_errno = errno;
+    free((char *)extensions_array);
+    errno = save_errno;
+
+    return res;
+}
+
+static bool
+store_source(struct sqlite_backend *sqlite, const struct rbh_value *sequence)
+{
+    const char **extensions = NULL;
+    const char *plugin = NULL;
+    size_t n_extensions = 0;
+    int save_errno;
+
+    for (size_t i = 0; i < sequence->sequence.count; i++) {
+        const struct rbh_value *map = &sequence->sequence.values[i];
+        bool is_plugin;
+
+        if (map->type != RBH_VT_MAP) {
+            errno = EINVAL;
+            goto free_extensions;
+        }
+
+        is_plugin = (map->map.count == 2);
+        for (size_t j = 0; j < map->map.count; j++) {
+            const struct rbh_value_pair *attr = &map->map.pairs[j];
+
+            if (attr->value->type != RBH_VT_STRING) {
+                errno = EINVAL;
+                goto free_extensions;
+            }
+
+            if (!strcmp(attr->key, "type")) {
+                if ((is_plugin && strcmp(attr->value->string, "plugin")) ||
+                    (!is_plugin && strcmp(attr->value->string, "extension"))) {
+                    errno = EINVAL;
+                    goto free_extensions;
+                }
+
+            }
+
+            if (is_plugin && !strcmp(attr->key, "plugin")) {
+                plugin = attr->value->string;
+                break;
+            }
+
+            if (!is_plugin && !strcmp(attr->key, "extension")) {
+                if (!extensions) {
+                    extensions = malloc(sizeof(*extensions) * 16);
+                    if (!extensions)
+                        goto free_extensions;
+                }
+
+                extensions[n_extensions++] = attr->value->string;
+                break;
+            }
+        }
+    }
+
+    if (!insert_source(sqlite, plugin, extensions, n_extensions))
+        goto free_extensions;
+
+    free(extensions);
+    return true;
+
+free_extensions:
+    save_errno = errno;
+    free(extensions);
+    errno = save_errno;
+
+    return false;
+}
+
+static bool
+insert_info(void *backend, const struct rbh_value_map *map)
+{
+    for (size_t i = 0; i < map->count; i++) {
+        const struct rbh_value_pair *pair = &map->pairs[0];
+
+        if (!strcmp(pair->key, "backend_source") &&
+            pair->value->type == RBH_VT_SEQUENCE) {
+
+            if (!store_source(backend, pair->value))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+static bool
+insert_log(void *backend, const struct rbh_value_map *map)
+{
+    return true;
+}
+
 int
 sqlite_backend_insert_metadata(void *backend,
-                               const struct rbh_value_map *value,
+                               const struct rbh_value_map *map,
                                enum metadata_type type)
 {
+    switch (type) {
+    case RBH_DT_INFO:
+        if (!insert_info(backend, map))
+            return -1;
+        break;
+    case RBH_DT_LOG:
+        if (!insert_log(backend, map))
+            return -1;
+        break;
+    default:
+        errno = EINVAL;
+        return -1;
+    }
+
     return 0;
 }
 
