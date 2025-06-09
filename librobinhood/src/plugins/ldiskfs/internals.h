fef76eeef47a2df5075d4868709babad75ef1b9a
Guillaume Courrier <guillaume.courrier@cea.fr>
2025-06-09 08:44:48 +0200

    librobinhood: add skeleton for ldiskfs backend
    This backend will scan block device of a Lustre OST or MDT using libext2fs.
    
    Change-Id: I6cc1ac0b2bd5743f9c143cfea6e41f9d7033658a
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>

---
+++ librobinhood/src/plugins/ldiskfs/internals.h
@@ -0,0 +1,20 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef ROBINHOOD_LDISKFS_INTERNALS_H
+#define ROBINHOOD_LDISKFS_INTERNALS_H
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <robinhood/backends/ldiskfs.h>
+
+void
+ldiskfs_backend_destroy(void *backend);
+
+#endif
