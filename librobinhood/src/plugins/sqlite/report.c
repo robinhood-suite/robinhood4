45fae9c374d550728d6a9cd3d76226bc458dcf72
Guillaume Courrier <guillaume.courrier@cea.fr>
2025-06-09 08:44:48 +0200

    sqlite: implement empty .report
    Change-Id: I63f4dada7903c4877944bb6ffbe6bc906b06065b
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1225291
    Tested-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ librobinhood/src/plugins/sqlite/report.c
@@ -0,0 +1,30 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#include "internals.h"
+
+struct rbh_mut_iterator *
+sqlite_backend_report(void *backend, const struct rbh_filter *filter,
+                      const struct rbh_group_fields *group,
+                      const struct rbh_filter_options *options,
+                      const struct rbh_filter_output *output)
+{
+    struct sqlite_query_options query_options = {0};
+    struct sqlite_filter_where where = {0};
+
+    if (rbh_filter_validate(filter))
+        return NULL;
+
+    if (!filter2where_clause(filter, &where))
+        return false;
+
+    if (!options2sql(options, &query_options))
+        return false;
+
+    errno = ENODATA;
+    return NULL;
+}
