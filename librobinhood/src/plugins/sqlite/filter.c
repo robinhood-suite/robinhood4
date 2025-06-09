32112b78c04aa4a767f9096908a228ec036272dc
Guillaume Courrier <guillaume.courrier@cea.fr>
2025-06-09 08:44:46 +0200

    sqlite: create sqlite iterator that always return NULL
    Change-Id: I99117756e7014490e3eeaa4ed7fe3767a0dd8b83
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- librobinhood/src/plugins/sqlite/filter.c
+++ librobinhood/src/plugins/sqlite/filter.c
@@ -7,11 +7,53 @@
 
 #include "internals.h"
 
+static void *
+sqlite_iter_next(void *iterator)
+{
+    errno = ENODATA;
+    return NULL;
+}
+
+static void
+sqlite_iter_destroy(void *iterator)
+{
+    struct sqlite_iterator *iter = iterator;
+
+    free(iter);
+}
+
+static const struct rbh_mut_iterator_operations SQLITE_ITER_OPS = {
+    .next    = sqlite_iter_next,
+    .destroy = sqlite_iter_destroy,
+};
+
+static const struct rbh_mut_iterator SQLITE_ITER = {
+    .ops = &SQLITE_ITER_OPS,
+};
+
+static struct sqlite_iterator *
+sqlite_iterator_new(void)
+{
+    struct sqlite_iterator *iter;
+
+    iter = malloc(sizeof(*iter));
+    if (!iter)
+        return NULL;
+
+    iter->iter = SQLITE_ITER;
+
+    return iter;
+}
+
 struct rbh_mut_iterator *
 sqlite_backend_filter(void *backend, const struct rbh_filter *filter,
                       const struct rbh_filter_options *options,
                       const struct rbh_filter_output *output)
 {
-    errno = ENODATA;
-    return NULL;
+    struct sqlite_iterator *iter = sqlite_iterator_new();
+
+    if (!iter)
+        return NULL;
+
+    return &iter->iter;
 }
