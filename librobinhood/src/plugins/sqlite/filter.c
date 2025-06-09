e0c99a1dfac9ce14c3f0c2f703dafd6f6f21834a
Guillaume Courrier <guillaume.courrier@cea.fr>
2025-06-09 08:44:46 +0200

    sqlite: add empty .filter method
    Also, improve the error message when the .filter
    method returns NULL in rbh-find.
    
    Change-Id: I78125b362aca89835e89a816a44b5b80b1ed2a9d
    Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>

---
+++ librobinhood/src/plugins/sqlite/filter.c
@@ -0,0 +1,17 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#include "internals.h"
+
+struct rbh_mut_iterator *
+sqlite_backend_filter(void *backend, const struct rbh_filter *filter,
+                      const struct rbh_filter_options *options,
+                      const struct rbh_filter_output *output)
+{
+    errno = ENODATA;
+    return NULL;
+}
--- librobinhood/src/plugins/sqlite/internals.h
+++ librobinhood/src/plugins/sqlite/internals.h
@@ -54,6 +54,11 @@ struct sqlite_backend {
 ssize_t
 sqlite_backend_update(void *backend, struct rbh_iterator *fsevents);
 
+struct rbh_mut_iterator *
+sqlite_backend_filter(void *backend, const struct rbh_filter *filter,
+                      const struct rbh_filter_options *options,
+                      const struct rbh_filter_output *output);
+
 void
 sqlite_backend_destroy(void *backend);
 
