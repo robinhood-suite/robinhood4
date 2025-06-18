a1536a90d13c27e59d0cb89cc433e51f77b15a2e
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-06-18 13:57:57 +0200

    rbh-policy: Provide initial code for the policy engine
    This patch introduces the policy engine code.
    
    Change-Id: Ic3a92b0f051b7ac395bcd60151a5a324de945405
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1217782
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: Patrice LUCAS <patrice.lucas@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-policy/rbh-policy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# This file is part of RobinHood 4
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#            alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+if __name__ == "__main__":
+    print("Hello, I'm PE")
