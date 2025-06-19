2dfea7c98e30ddda47a4f3ae4c328fb0d27746ca
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-06-19 10:20:11 +0200

    rbh-policy: Python packaging and setting up the meson
    This patch will enable packaging and installation of
    the Python package with Meson. It will also enable
    running Python tests.
    
    Change-Id: I5ad9a6c364dc235b837dd7ccffd2be067429c297
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-policy/rbhpolicy/config/config_loader.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.9
+# This file is part of RobinHood 4
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#            alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+import os
+
+CONFIG_DIR = "/etc/robinhood4.d/"
+
+def get_config_path(fs_name):
+    """
+    Returns the path of the configuration file for the specified filesystem.
+    """
+    config_file = f"{fs_name}.py"
+    config_path = os.path.join(CONFIG_DIR, config_file)
+
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Configuration file for {fs_name} not found:"
+                                f"{config_path}")
+
+    return config_path
+
+def load_config(fs_name):
+    """Dynamically loads the configuration file"""
+    config_path = get_config_path(fs_name)
+    spec = importlib.util.spec_from_file_location("config_module", config_path)
+    config_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(config_module)
+    return config_module
