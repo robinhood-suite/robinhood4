31ce2192a1013ac8649ca8467837ab2897e23498
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-07-07 15:21:23 +0200

    find: add test for the projection
    Change-Id: I7ef3d965589c15504060bd05b1d04cbddf66b157
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ rbh-find/tests/retention/test_retention_projection.bash
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# This file is part of the RobinHood Library
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+check_printf_project()
+{
+    local printf=$1
+    local project=$2
+
+    local output="$(rbh_find --verbose "rbh:$db:$testdb" -printf "$printf")"
+
+    echo "$output" | grep "\$project" | grep "$project"
+}
+
+test_printf()
+{
+    touch file
+
+    rbh_sync "rbh:retention:." "rbh:$db:$testdb"
+
+    check_printf_project "%e\n" '"xattrs" : true'
+    check_printf_project "%E\n" '"xattrs" : true'
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_printf)
+
+tmpdir=$(mktemp --directory)
+test_user="$(get_test_user "$(basename "$0")")"
+trap -- "rm -rf '$tmpdir'; delete_test_user $test_user || true" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
