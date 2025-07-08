d48d7ed8e8cc084de680e622427e5a667fe27fa4
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-07-08 06:55:28 +0000

    librobinhood: specify inode xattr subfields in projection
    This patch updates the projection to include only the specific xattrs
    that need to be retrieved, rather than fetching all xattrs.
    This improves the performance of rbh-find, especially with the Lustre
    backend, where all information is stored as xattrs.
    
    Change-Id: Id08f6d57a6c04a0c516cb65180f89c2f3fb9e54c
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- librobinhood/src/projection.c
+++ librobinhood/src/projection.c
@@ -5,9 +5,25 @@
  * SPDX-License-Identifer: LGPL-3.0-or-later
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <error.h>
+
 #include "robinhood/fsentry.h"
 #include "robinhood/projection.h"
 #include "robinhood/statx.h"
+#include "robinhood/sstack.h"
+#include "robinhood/value.h"
+
+static __thread struct rbh_sstack *sstack;
+
+__attribute__((destructor))
+static void
+destroy_sstack(void)
+{
+    if (sstack)
+        rbh_sstack_destroy(sstack);
+}
 
 void
 rbh_projection_add(struct rbh_filter_projection *projection,
@@ -28,7 +44,30 @@ rbh_projection_add(struct rbh_filter_projection *projection,
         // TODO: handle subfields
         break;
     case RBH_FP_INODE_XATTRS:
-        // TODO: handle subfields
+        if (field->xattr) {
+            if (sstack == NULL) {
+                sstack = rbh_sstack_new((1 << 10) *
+                                        (sizeof(struct rbh_value_pair)));
+
+                if (sstack == NULL)
+                    error(EXIT_FAILURE, errno, "rbh_sstack_new");
+            }
+
+            struct rbh_value_pair pair = {
+                .key = RBH_SSTACK_PUSH(sstack, field->xattr,
+                                       strlen(field->xattr) + 1),
+                .value = NULL
+            };
+
+            if (projection->xattrs.inode.pairs == NULL) {
+                projection->xattrs.inode.pairs = RBH_SSTACK_PUSH(sstack, &pair,
+                                                                 sizeof(pair));
+                projection->xattrs.inode.count++;
+            } else {
+                value_map_insert_pair(sstack, &projection->xattrs.inode, &pair);
+            }
+        }
+
         break;
     }
 }
