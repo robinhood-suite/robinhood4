a8455ce9a33b0701f766dd0a34c5ba4f5c95aaf1
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-07-09 14:47:23 +0200

    rbh-policy: Adds configuration loading
    Adds support for loading configuration from either
    a single file or a directory. Raises an error if
    both exist or if neither is found. Configuration
    is executed into a dedicated module namespace.
    
    Change-Id: I1c570539b7fdd95084f53a3f53dc519f0c447788
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- rbh-policy/rbh-policy.py
+++ rbh-policy/rbh-policy.py
@@ -6,10 +6,15 @@
 # SPDX-License-Identifer: LGPL-3.0-or-later
 
 from rbhpolicy.config.config_loader import load_config
+import argparse
 
 if __name__ == "__main__":
-    fs_name = "fs1"
-    config = load_config(fs_name)
+    parser = argparse.ArgumentParser(description="Load configuration for a filesystem")
+    parser.add_argument('filesystem', help="Name of the filesystem to load config for")
 
-    print(f"Loaded configuration for {fs_name}: {config}")
+    args = parser.parse_args()
+    fs_name = args.filesystem
+
+    load_config(fs_name)
+    print(f"Loaded configuration for {fs_name}")
     print("Hello, I'm PE")
