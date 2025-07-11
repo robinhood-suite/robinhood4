510fd7ccb6c990d4056f91c5aebfe58af605548e
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-07-11 16:45:08 +0200

    rbh-policy: Adding the fileclass and Condition structure.
    This patch adds the python classes that
    allow you to define fileclasses and (conditions).
    
    Change-Id: I7d1ed0e916cf25104cfea66ffb1b100ae66dc6d0
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-policy/rbhpolicy/config/models/fileclass.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3.9
+# This file is part of RobinHood 4
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#            alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+from rbhpolicy.config.models.conditions import Condition
+import sys
+
+fileclasses = {}
+
+class FileClass:
+    """A named file‐class defined by a Condition."""
+    def __init__(self, name: str, condition: Condition):
+        self.name = name
+        self.condition = condition
+
+    def evaluate(self, data: dict) -> bool:
+        """Return True if data matches the file‐class condition."""
+        return self.condition.evaluate(data)
+
+    def __and__(self, other: Condition) -> Condition:
+        return self.condition & other
+
+    def __or__(self, other: Condition) -> Condition:
+        return self.condition | other
+
+    def __invert__(self) -> Condition:
+        return ~self.condition
+
+    def __repr__(self) -> str:
+        return f"FileClass({self.name})"
+
+
+def declare_fileclass(name: str, condition: Condition) -> FileClass:
+    """
+    Declare a new file‐class in the global registry and inject it
+    into the caller’s namespace so it’s directly available in config files.
+    """
+    fc = FileClass(name, condition)
+    fileclasses[name] = fc
+
+    caller_ns = sys._getframe(1).f_globals
+    caller_ns[name] = fc
+
+    return fc
