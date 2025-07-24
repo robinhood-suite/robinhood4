8d7d78d655b28b03316ada382dac287c0ad1f63f
Alban RIO <Alban.RIO@cea.fr>
2025-07-24 14:05:58 +0200

    s3: add MPI iterator
    This patch adds another iterator to the s3 plugin.
    The new iterator uses MPI to iterate through buckets
    in parallel
    
    Change-Id: I0ef786042f3ff2cb14354027b4af644b7c72e129
    Signed-off-by: Alban RIO <Alban.RIO@cea.fr>

---
+++ librobinhood/include/robinhood/backends/s3_extension.h
@@ -0,0 +1,46 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef ROBINHOOD_S3_EXTENSION_H
+#define ROBINHOOD_S3_EXTENSION_H
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <robinhood/plugin.h>
+#include <robinhood/plugins/backend.h>
+
+#include <robinhood/backends/common.h>
+#include <robinhood/iterator.h>
+#include <robinhood/sstack.h>
+
+struct rbh_value_pair;
+struct rbh_statx;
+struct entry_info;
+struct rbh_sstack;
+
+typedef struct rbh_mut_iterator *(*iter_new_t)(const char *, const char *, int);
+
+struct s3_backend {
+    struct rbh_backend backend;
+    struct s3_iterator *(*iter_new)();
+};
+
+struct rbh_s3_extension {
+    struct rbh_plugin_extension extension;
+    struct s3_iterator *(*iter_new)();
+};
+
+static inline const struct rbh_s3_extension *
+rbh_s3_load_extension(const struct rbh_plugin *plugin, const char *name)
+{
+    return (const struct rbh_s3_extension *)
+        rbh_plugin_load_extension(plugin, name);
+}
+
+#endif
---
+++ librobinhood/src/plugins/s3/iterators/mpi/iterator.c
@@ -0,0 +1,213 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <error.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <mpi.h>
+
+#include "s3_mpi_internals.h"
+#include "../../s3_internals.h"
+
+#include "robinhood/backends/s3.h"
+#include "robinhood/config.h"
+#include "robinhood/sstack.h"
+#include "robinhood/statx.h"
+
+#include "../../s3_wrapper.h"
+
+/*----------------------------------------------------------------------------*
+ |                                s3_iterator                                 |
+ *----------------------------------------------------------------------------*/
+
+void
+rbh_mpi_initialize(void)
+{
+    int initialized;
+
+    MPI_Initialized(&initialized);
+    if (!initialized) {
+        MPI_Init(NULL, NULL);
+    }
+}
+
+void
+rbh_mpi_finalize(void)
+{
+    int initialized;
+    int finalized;
+
+    /* Prevents finalizing twice MPI if we use two backends with MPI */
+    MPI_Initialized(&initialized);
+    MPI_Finalized(&finalized);
+
+    if (initialized && !finalized) {
+        MPI_Finalize();
+    }
+}
+
+static void
+s3_iter_destroy(void *iterator)
+{
+    struct s3_iterator *s3_iter = iterator;
+
+    rbh_sstack_destroy(s3_iter->values);
+    if (s3_iter->obj_data.list != NULL)
+        s3_delete_list(s3_iter->obj_data.length, s3_iter->obj_data.list);
+
+    s3_delete_list(s3_iter->bkt_data.length, s3_iter->bkt_data.list);
+    rbh_mpi_finalize();
+
+    free(s3_iter);
+}
+
+static const struct rbh_mut_iterator_operations S3_ITER_OPS = {
+    .next = s3_iter_next,
+    .destroy = s3_iter_destroy,
+};
+
+static const struct rbh_mut_iterator S3_ITER = {
+    .ops = &S3_ITER_OPS,
+};
+
+int get_sub_array_bounds(int array_size, int num_chunks, int chunk_index,
+                          int *start, int *end)
+{
+    if (array_size < num_chunks) {
+        num_chunks = array_size;
+    }
+
+    if (chunk_index >= array_size || array_size == 0)
+        return -1;
+
+    int base_chunk_size = array_size / num_chunks;
+    int remainder = array_size % num_chunks;
+
+    if (chunk_index < remainder) {
+        *start = chunk_index * (base_chunk_size);
+        *end = *start + base_chunk_size - 1;
+
+    } else {
+        *start = chunk_index * base_chunk_size + remainder;
+        *end = *start + base_chunk_size - 1;
+    }
+
+    return 0;
+}
+
+static struct s3_iterator *
+s3_mpi_iterator_new()
+{
+    struct s3_iterator *s3_iter = NULL;
+    char *sub_string = malloc(64); //Max size of an S3 bucket name
+    char **temp_array;
+    char **sub_array;
+    int arr_start;
+    int rcv_size;
+    int mpi_rank;
+    int mpi_size;
+    int sub_size;
+    int arr_end;
+    int rc;
+
+    s3_iter = malloc(sizeof(*s3_iter));
+    if (s3_iter == NULL)
+        return NULL;
+
+    rbh_mpi_initialize();
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+
+    if (mpi_rank == 0) {
+        s3_get_bucket_list(&s3_iter->bkt_data.length, &s3_iter->bkt_data.list);
+        s3_iter->obj_data.list = NULL;
+
+        if (s3_iter->bkt_data.length == -1) {
+            free(s3_iter);
+            return NULL;
+        }
+
+        for (int i = 1; i < mpi_size; i++) {
+            rc = get_sub_array_bounds(s3_iter->bkt_data.length, mpi_size, i,
+                                      &arr_start, &arr_end);
+            if (rc == -1) {
+                sub_size = 0;
+                MPI_Send(&sub_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+            } else {
+                sub_size = arr_end - arr_start + 1;
+                MPI_Send(&sub_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+                for (size_t j = arr_start; j <= arr_end; j++) {
+                    MPI_Send(s3_iter->bkt_data.list[j],
+                             strlen(s3_iter->bkt_data.list[j]) + 1,
+                    MPI_CHAR, i, 0, MPI_COMM_WORLD);
+                }
+            }
+        }
+        rc = get_sub_array_bounds(s3_iter->bkt_data.length, mpi_size, 0,
+                                  &arr_start, &arr_end);
+    if (rc == 0) {
+            sub_size = arr_end - arr_start + 1;
+            temp_array = (char**)malloc(sub_size * sizeof(char*));
+            memcpy(temp_array ,s3_iter->bkt_data.list,
+                   sub_size * sizeof(char*));
+
+            s3_iter->bkt_data.list = temp_array;
+            s3_iter->bkt_data.length = sub_size;
+    } else {
+            s3_iter->bkt_data.length = 0;
+    }
+
+
+    } else {
+        MPI_Recv(&rcv_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+
+        sub_array = (char**)malloc(rcv_size * sizeof(char*));
+
+        for (int i = 0; i < rcv_size; i++) {
+            sub_array[i] = (char*)malloc(64 * sizeof(char));
+            MPI_Recv(sub_string, 64, MPI_CHAR, 0, 0, MPI_COMM_WORLD,
+                     MPI_STATUS_IGNORE);
+            strcpy(sub_array[i], sub_string);
+        }
+
+        s3_iter->bkt_data.list = sub_array;
+        s3_iter->bkt_data.length = rcv_size;
+    }
+    if (s3_iter->bkt_data.length > 0)
+        s3_get_object_list(s3_iter->bkt_data.list[0], &s3_iter->obj_data.length,
+                           &s3_iter->obj_data.list);
+
+    s3_iter->obj_data.current_id = -1;
+    s3_iter->bkt_data.current_id = 0;
+    s3_iter->iterator = S3_ITER;
+
+    s3_iter->values = rbh_sstack_new(1 << 10);
+    if (s3_iter->values == NULL)
+        return NULL;
+
+    return (struct s3_iterator *)s3_iter;
+}
+
+/*----------------------------------------------------------------------------*
+ |                                  s3_backend                                |
+ *----------------------------------------------------------------------------*/
+
+struct s3_iterator *
+rbh_s3_mpi_iter_new()
+{
+    return s3_mpi_iterator_new();
+}
