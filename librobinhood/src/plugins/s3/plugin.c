08d0a0c2db6d5d22053fff3429d8dead8c9f92de
Alban RIO <Alban.RIO@cea.fr>
2025-07-28 16:41:04 +0200

    s3: add filter parser
    Adds a parser to check the validity of a
    token for the s3 plugin
    
    Change-Id: Iac8a579d5d13bbcfacc8ab3ebd866cedbefd3801
    Signed-off-by: Alban RIO <Alban.RIO@cea.fr>

--- librobinhood/src/plugins/s3/plugin.c
+++ librobinhood/src/plugins/s3/plugin.c
@@ -17,6 +17,10 @@
 
 #include "s3_internals.h"
 
+static const struct rbh_pe_common_operations S3_BACKEND_PLUGIN_COMMON_OPS = {
+    .check_valid_token = rbh_s3_check_valid_token,
+};
+
 static const struct rbh_backend_plugin_operations S3_BACKEND_PLUGIN_OPS = {
     .new = rbh_s3_backend_new,
     .destroy = rbh_s3_plugin_destroy,
@@ -29,5 +33,6 @@ const struct rbh_backend_plugin RBH_BACKEND_PLUGIN_SYMBOL(S3) = {
         .version = RBH_S3_BACKEND_VERSION,
     },
     .ops = &S3_BACKEND_PLUGIN_OPS,
+    .common_ops = &S3_BACKEND_PLUGIN_COMMON_OPS,
     .capabilities = RBH_SYNC_OPS,
 };
