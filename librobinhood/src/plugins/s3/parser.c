23064c74966fcc7b97c0608964176ae3256d7d04
Alban RIO <Alban.RIO@cea.fr>
2025-07-28 16:41:04 +0200

    s3: add filter parser
    Adds a parser to check the validity of a
    token for the s3 plugin
    
    Change-Id: Iac8a579d5d13bbcfacc8ab3ebd866cedbefd3801
    Signed-off-by: Alban RIO <Alban.RIO@cea.fr>

---
+++ librobinhood/src/plugins/s3/parser.c
@@ -0,0 +1,42 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#include <assert.h>
+#include <string.h>
+
+#include <robinhood/filter.h>
+#include <robinhood/backend.h>
+
+enum rbh_parser_token
+rbh_s3_check_valid_token(const char *token)
+{
+    assert(token[0] == '-');
+
+    switch (token[1]) {
+    case 'm':
+        if (strcmp(&token[3], "time") == 0)
+            return RBH_TOKEN_PREDICATE;
+        break;
+
+    case 'n':
+        if (strcmp(&token[3], "ame") == 0)
+            return RBH_TOKEN_PREDICATE;
+        break;
+
+    case 'p':
+        if (strcmp(&token[3], "ath") == 0)
+            return RBH_TOKEN_PREDICATE;
+        break;
+
+    case 's':
+        if (strcmp(&token[3], "ize") == 0)
+            return RBH_TOKEN_PREDICATE;
+        break;
+    }
+
+    return RBH_TOKEN_UNKNOWN;
+}
