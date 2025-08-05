0f80e7acdc0aa0223f4adba79a83242133364001
Karl ENGELBRECHT <karl.engelbrecht@cea.fr>
2025-08-05 14:51:32 +0200

    undelete: new simple_list_test added
    This test simply removes an archived file and checks if rbh-undelete
    --list returns this file inside its output and the path returns is
    matching the original one.
    
    Change-Id: I304130547e56f25417b6bc63b8496fb458f29393
    Signed-off-by: Karl ENGELBRECHT <karl.engelbrecht@cea.fr>

--- rbh-undelete/tests/undelete.bash
+++ rbh-undelete/tests/undelete.bash
@@ -46,11 +46,48 @@ test_simple_undelete()
     rm $entry $cp_entry
 }
 
+test_list()
+{
+    local fileA="test"
+    local fileB="test2"
+
+    touch $fileB
+    touch $fileA
+
+    local path=$(realpath "$fileA")
+
+    archive_file $fileA
+
+    invoke_rbh-fsevents
+
+    rm $fileA
+
+    invoke_rbh-fsevents
+
+    local list=$(rbh_undelete "rbh:$db:$testdb" "rbh:lustre:$pwd" --list)
+    local rm_path=$(echo "$list" | grep rm_path |
+        sed -n 's/.*rm_path: \([^ ]*\).*/\1/p')
+    local n_lines=$(echo "$list" | wc -l)
+
+    if ((n_lines != 2)); then
+        error "There shoud be only one file deleted"
+    fi
+
+    local mountpoint=$(mongo "$testdb" --eval \
+    'db.info.findOne({_id: "mountpoint_info"}, {mountpoint: 1}).mountpoint')
+
+    local rm_full_path="${mountpoint}${rm_path}"
+
+    if [[ "$rm_full_path" != "$path" ]]; then
+        error "rm_path is not equal to path"
+    fi
+}
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
-declare -a tests=(test_simple_undelete)
+declare -a tests=(test_simple_undelete test_list)
 
 LUSTRE_DIR=/mnt/lustre/
 cd "$LUSTRE_DIR"
