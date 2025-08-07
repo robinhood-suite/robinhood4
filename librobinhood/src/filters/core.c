0558dd5c3acf6153020e40e4ed67dadab7c615e5
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-08-07 11:45:11 +0200

    rbh-policy: Added the function that will be used to create the filters
    Added the function that will be used in python to create filters.
    This function takes the uri as the arguments of the filter to be
    created and uses the codes from the previous patch to create a
    filter.
    If the uri does not have this filter, it returns NULL, otherwise
    it returns the filter.
    
    Change-Id: Ibcaabdb887e00078ec142975107a749378cf78f3
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- librobinhood/src/filters/core.c
+++ librobinhood/src/filters/core.c
@@ -11,11 +11,12 @@
 
 #include "robinhood/filters/core.h"
 #include "robinhood/statx.h"
-<<<<<<< HEAD
 #include "robinhood/utils.h"
-=======
 #include <robinhood.h>
->>>>>>> 61dc18e (rbh-policy: add function to retrieve backend plugin and extension info)
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
 
 void
 filters_ctx_finish(struct filters_context *ctx)
@@ -242,6 +243,41 @@ get_backend_plugin_info(const char *uri)
     free(extension_names);
 
     return info;
+}
+
+struct rbh_filter *
+build_filter_from_uri(const char *uri, const char **argv)
+{
+    struct rbh_backend_plugin_info info = get_backend_plugin_info(uri);
+    struct rbh_filter *filter = NULL;
+    bool need_prefetch = false;
+    int index = 0;
+    int argc = 2;
+
+    for (int i = 0; i < info.extension_count; ++i) {
+        const struct rbh_plugin_extension *ext = info.extensions[i];
+        if (ext->common_ops && ext->common_ops->build_filter &&
+            ext->common_ops->check_valid_token(argv[0]) ==
+            RBH_TOKEN_PREDICATE) {
+
+            filter = ext->common_ops->build_filter(argv, argc, &index,
+                                                   &need_prefetch);
+            if (filter != NULL)
+                return filter;
+        }
+    }
+
+    if (info.plugin->common_ops && info.plugin->common_ops->build_filter &&
+        info.plugin->common_ops->check_valid_token(argv[0]) ==
+        RBH_TOKEN_PREDICATE) {
+
+        filter = info.plugin->common_ops->build_filter(argv, argc, &index,
+                                                       &need_prefetch);
+        if (filter != NULL)
+            return filter;
+    }
+
+    return NULL;
 }
 
 static int
