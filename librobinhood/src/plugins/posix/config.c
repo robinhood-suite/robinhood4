4dafbed23cd885c3ee07a6decd6f2d5244593dc6
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-08-28 15:44:33 +0200

    librobinhood: externalize extension loading from POSIX plugin
    This patch moves the extension loading logic from the POSIX plugin into
    librobinhood, allowing other plugins to load enrichers and iterators.
    
    Change-Id: I764ae891a5dd5ac966da2e65f14c99751b7259c4
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- librobinhood/src/plugins/posix/config.c
+++ librobinhood/src/plugins/posix/config.c
@@ -14,29 +14,18 @@
 
 #include "robinhood/backends/posix_extension.h"
 
-static char *
-config_iterator_key(const char *type)
+int
+rbh_posix_backend_load_iterator(const struct rbh_backend_plugin *self,
+                                void *backend, const char *iterator,
+                                const char *type)
 {
-    char *key;
-
-    if (asprintf(&key, "backends/%s/iterator", type) == -1)
-        return NULL;
-
-    return key;
-}
-
-static int
-load_iterator(const struct rbh_plugin *self,
-              struct posix_backend *posix,
-              const char *iterator,
-              const char *type)
-{
+    struct posix_backend *posix = (struct posix_backend *) backend;
     const struct rbh_posix_extension *extension;
 
     if (!strcmp(iterator, "fts"))
         return 0;
 
-    extension = rbh_posix_load_extension(self, iterator);
+    extension = rbh_posix_load_extension(&self->plugin, iterator);
     if (!extension) {
         rbh_backend_error_printf("failed to load iterator '%s' for backend '%s'",
                                  iterator, type);
@@ -48,23 +37,13 @@ load_iterator(const struct rbh_plugin *self,
     return 0;
 }
 
-static char *
-config_enrichers_key(const char *type)
+int
+rbh_posix_backend_load_enrichers(const struct rbh_backend_plugin *self,
+                                 void *backend,
+                                 const struct rbh_value *enrichers,
+                                 const char *type)
 {
-    char *key;
-
-    if (asprintf(&key, "backends/%s/enrichers", type) == -1)
-        return NULL;
-
-    return key;
-}
-
-static int
-load_enrichers(const struct rbh_plugin *self,
-               struct posix_backend *posix,
-               const struct rbh_value *enrichers,
-               const char *type)
-{
+    struct posix_backend *posix = (struct posix_backend *) backend;
     size_t count = enrichers->sequence.count + 1; /* NULL terminated */
     const struct rbh_posix_extension *extension;
 
@@ -78,7 +57,7 @@ load_enrichers(const struct rbh_plugin *self,
 
         assert(value->type == RBH_VT_STRING);
 
-        extension = rbh_posix_load_extension(self, value->string);
+        extension = rbh_posix_load_extension(&self->plugin, value->string);
         if (!extension) {
             rbh_backend_error_printf("failed to load extension '%s' for backend '%s'",
                                      value->string, type);
@@ -88,67 +67,6 @@ load_enrichers(const struct rbh_plugin *self,
         posix->enrichers[i] = extension;
     }
     posix->enrichers[count - 1] = NULL;
-
-    return 0;
-}
-
-int
-rbh_posix_enrichers_list(struct rbh_config *config, const char *type,
-                         struct rbh_value *enrichers)
-{
-    char *key;
-    int rc;
-
-    key = config_enrichers_key(type);
-    rc = rbh_config_find(key, enrichers, RBH_VT_SEQUENCE);
-    free(key);
-
-    return rc;
-}
-
-int
-load_posix_extensions(const struct rbh_plugin *self,
-                      struct posix_backend *posix,
-                      const char *type,
-                      struct rbh_config *config)
-{
-    struct rbh_value enrichers;
-    struct rbh_value iterator;
-    char *key;
-    int rc;
-
-    if (!config)
-        return 0;
-
-    key = config_iterator_key(type);
-    rc = rbh_config_find(key, &iterator, RBH_VT_STRING);
-    free(key);
-    switch (rc) {
-    case KPR_FOUND:
-        if (load_iterator(self, posix, iterator.string, type) == -1)
-            return -1;
-        break;
-    case KPR_NOT_FOUND:
-        break;
-    default:
-        rbh_backend_error_printf("failed to retrieve 'iterator' key in configuration: %s",
-                                 strerror(errno));
-        return -1;
-    }
-
-    rc = rbh_posix_enrichers_list(config, type, &enrichers);
-    switch (rc) {
-    case KPR_FOUND:
-        if (load_enrichers(self, posix, &enrichers, type) == -1)
-            return -1;
-        break;
-    case KPR_NOT_FOUND:
-        break;
-    default:
-        rbh_backend_error_printf("failed to retrieve 'enrichers' key in configuration: %s",
-                                 strerror(errno));
-        return -1;
-    }
 
     return 0;
 }
