da2eb6d31589a45945c3652ceafbf015562acb50
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-09-03 16:09:19 +0200

    rbh-find: add s3 tests
    Change-Id: Ie81f8cab058114590f8b0b13eb57d1dad5b5a2f5
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ rbh-find/tests/s3/s3_utils.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This file is part of the RobinHood Library
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+minio_setup()
+{
+    export MC_INSECURE=1
+    yes | mc alias set local https://127.0.0.1:9000 minioadmin minioadmin
+}
+
+minio_teardown()
+{
+    mc rb --force --dangerous local
+}
