30a512b2819aae60b679b8eff97e010720778884
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-09-03 16:07:22 +0200

    s3: add build filter callback
    This patch adds the build filter callback of the s3 plugin to build
    the available filters in rbh-find.
    
    Change-Id: I4d30538e6d740a4688d14b7e38a5a686d74a7196
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ librobinhood/src/plugins/s3/plugin_callback_common.c
@@ -0,0 +1,22 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#include <stdlib.h>
+
+#include <robinhood/plugins/backend.h>
+
+#include "plugin_callback_common.h"
+
+const struct rbh_backend_plugin *posix_plugin;
+
+int
+import_posix_plugin()
+{
+    posix_plugin = rbh_backend_plugin_import("posix");
+
+    return posix_plugin ? 0 : 1;
+}
---
+++ librobinhood/src/plugins/s3/plugin_callback_common.h
@@ -0,0 +1,16 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_S3_PLUGIN_CALLBACK_COMMON_H
+#define RBH_S3_PLUGIN_CALLBACK_COMMON_H
+
+extern const struct rbh_backend_plugin *posix_plugin;
+
+int
+import_posix_plugin();
+
+#endif
