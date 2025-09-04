ade2bd1839841eed2a80115c63de903f10678220
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-09-04 14:08:19 +0200

    s3: add printf and fill_projection callbacks
    This patch adds the printf and fill_projection callbacks to the S3
    plugin. Also, the printf callback now supports a directive to
    display the bucket to which entries belong.
    
    Change-Id: I466f4e996ff4ae104df88cf5dd3de66f359a3efd
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- librobinhood/src/plugins/s3/action.c
+++ librobinhood/src/plugins/s3/action.c
@@ -5,9 +5,15 @@
  * SPDX-License-Identifer: LGPL-3.0-or-later
  */
 
+#include <assert.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <robinhood/fsentry.h>
+#include "robinhood/filter.h"
+#include "robinhood/fsentry.h"
+#include "robinhood/projection.h"
+#include "robinhood/utils.h"
 
 #include "s3_wrapper.h"
 
@@ -34,3 +40,77 @@ rbh_s3_delete_entry(struct rbh_fsentry *fsentry)
     return s3_delete_object(bucket, object);
 }
 
+int
+rbh_s3_fill_entry_info(char *output, int max_length,
+                       const struct rbh_fsentry *fsentry,
+                       const char *directive, const char *backend)
+{
+    const struct rbh_value *value;
+    char buffer[1024];
+
+    assert(directive != NULL);
+    assert(*directive != '\0');
+
+    switch(*directive) {
+    case 'b':
+        value = rbh_fsentry_find_inode_xattr(fsentry, "bucket");
+        if (value == NULL)
+            return 0;
+        return snprintf(output, max_length, "%s", value->string);
+    case 'f':
+        return snprintf(output, max_length, "%s", fsentry->name);
+    case 'H':
+        return snprintf(output, max_length, "%s", backend);
+    case 'I':
+        if (base64_encode(buffer, fsentry->id.data, fsentry->id.size) == 0)
+            return -1;
+
+        return snprintf(output, max_length, "%s", buffer);
+    case 'p':
+        return snprintf(output, max_length, "%s", fsentry_path(fsentry));
+    case 's':
+        return snprintf(output, max_length, "%lu", fsentry->statx->stx_size);
+    case 't':
+        return snprintf(output, max_length, "%s",
+                        time_from_timestamp(&fsentry->statx->stx_mtime.tv_sec));
+    case 'T':
+        return snprintf(output, max_length, "%lu",
+                        fsentry->statx->stx_mtime.tv_sec);
+    }
+
+    return 0;
+}
+
+int
+rbh_s3_fill_projection(struct rbh_filter_projection *projection,
+                       const char *directive)
+{
+    assert(directive != NULL);
+    assert(*directive != '\0');
+
+    switch (*directive) {
+    case 'b':
+        rbh_projection_add(projection, str2filter_field("xattrs.bucket"));
+        break;
+    case 'f':
+        rbh_projection_add(projection, str2filter_field("name"));
+        break;
+    case 'I':
+        rbh_projection_add(projection, str2filter_field("id"));
+        break;
+    case 'p':
+        rbh_projection_add(projection, str2filter_field("ns-xattrs"));
+        break;
+    case 's':
+        rbh_projection_add(projection, str2filter_field("statx.size"));
+        break;
+    case 't':
+    case 'T':
+        rbh_projection_add(projection, str2filter_field("statx.mtime.sec"));
+        break;
+    default:
+        return 0;
+    }
+
+    return 1;
+}
--- librobinhood/src/plugins/s3/plugin.c
+++ librobinhood/src/plugins/s3/plugin.c
@@ -21,6 +21,8 @@ static const struct rbh_pe_common_operations S3_BACKEND_PLUGIN_COMMON_OPS = {
     .check_valid_token = rbh_s3_check_valid_token,
     .build_filter = rbh_s3_build_filter,
     .delete_entry = rbh_s3_delete_entry,
+    .fill_entry_info = rbh_s3_fill_entry_info,
+    .fill_projection = rbh_s3_fill_projection,
 };
 
 static const struct rbh_backend_plugin_operations S3_BACKEND_PLUGIN_OPS = {
