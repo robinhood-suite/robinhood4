bc62b7d94258c30b1c5b936265e8a1421a3fda6d
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-09-29 13:28:15 +0200

    rbh-policy: Add a function for retrieve an fsentry iterator from db
    Added a c function in robinhood that takes a database uri provided in
    the configuration and an rbh_filter filter to query the db and retrieve
    an fsentry iterator.
    
    Change-Id: I91f2c0559219ed981da19e4c7b1b7dc41534fb00
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ librobinhood/src/policyengine.c
@@ -0,0 +1,45 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+#include <stdbool.h>
+#include <string.h>
+#include <sysexits.h>
+
+#include "robinhood/filters/core.h"
+#include "robinhood/statx.h"
+#include <robinhood.h>
+
+struct rbh_mut_iterator *
+rbh_collect_fsentry(const char *uri, struct rbh_filter *filter)
+{
+    struct rbh_backend *backend = rbh_backend_from_uri(uri, true);
+    if (!backend)
+        error(EXIT_FAILURE, errno, "rbh_backend_from_uri failed");
+
+    struct rbh_filter_options options = {
+        .skip = 0,
+        .limit = 0,
+        .skip_error = true,
+        .one = false,
+        .verbose = false,
+        .dry_run = false
+    };
+
+    struct rbh_filter_output output = {
+        .type = RBH_FOT_PROJECTION,
+        .projection = {
+            .fsentry_mask = RBH_FP_ID | RBH_FP_NAME,
+            .statx_mask = 0,
+        }
+    };
+
+    struct rbh_mut_iterator *it = rbh_backend_filter(backend, filter, &options,
+                                                     &output, NULL);
+    if (!it)
+        error(EXIT_FAILURE, errno, "rbh_backend_filter failed");
+
+    return it;
+}
