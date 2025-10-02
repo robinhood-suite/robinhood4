e85a82540390e3097a8c086a73c16f8f2d2376eb
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-10-02 14:30:25 +0200

    report: fix bug with more than 250 results
    This patch fixes a bug that occurred when displaying more than 250
    results (for example, when performing a group-by on uid). The buffer
    size was previously static and limited to 250. It has now been
    replaced with a list to store all results.
    
    Change-Id: Ie7459e7a0a15df18290310bf0a4dc289cf83cb42
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ rbh-report/tests/test_many_output.bash
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood 4
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_many_output()
+{
+    for i in {1..252}; do
+        local size=$((1 + $RANDOM % 100))
+        truncate -s $size "entry$i"
+    done
+
+    rbh_sync "rbh:posix:." "rbh:$db:$testdb"
+
+    rbh_report "rbh:$db:$testdb" \
+        --output "avg(statx.size),sum(statx.size),count()" \
+        --group-by "name,statx.size"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_many_output)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
