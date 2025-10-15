23848a6e5b56950243c8a09b144ac7e9d8f3e33f
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-10-15 16:41:42 +0200

    fsevents: reorganize sources/lustre.c
    This patch reorganizes the sources/lustre.c file, as it was becoming
    too large.
    
    Change-Id: Ia817d4b39b4941d6d021cb1b8f757c0a134f4651
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ rbh-fsevents/src/sources/lustre/lustre.h
@@ -0,0 +1,78 @@
+/* This file is part of Robinhood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_FSEVENTS_SOURCE_LUSTRE_H
+#define RBH_FSEVENTS_SOURCE_LUSTRE_H
+
+#include <stdbool.h>
+#include <pthread.h>
+
+#include <robinhood/list.h>
+#include <robinhood/iterator.h>
+
+#include "source.h"
+
+/** This structure will contains all the information of one batch sent to
+ *  enrichment. It contains the last changelog index read for that batch,
+ *  the number of threads the batch was divided among
+ */
+struct source_batch_node {
+    struct rbh_list_node link;
+    uint64_t batch_id;             /** Id of this batch */
+    uint64_t last_changelog_index; /** Index of the last changelog index read */
+    size_t ack_required;           /** Number of threads the batch was divided
+                                    *  among
+                                    */
+};
+
+struct lustre_changelog_iterator {
+    struct rbh_iterator iterator;
+
+    void *reader;
+    struct sink *sink;
+    struct rbh_iterator *fsevents_iterator;
+
+    char *username;
+    char *mdt_name;
+    int32_t source_mdt_index;
+    uint64_t last_changelog_index;
+    uint64_t last_batch_changelog_index;
+    uint64_t nb_changelog;
+    uint64_t max_changelog;
+    bool empty;
+
+    /* Only use without dedup, it's a reference to the current batch in the
+     * list of batches saved to avoid iterating over the list each times.
+     */
+    struct source_batch_node *curr_batch;
+
+    FILE *dump_file;
+};
+
+struct lustre_source {
+    struct source source;
+
+    /** List of all the batch sent to enrichment */
+    struct rbh_list_node *batch_list;
+    pthread_mutex_t batch_lock;
+    /** Current batch id */
+    uint64_t batch_id;
+    struct lustre_changelog_iterator events;
+};
+
+void lustre_changelog_save_batch(void *source, size_t ack_required,
+                                 bool dedup);
+
+void lustre_changelog_ack_batch(void *source, uint64_t batch_id);
+
+const void *
+lustre_changelog_iter_next(void *iterator);
+
+void
+lustre_changelog_iter_destroy(void *iterator);
+
+#endif
---
+++ rbh-fsevents/src/sources/lustre/lustre_utils.h
@@ -0,0 +1,79 @@
+/* This file is part of Robinhood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_FSEVENTS_SOURCE_LUSTRE_UTILS_H
+#define RBH_FSEVENTS_SOURCE_LUSTRE_UTILS_H
+
+#include <lustre/lustreapi.h>
+#include <linux/lustre/lustre_fid.h>
+
+#include <robinhood/value.h>
+#include <robinhood/statx.h>
+
+#include "lustre.h"
+
+static inline time_t
+cltime2sec(uint64_t cltime)
+{
+    /* extract secs from time field */
+    return cltime >> 30;
+}
+
+static inline unsigned int
+cltime2nsec(uint64_t cltime)
+{
+    /* extract nanosecs: */
+    return cltime & ((1 << 30) - 1);
+}
+
+struct rbh_value *
+fill_xattrs_fid(void *arg);
+
+struct rbh_value *
+fill_xattrs_mdt_index(void *arg);
+
+struct rbh_value *
+fill_xattrs_nb_children(void *arg);
+
+struct rbh_value *
+fill_inode_xattrs(void *arg);
+
+struct rbh_value *
+build_symlink_enrich_map(void *arg);
+
+int
+build_enrich_xattr_fsevent(struct rbh_value_map *xattrs_map,
+                           char *key, struct rbh_value *value, ...);
+
+struct rbh_id *
+build_id(const struct lu_fid *fid);
+
+int
+create_statx_uid_gid(struct changelog_rec *record,
+                     struct rbh_statx **rec_statx);
+
+struct rbh_value *
+build_statx_map(void *enrich_mask);
+
+#define RBH_PATH_MAX    PATH_MAX
+#define CL_BASE_FORMAT "%s: %llu %02d%-5s %u.%09u 0x%x%s t="DFID
+#define CL_BASE_ARG(_mdt, _rec_) (_mdt), (_rec_)->cr_index, (_rec_)->cr_type, \
+                                 changelog_type2str((_rec_)->cr_type),        \
+                                 (uint32_t)cltime2sec((_rec_)->cr_time),      \
+                                 cltime2nsec((_rec_)->cr_time),               \
+                                 (_rec_)->cr_flags & CLF_FLAGMASK, flag_buff, \
+                                 PFID(&(_rec_)->cr_tfid)
+#define CL_NAME_FORMAT "p="DFID" %.*s"
+#define CL_NAME_ARG(_rec_) PFID(&(_rec_)->cr_pfid), (_rec_)->cr_namelen, \
+        changelog_rec_name(_rec_)
+#define CL_EXT_FORMAT   "s="DFID" sp="DFID" %.*s"
+
+void
+dump_changelog(struct lustre_changelog_iterator *records,
+               struct changelog_rec *record);
+
+#endif
