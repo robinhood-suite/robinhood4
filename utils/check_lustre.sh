a161fd03b87f16ed7f25bc69012b4b5ead1a26d0
VALERI Yoann <yoann.valeri@cea.fr>
2025-10-20 15:16:14 +0200

    meson: use {build, test}_lustre variables in main meson
    This patch adds `build_lustre` and `test_lustre` variables so as to not
    rely solely on the option given to meson but also on the state of the
    system. And also it allows building Lustre-related code without actually
    testing it.
    
    Change-Id: Ie330d4866b8cd4ae8d6b54780b448d4dc6dff6fe
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- utils/check_lustre.sh
+++ utils/check_lustre.sh
@@ -1,17 +1,9 @@
+#!/usr/bin/env bash
+
 # This file is part of RobinHood 4
 # Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
 #                    alternatives
 #
 # SPDX-License-Identifer: LGPL-3.0-or-later
 
-subdir('posix')
-subdir('retention')
-subdir('mongo')
-
-if test_mfu
-    subdir('mpifile')
-endif
-
-if test_lustre
-    subdir('lustre')
-endif
+grep lustre /proc/mounts
