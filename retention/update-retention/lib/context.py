c46a2641567dedaffe7fc0fc201b995d46f7b9ea
VALERI Yoann <yoann.valeri@cea.fr>
2025-10-21 16:24:11 +0200

    update-ret: add context class
    This patch adds a command context class for general arguments like URI,
    config, or times.
    
    Change-Id: I0e3acf720be7a68a752418dcaa5e3c2b8558e7ff
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ retention/update-retention/lib/context.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# This file is part of RobinHood 4
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#            alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+import subprocess
+
+from datetime import datetime, timedelta
+
+class Context():
+    """General context of the command"""
+    def __init__(self, uri, config, delay, delete):
+        self.uri = uri
+        self.config = config
+        self.delete = delete
+
+        dt = datetime.now()
+        self.current = datetime.timestamp(dt)
+
+        if delay is 0:
+            self.delay = int(self.current)
+        else:
+            two_days = timedelta(days=delay)
+            self.delay = int(datetime.timestamp(dt + two_days))
+
+        command = (["rbh-info", uri, "-m"])
+
+        try:
+            process = subprocess.check_output(command)
+            self.mountpoint = process.decode('utf-8').rstrip()
+
+        except subprocess.CalledProcessError as e:
+            print(f"rbh-info failed: {e.output.decode('utf-8')}")
+            sys.tracebacklimit = -1
+
