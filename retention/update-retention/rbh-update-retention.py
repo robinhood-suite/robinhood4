8d467cb0ec9ef856506f4d3ef2dce76ddbf28f92
VALERI Yoann <yoann.valeri@cea.fr>
2025-10-21 13:03:30 +0200

    update-ret: add argparse and command arguments
    This patchs ports all the arguments from the old `rbh-update-retention`
    bash script to the new script using argparse.
    
    Change-Id: I000b01f4f3535167b6677b2644203ed2541a4290
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- retention/update-retention/rbh-update-retention.py
+++ retention/update-retention/rbh-update-retention.py
@@ -6,9 +6,60 @@
 #
 # SPDX-License-Identifer: LGPL-3.0-or-later
 
+import argparse
+import pathlib
+import re
+
 from lib.directory import Directory
+
+def looks_like_URI(string):
+    pattern = re.compile(r"rbh:mongo:[A-Za-z0-9]+", re.IGNORECASE)
+    if pattern.match(string) is None:
+        raise TypeError(f"'{string}' does not look like a URI, expected "
+                         "'rbh:mongo:<db_name>'")
+
+    return string
+
+def make_parser():
+    """Create command line parser"""
+
+    parser = argparse.ArgumentParser(
+        description='Update the retention of attributes of any directory that '
+                    'is expired, based on their content\'s last accessed time. '
+                    'If a directory is expired, it is printed to the output.'
+    )
+
+    parser.add_argument('uri', metavar='URI', type=looks_like_URI,
+                        help='URI of where to fetch and update expired '
+                             'directories')
+    parser.add_argument('-c', '--config', metavar='CONFIG', type=pathlib.Path,
+                        default='/etc/robinhood4.d/default.yaml',
+                        help='path to a configuration file, used to determine '
+                             'the expiration attribute to check for. By '
+                             'default, use \'/etc/robinhood4.d/default.yaml\'')
+    parser.add_argument('-d', '--delay', metavar='DELAY', default=0, type=int,
+                        help='a delay window during which entries that will '
+                             'expire should be notified. Should be given as '
+                             'a number of days. No delay by default.')
+    parser.add_argument('--delete', action='store_true', default=False,
+                        help='delete the expired directories instead of just '
+                             'printing. False by default.')
+
+    return parser
+
+def main(args=None):
+    args = make_parser().parse_args(args)
+
+    print("uri = " + args.uri)
+    print("config = " + str(args.config))
+    print("delay = " + str(args.delay))
+    print("delete = " + str(args.delete))
+
+    return 0
 
 if __name__ == "__main__":
     print("Hello, I'm UR")
     directory = Directory("blob")
     print(directory.name)
+
+    main()
