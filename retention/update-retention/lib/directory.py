f642c0ec433431f1f5c81a996e863d8116b17c0a
VALERI Yoann <yoann.valeri@cea.fr>
2025-10-21 16:05:43 +0200

    update-ret: properly define directory class and functions
    This patch properly defines the directory class, its init function and
    other functions for general usage. Especially, it adds the retrieval of
    the maximum access/modify time of a file in that directory.
    
    Change-Id: I0e7d975a514b101b305ab26e0a360bc97fdb13ea
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- retention/update-retention/lib/directory.py
+++ retention/update-retention/lib/directory.py
@@ -6,11 +6,40 @@
 #
 # SPDX-License-Identifer: LGPL-3.0-or-later
 
-import sys
-import os
-import importlib
+import subprocess
 
 class Directory():
-    """Imports the config module named fs_name from CONFIG_DIR"""
-    def __init__(self, name):
-        self.name = name
+    """Class representing a directory as output of rbh-find"""
+    def __init__(self, path, retention_attr, expiration_date, ID):
+        self.path = path
+        self.relative_retention = (retention_attr[0] == '+')
+        self.retention_attr = (retention_attr[1:] if self.relative_retention
+                                                  else retention_attr)
+        self.expiration_date = expiration_date
+        self.ID = ID
+
+    def set_max_time(self, uri):
+        branch = f"{uri}#{self.path}"
+        command = (["rbh-find", branch, "-type", "f",
+                    "-printf", "%A\n%T\n"])
+
+        try:
+            process = subprocess.Popen(command, stdout=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            print(f"rbh-find failed: {e.output.decode('utf-8')}")
+            sys.tracebacklimit = -1
+            return 1
+
+        print(f"args = '{process.args}'")
+
+        self.max_time = 0
+        for line in iter(process.stdout.readline, b""):
+            line = line.decode('utf-8').rstrip()
+            if int(line) > self.max_time:
+                self.max_time = int(line)
+
+    def actual_expiration_date(self):
+        return self.retention_attr + self.max_time
+
+    def is_truly_expired(self, current):
+        return self.actual_expiration_date <= current
