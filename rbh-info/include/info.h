ada725af3a459692347bb2c6970e0b9659f9d3f7
VALERI Yoann <yoann.valeri@cea.fr>
2025-10-28 10:38:36 +0100

    info: divide main file in 3
    This patch divides the main rbh-info file in 3, plus some minor
    modifications.
    
    Change-Id: Iceb41e1fc804a799e6e00bd4d132f46ad326b205
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-info/include/info.h
@@ -0,0 +1,39 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_INFO_H
+#define RBH_INFO_H
+
+#include <robinhood/plugin.h>
+#include <robinhood/plugins/backend.h>
+
+/**
+ * List the plugins and extensions currently installed.
+ */
+void
+list_plugins_and_extensions();
+
+/**
+ * Show the capabilities of a given plugin.
+ *
+ * @param plugin   The plugin whose capabilities should be shown
+ */
+void
+capabilities_translate(const struct rbh_backend_plugin *plugin);
+
+/**
+ * Show the information requested in \p flags using the backend \p from.
+ *
+ * @param from     Where to fetch the information
+ * @param flags    The information to fetch and show
+ */
+void
+print_info_fields(struct rbh_backend *from, int flags);
+
+void
+info_translate(const struct rbh_backend_plugin *plugin);
+#endif
