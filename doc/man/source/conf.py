cf52d56e0c842f9fe290d61563b04f3eb46db949
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-11-03 14:19:40 +0100

    doc: add setup for man pages
    This patch adds all the requirements needed to build man pages with
    Sphinx. A build option has also been added to control whether the man
    pages should be generated or not.
    
    Change-Id: Ie0f01f33c4e67ed37869dbb36c9b1bb0e07ad679
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ doc/man/source/conf.py
@@ -0,0 +1,55 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'RobinHood v4'
+copyright = '2025, CEA/DAM'
+author = 'CEA/DAM'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+language = 'en'
+
+extensions = []
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This patterns also effect to html_static_path and html_extra_path
+exclude_patterns = []
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = '.rst'
+source_encoding = 'utf-8-sig'
+master_doc = 'index'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+# -- Options for manual page output ---------------------------------------
+
+rst_prolog = ''
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('rbh-sync', 'rbh-sync', 'rbh-sync tool', [author], 1),
+    ('rbh-find', 'rbh-find', 'rbh-find tool', [author], 1),
+    ('rbh-fsevents', 'rbh-fsevents', 'rbh-fsevents tool', [author], 1),
+    ('rbh-report', 'rbh-report', 'rbh-report tool', [author], 1),
+    ('rbh-info', 'rbh-info', 'rbh-info tool', [author], 1),
+    ('rbh-undelete', 'rbh-undelete', 'rbh-undelete tool', [author], 1),
+]
+
+smartquotes = False
