c63c4270d6cd2b5f03b86df320014845aa7f5b7e
VALERI Yoann <yoann.valeri@cea.fr>
2025-11-06 10:58:36 +0100

    sync: move test files to different directories
    This patch creates a bunch of directories to properly separate the tests
    and the meson files.
    
    Change-Id: Ic9700dd13ac49520722e7d4f9b643a16dbdc9929
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1225876
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- rbh-sync/tests/lustre/test_lustre_db_sync.bash
+++ rbh-sync/tests/lustre/test_lustre_db_sync.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # This file is part of RobinHood 4
-# Copyright (C) 2022 Commissariat a l'energie atomique et aux energies
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
 #                    alternatives
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 test_dir=$(dirname $(readlink -e $0))
-. $test_dir/../../utils/tests/framework.bash
+. $test_dir/../../../utils/tests/framework.bash
 . $test_dir/lustre_utils.bash
 
 rbh_sync_lustre()
--- rbh-sync/tests/posix/test_alias.bash
+++ rbh-sync/tests/posix/test_alias.bash
@@ -7,7 +7,7 @@
 # SPDX-License-Identifer: LGPL-3.0-or-later
 
 test_dir=$(dirname $(readlink -e $0))
-. $test_dir/../../utils/tests/framework.bash
+. $test_dir/../../../utils/tests/framework.bash
 
 ################################################################################
 #                                    TESTS                                     #
