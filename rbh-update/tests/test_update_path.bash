f8958403439849ebecbe2a513fa7ca7aec524a57
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2025-11-14 16:27:11 +0100

    rbh-update: add a simple test
    This simple test checks that if a directory does not have a path
    (removed during a rename), the directory and all its children's paths
    will be updated.
    
    Change-Id: I95de2db2beba22edeaedb9fda15b59e7fe3eeddb
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

---
+++ rbh-update/tests/test_update_path.bash
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# This file is part of rbh-undelete.
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_simple_update_path()
+{
+    mkdir dir
+    touch dir/file
+
+    rbh_sync "rbh:posix:." "rbh:$db:$testdb"
+
+    local userid="$(lctl --device "$LUSTRE_MDT" changelog_register |
+                        cut -d "'" -f2)"
+    trap -- "lctl --device '$LUSTRE_MDT' changelog_deregister '$userid'" EXIT
+
+    mv dir new_dir
+
+    invoke_rbh-fsevents
+
+    rbh_find "rbh:$db:$testdb" | sort | difflines "/" "/dir/file"
+
+    rbh_update "rbh:$db:$testdb"
+
+    rbh_find "rbh:$db:$testdb" | sort | difflines "/" "/new_dir" "/new_dir/file"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+declare -a tests=(test_simple_update_path)
+
+LUSTRE_DIR=/mnt/lustre/
+cd "$LUSTRE_DIR"
+
+LUSTRE_MDT=lustre-MDT0000
+
+tmpdir=$(mktemp --directory --tmpdir=$LUSTRE_DIR)
+lfs setdirstripe -D -i 0 $tmpdir
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests "${tests[@]}"
