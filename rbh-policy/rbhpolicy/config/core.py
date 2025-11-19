dd13a5e847503446bf9b436b871e34fc2ce1d519
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-11-19 12:49:32 +0100

    rbh-policy: Added a core file to import api in the configuration
    We add a file containing the imports that enable the use of the
    policy engine's functions. The administrator will then only need
    to import this core.py file to use the rbh4 Python API.
    
    Change-Id: Ib44879c2a38f2e377cf89c43b5a32270cd09ee73
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-policy/rbhpolicy/config/core.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.9
+# This file is part of RobinHood 4
+# Copyright (C) 2025 Commissariat a l'energie atomique et aux energies
+#            alternatives
+#
+# SPDX-License-Identifer: LGPL-3.0-or-later
+
+from rbhpolicy.config.fileclass import *
+from rbhpolicy.config.entities import *
+from rbhpolicy.config.policy import *
+from rbhpolicy.config.filter import *
