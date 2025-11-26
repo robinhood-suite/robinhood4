1405e7bfe6a676c5ca6b36186ba05698381e20eb
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2025-11-26 12:53:58 +0100

    rbh-policy: add field extraction with correct type mapping
    Extract field values from fsentries with correct RBH_VT_* types
    to match filter normalization.
    Type mismatch causes filter evaluation to fail.
    
    Change-Id: I08d664612349788bed136c6924223bbb1bcfd5d1
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- librobinhood/src/policyengine.c
+++ librobinhood/src/policyengine.c
@@ -55,3 +55,138 @@ rbh_pe_get_path(const struct rbh_fsentry *fsentry)
 
     return path_value->string;
 }
+
+static const struct rbh_value *
+get_field_value(const struct rbh_fsentry *fsentry,
+                const struct rbh_filter_field *field)
+{
+    static struct rbh_value value;
+
+    switch (field->fsentry) {
+    case RBH_FP_NAME:
+        if (!(fsentry->mask & RBH_FP_NAME))
+            return NULL;
+        value.type = RBH_VT_STRING;
+        value.string = fsentry->name;
+        return &value;
+
+    case RBH_FP_SYMLINK:
+        if (!(fsentry->mask & RBH_FP_SYMLINK))
+            return NULL;
+        value.type = RBH_VT_STRING;
+        value.string = fsentry->symlink;
+        return &value;
+
+    case RBH_FP_NAMESPACE_XATTRS:
+        if (!(fsentry->mask & RBH_FP_NAMESPACE_XATTRS))
+            return NULL;
+        if (field->xattr == NULL)
+            return NULL;
+        return rbh_fsentry_find_ns_xattr(fsentry, field->xattr);
+
+    case RBH_FP_INODE_XATTRS:
+        if (!(fsentry->mask & RBH_FP_INODE_XATTRS))
+            return NULL;
+        if (field->xattr == NULL)
+            return NULL;
+        return rbh_fsentry_find_inode_xattr(fsentry, field->xattr);
+
+    case RBH_FP_STATX:
+        if (!(fsentry->mask & RBH_FP_STATX))
+            return NULL;
+
+        const struct rbh_statx *statx = fsentry->statx;
+
+        switch (field->statx) {
+        case RBH_STATX_TYPE:
+            if (!(statx->stx_mask & RBH_STATX_TYPE))
+                return NULL;
+            value.type = RBH_VT_INT32;
+            value.int32 = statx->stx_mode & S_IFMT;
+            return &value;
+
+        case RBH_STATX_MODE:
+            if (!(statx->stx_mask & RBH_STATX_MODE))
+                return NULL;
+            value.type = RBH_VT_UINT32;
+            value.uint32 = statx->stx_mode;
+            return &value;
+
+        case RBH_STATX_SIZE:
+            if (!(statx->stx_mask & RBH_STATX_SIZE))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_size;
+            return &value;
+
+        case RBH_STATX_ATIME_SEC:
+            if (!(statx->stx_mask & RBH_STATX_ATIME_SEC))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_atime.tv_sec;
+            return &value;
+
+        case RBH_STATX_MTIME_SEC:
+            if (!(statx->stx_mask & RBH_STATX_MTIME_SEC))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_mtime.tv_sec;
+            return &value;
+
+        case RBH_STATX_CTIME_SEC:
+            if (!(statx->stx_mask & RBH_STATX_CTIME_SEC))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_ctime.tv_sec;
+            return &value;
+
+        case RBH_STATX_BTIME_SEC:
+            if (!(statx->stx_mask & RBH_STATX_BTIME_SEC))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_btime.tv_sec;
+            return &value;
+
+        case RBH_STATX_UID:
+            if (!(statx->stx_mask & RBH_STATX_UID))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_uid;
+            return &value;
+
+        case RBH_STATX_GID:
+            if (!(statx->stx_mask & RBH_STATX_GID))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_gid;
+            return &value;
+
+        case RBH_STATX_NLINK:
+            if (!(statx->stx_mask & RBH_STATX_NLINK))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_nlink;
+            return &value;
+
+        case RBH_STATX_BLOCKS:
+            if (!(statx->stx_mask & RBH_STATX_BLOCKS))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_blocks;
+            return &value;
+
+        case RBH_STATX_INO:
+            if (!(statx->stx_mask & RBH_STATX_INO))
+                return NULL;
+            value.type = RBH_VT_UINT64;
+            value.uint64 = statx->stx_ino;
+            return &value;
+
+        default:
+            return NULL;
+        }
+
+    default:
+        return NULL;
+    }
+}
