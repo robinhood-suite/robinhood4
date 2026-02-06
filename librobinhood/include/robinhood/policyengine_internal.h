00c579b78dc02f5bc97f62b49bdcfd59dfe54b3e
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2026-02-06 12:04:12 +0100

    rbh-policy: add unit tests for compare_values
    Add two unit tests for the compare_values: one
    covering all supported types and operators
    (success scenarios), and one for incompatible types,
    mismatched values, and invalid operators (failure scenarios).
    
    Change-Id: I232f92d73d78b56f7b699148c1730d10c5386b82
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ librobinhood/include/robinhood/policyengine_internal.h
@@ -0,0 +1,19 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_POLICYENGINE_INTERNAL_H
+#define RBH_POLICYENGINE_INTERNAL_H
+
+#include "robinhood/policyengine.h"
+#include "robinhood/value.h"
+
+bool
+compare_values(enum rbh_filter_operator op,
+               const struct rbh_value *field_val,
+               const struct rbh_value *filter_val);
+
+#endif
