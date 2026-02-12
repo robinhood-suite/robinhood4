d2fe32ce7304abdaa0b2c2e080aa18647f3ec5a5
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2026-02-12 14:23:55 +0100

    librobinhood: add rbh_strerror macro for backend error reporting
    Introduce the rbh_strerror(e) macro to retrieve error messages from either
    the backend (when errno == RBH_BACKEND_ERROR) or the system (strerror).
    
    Change-Id: I1243511731ae56999b2b8bfff622deb6137810b9
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- librobinhood/src/policyengine.c
+++ librobinhood/src/policyengine.c
@@ -749,7 +749,8 @@ rbh_pe_execute(struct rbh_mut_iterator *mirror_iter,
             if (errno == EAGAIN)
                 continue;
 
-            fprintf(stderr, "Error during iteration: %s\n", strerror(errno));
+            fprintf(stderr, "Error during iteration: %s\n",
+                    rbh_strerror(errno));
             rbh_pe_actions_destroy(&action_cache);
             rbh_backend_destroy(fs_backend);
             return -1;
@@ -758,7 +759,7 @@ rbh_pe_execute(struct rbh_mut_iterator *mirror_iter,
         fresh = rbh_get_fresh_fsentry(fs_backend, mirror_entry);
         if (fresh == NULL) {
             fprintf(stderr, "Warning: cannot get fresh metadata %s\n",
-                    strerror(errno));
+                    rbh_strerror(errno));
             continue;
         }
 
