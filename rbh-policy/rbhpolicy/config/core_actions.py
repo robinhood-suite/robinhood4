1fd92c55ab3fdae1d04daeea83bc7c34b37c4074
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2026-03-04 15:26:30 +0100

    rbh-policy: add symbolic action constants for config API
    This patch introduces a new core_actions.py module that
    defines symbolic names for built-in actions such as log
    and delete. These are now exposed via the config API,
    allowing users to write action=action.log instead of
    using string literals.
    
    Change-Id: I2e3082482d6166caa1cdeed5a68897b320c401aa
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-policy/rbhpolicy/config/core_actions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# This file is part of RobinHood 4
+# Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+#            alternatives
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+class action:
+    log = "log"
+    delete = "delete"
+    # Add more built-in actions as needed
