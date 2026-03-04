132ad09fd40904e713e1508cce58743a11bf8fb5
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2026-03-04 15:26:30 +0100

    rbh-policy: add symbolic action constants for config API
    This patch introduces a new core_actions.py module that
    defines symbolic names for built-in actions such as log
    and delete. These are now exposed via the config API,
    allowing users to write action=action.log instead of
    using string literals.
    
    Change-Id: I2e3082482d6166caa1cdeed5a68897b320c401aa
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- rbh-policy/tests/lustre/test_actions_lustre.py
+++ rbh-policy/tests/lustre/test_actions_lustre.py
@@ -189,7 +189,7 @@ config(
 declare_policy(
     name = "test_lustre_log_policy",
     target = (Type == "f"),
-    action = "log",
+    action = action.log,
     trigger = 'Periodic("10m")'
 )
 """)
@@ -249,7 +249,7 @@ config(
 declare_policy(
     name = "test_lustre_delete",
     target = (Type == "f") & (Name == "file1.txt"),
-    action = "delete",
+    action = action.delete,
     trigger = 'Periodic("10m")'
 )
 """)
