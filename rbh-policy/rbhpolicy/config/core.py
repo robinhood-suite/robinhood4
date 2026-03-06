28e8b6bfa201cdd126f02ddd6a39622cce04047a
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2026-03-06 16:03:20 +0100

    rbh-policy: add temporal triggers (periodic and scheduled)
    Both triggers are currently minimal and always match in manual mode, ensuring
    they do not block manual evaluations. Their full time-based behavior will be
    implemented later when the daemon is added.
    
    Change-Id: I6026f9f83a077264872a918f26a79c5eec07ffa0
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

--- rbh-policy/rbhpolicy/config/core.py
+++ rbh-policy/rbhpolicy/config/core.py
@@ -5,9 +5,17 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
+from rbhpolicy.config.triggers.triggers import Periodic, Scheduled
 from rbhpolicy.config.fileclass import declare_fileclass
 from rbhpolicy.config.policy import declare_policy, Rule
 from rbhpolicy.config.cpython import config
 from rbhpolicy.config.utils import cmd
 from rbhpolicy.config.entities import *
 from rbhpolicy.config.core_actions import *
+
+# Convenient trigger shortcuts for common intervals
+EveryMinute = Periodic("1m")
+Hourly = Periodic("1h")
+Daily = Periodic("1d")
+Weekly = Periodic("7d")
+Monthly = Periodic("30d")
