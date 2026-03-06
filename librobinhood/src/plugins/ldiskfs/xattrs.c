733e020c2e38ead32d6c73fb3802847b362362fc
Elouan LESTEVEN <elouan.lesteven@cea.fr>
2026-03-06 15:14:41 +0000

    ldiskfs: added scanning of extended attributes
    Use the ext2fs library to iterate over the extended attributes of
    each inode and store them in a value_map
    
    Change-Id: Ia71415ad11a02695750a7f848ef12c27d3ce33e8
    Signed-off-by: Elouan LESTEVEN <elouan.lesteven@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1233209
    Tested-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ librobinhood/src/plugins/ldiskfs/xattrs.c
@@ -0,0 +1,106 @@
+/* This file is part of RobinHood
+ * Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#include "internals.h"
+
+int
+rbh_get_xattrs(char *_name, char *_value,size_t value_len,
+               ext2_ino_t inode_num, void *data)
+{
+
+    (void) inode_num;
+
+    // FIXME I'm pretty sure this is a memory leak since rbh fsentries are freed using only a call to free()
+    size_t name_len = strlen(_name);
+    struct rbh_value *xattr_value;
+    char *value;
+    char *name;
+
+    xattr_value = xmalloc(sizeof(struct rbh_value));
+    value = xmalloc(value_len * sizeof(char));
+    name = xmalloc(name_len * sizeof(char));
+
+    memcpy(value, _value, value_len * sizeof(char));
+    memcpy(name, _name, name_len * sizeof(char));
+
+    // Skip the system.data xattr that only contains inline data if it exists
+    if (strcmp(name, "system.data") == 0) {
+        free(value);
+        free(name);
+        free(xattr_value);
+        return 0;
+    }
+
+    struct rbh_value _xattr_value = {
+        .type = RBH_VT_STRING,
+        .binary = {
+            .data = value,
+            .size = value_len,
+        }
+    };
+
+    *xattr_value = _xattr_value;
+
+    struct rbh_value_pair xattr_pair = {
+        .key = name,
+        .value = xattr_value,
+    };
+
+    struct rbh_value_map *xattrs = data;
+    struct rbh_value_pair *pairs = (struct rbh_value_pair *)xattrs->pairs;
+
+    pairs[xattrs->count] = xattr_pair;
+    xattrs->count++;
+
+    return 0;
+}
+
+struct rbh_value_map
+get_xattrs_from_inode(ext2_filsys fs, struct ext2_inode_large *inode,
+                      ext2_ino_t ino)
+{
+    struct rbh_value_map default_ret = {0};
+    struct ext2_xattr_handle *handle;
+    struct rbh_value_pair *pairs;
+    struct rbh_value_map ret;
+    errcode_t err;
+    size_t count;
+
+    err = ext2fs_xattrs_open(fs, ino, &handle);
+    if (err)
+        goto err;
+
+    err = ext2fs_xattrs_read(handle);
+    if (err)
+        goto err2;
+
+    err = ext2fs_xattrs_count(handle, &count);
+    if (err)
+        goto err2;
+
+    pairs = xmalloc(count * sizeof(struct rbh_value_pair));
+
+    ret.count = 0;
+    ret.pairs = pairs;
+
+    // we don't change any xattr value so the return value will always be 0
+    ext2fs_xattrs_iterate(handle, &rbh_get_xattrs, &ret);
+
+    err = ext2fs_xattrs_close(&handle);
+    if (err)
+        goto err;
+
+    return ret;
+
+err2:
+    ext2fs_xattrs_close(&handle);
+
+err:
+    rbh_backend_error_printf("Failed to read extended attributes of inode %d",
+                            ino);
+    return default_ret;
+}
