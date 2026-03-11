e349c7c02593bfb51bb07603336af7b8135ed064
Elouan LESTEVEN <elouan.lesteven@cea.fr>
2026-03-11 13:40:07 +0000

    ldiskfs: fix memory leaks when copying xattrs
    Fixed the existing memory leaks using rbh_sstack structures
    Moved all declarations at the beginning of functions
    
    Change-Id: I74445cc179d92628b8c4dd372d2e4a223ce31591
    Signed-off-by: Elouan LESTEVEN <elouan.lesteven@cea.fr>

--- librobinhood/src/plugins/ldiskfs/xattrs.c
+++ librobinhood/src/plugins/ldiskfs/xattrs.c
@@ -7,49 +7,40 @@
 
 #include "internals.h"
 
+struct xattr_iter_data {
+    struct rbh_value_map *values;
+    struct rbh_sstack *sstack;
+};
+
+
 int
-rbh_get_xattrs(char *_name, char *_value,size_t value_len,
-               ext2_ino_t inode_num, void *data)
+rbh_get_xattrs(char *_name, char *value,size_t value_len,
+               ext2_ino_t inode_num, void *_data)
 {
+    struct xattr_iter_data *data = _data;
+    struct rbh_value_pair *xattr_pair;
+    struct rbh_value_map *xattrs;
+    struct rbh_value_pair *pairs;
+    char *name;
+    int rc;
 
     (void) inode_num;
 
-    // FIXME I'm pretty sure this is a memory leak since rbh fsentries are freed using only a call to free()
-    struct rbh_value *xattr_value = xmalloc(sizeof(struct rbh_value));
-    char *value = xmalloc(value_len * sizeof(char));
-    char *name = xmalloc(name_len * sizeof(char));
-    size_t name_len = strlen(_name);
-
-    memcpy(value, _value, value_len * sizeof(char));
-    memcpy(name, _name, name_len * sizeof(char));
+    xattrs = data->values;
+    pairs = (struct rbh_value_pair *)xattrs->pairs;
+    xattr_pair = &pairs[xattrs->count];
 
     // Skip the system.data xattr that only contains inline data if it exists
-    if (strcmp(name, "system.data") == 0) {
-        free(value);
-        free(name);
-        free(xattr_value);
+    if (!strcmp(_name, "system.data"))
         return 0;
-    }
 
-    struct rbh_value _xattr_value = {
-        .type = RBH_VT_STRING,
-        .binary = {
-            .data = value,
-            .size = value_len,
-        }
-    };
+    // fill_binary_pair does not push the name on the sstack so we do it manually.
+    name = rbh_sstack_push(data->sstack, _name, strlen(_name) + 1);
 
-    *xattr_value = _xattr_value;
+    rc = fill_binary_pair(name, value, value_len, xattr_pair, data->sstack);
+    if (rc)
+        return -1;
 
-    struct rbh_value_pair xattr_pair = {
-        .key = name,
-        .value = xattr_value,
-    };
-
-    struct rbh_value_map *xattrs = data;
-    struct rbh_value_pair *pairs = (struct rbh_value_pair *)xattrs->pairs;
-
-    pairs[xattrs->count] = xattr_pair;
     xattrs->count++;
 
     return 0;
@@ -57,14 +48,18 @@ rbh_get_xattrs(char *_name, char *_value,size_t value_len,
 
 struct rbh_value_map
 get_xattrs_from_inode(ext2_filsys fs, struct ext2_inode_large *inode,
-                      ext2_ino_t ino)
+                      ext2_ino_t ino, struct rbh_sstack *sstack)
 {
     struct rbh_value_map default_ret = {0};
     struct ext2_xattr_handle *handle;
     struct rbh_value_pair *pairs;
+    struct xattr_iter_data data;
     struct rbh_value_map ret;
     errcode_t err;
     size_t count;
+
+    data.values = &ret;
+    data.sstack = sstack;
 
     err = ext2fs_xattrs_open(fs, ino, &handle);
     if (err)
@@ -78,12 +73,12 @@ get_xattrs_from_inode(ext2_filsys fs, struct ext2_inode_large *inode,
     if (err)
         goto err;
 
-    pairs = xmalloc(count * sizeof(struct rbh_value_pair));
+    pairs = rbh_sstack_alloc(sstack, NULL, count * sizeof(struct rbh_value_pair));
 
     ret.count = 0;
     ret.pairs = pairs;
 
-    err = ext2fs_xattrs_iterate(handle, &rbh_get_xattrs, &ret);
+    err = ext2fs_xattrs_iterate(handle, rbh_get_xattrs, &data);
     if (err)
         goto err;
 
