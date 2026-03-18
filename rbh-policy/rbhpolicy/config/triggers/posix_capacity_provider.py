ede2da7b765a6cc2699f48e19354bfcd31308e8d
Arnaud GAUTHERET <arnaud.gautheret@cea.fr>
2026-03-18 12:02:03 +0100

    rbh-policy: Add capacity provider + POSIX implementation
    This patch adds a generic capacity provider framework used by
    trigger evaluation. It also introduces a POSIX capacity provider
    capable of extracting capacity metrics from local mount points,
    with automatic resolution based on the backend type and filesystem
    URI.
    
    Change-Id: I8c702d24b33df66f603513b11855bc0de68be5ee
    Signed-off-by: Arnaud GAUTHERET <arnaud.gautheret@cea.fr>

---
+++ rbh-policy/rbhpolicy/config/triggers/posix_capacity_provider.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# This file is part of RobinHood
+# Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+#            alternatives
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+import os
+from typing import Optional
+
+from rbhpolicy.config.triggers.capacity_providers import (
+    CapacityMetrics,
+    CapacityProvider,
+    register_capacity_provider_resolver,
+)
+from rbhpolicy.config.cpython import (
+    resolve_backend_type,
+    resolve_backend_mountpoint,
+)
+
+class PosixCapacityProvider(CapacityProvider):
+    """Capacity provider for POSIX-backed filesystems."""
+
+    def __init__(self, mount_path: str):
+        if not os.path.isabs(mount_path):
+            raise ValueError("mount_path must be an absolute path")
+        self.mount_path = mount_path
+
+    def get_capacity(self) -> CapacityMetrics:
+        vfs = os.statvfs(self.mount_path)
+        total_bytes = vfs.f_blocks * vfs.f_frsize
+        free_bytes = vfs.f_bfree * vfs.f_frsize
+        used_bytes = total_bytes - free_bytes
+
+        total_inodes = vfs.f_files
+        free_inodes = vfs.f_ffree
+        used_inodes = total_inodes - free_inodes
+
+        return CapacityMetrics(
+            used_bytes=used_bytes,
+            total_bytes=total_bytes,
+            used_inodes=used_inodes,
+            total_inodes=total_inodes,
+        )
+
+def _extract_posix_path(database_uri: Optional[str]) -> Optional[str]:
+    if not database_uri:
+        return None
+
+    source_backend = resolve_backend_type(database_uri)
+    if source_backend != "posix":
+        return None
+
+    mountpoint = resolve_backend_mountpoint(database_uri)
+    if mountpoint and os.path.isabs(mountpoint):
+        return mountpoint
+
+    return None
+
+def _resolve_posix_provider(database_uri: Optional[str]) -> Optional[CapacityProvider]:
+    path = _extract_posix_path(database_uri)
+    if path is None:
+        return None
+    return PosixCapacityProvider(path)
+
+register_capacity_provider_resolver(_resolve_posix_provider)
