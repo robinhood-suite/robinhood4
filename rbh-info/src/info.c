e8315260131b1b260807cd557a39be7c16dbfe0e
VALERI Yoann <yoann.valeri@cea.fr>
2026-04-03 22:31:55 +0200

    info: improve info listing
    This patch improves the info listing, from available info being shown to
    result values, adding a header and mutualizing functions.
    
    Change-Id: Ia4cf0bcee17a12a64dffff62e1cd309c07d74c93
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1234933
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- rbh-info/src/info.c
+++ rbh-info/src/info.c
@@ -12,39 +12,61 @@
 #define WIDTH 32
 
 struct rbh_info_fields {
-    char *field_name;
-    void (*value_function)(const struct rbh_value *value);
+    enum rbh_info flag;
+    const char *helper;
+    const char *header;
+    const char *field;
+    void (*value_function)(const struct rbh_value *, const char *);
 };
 
 static void
-_get_collection_avg_obj_size(const struct rbh_value *value)
+_get_size_value(const struct rbh_value *value, const char *header)
 {
     char _buffer[32];
     char *buffer;
 
     buffer = _buffer;
 
-    size_printer(buffer, sizeof(_buffer), value->int32);
+    if (value->type == RBH_VT_INT32)
+        size_printer(buffer, sizeof(_buffer), value->int32);
+    else if (value->type == RBH_VT_INT64)
+        size_printer(buffer, sizeof(_buffer), value->int64);
 
-    printf("%s\n", buffer);
+    printf("%s: %s\n", header, buffer);
+}
+
+static void
+_get_int_value(const struct rbh_value *value, const char *header)
+{
+    if (value->type == RBH_VT_INT32)
+        printf("%s: %d\n", header, value->int32);
+    else if (value->type == RBH_VT_INT64)
+        printf("%s: %ld\n", header, value->int64);
 }
 
 static void
 print_conn_param(const struct rbh_value_map *map)
 {
-    printf("Connection parameters used for this plugin:\n");
+    printf("   Connection parameters used for this plugin:\n");
     for (int i = 0; i < map->count; i++) {
         const struct rbh_value_pair *pair = &map->pairs[i];
 
         assert(pair->value->type == RBH_VT_STRING);
-        printf("%s: %s\n", pair->key, pair->value->string);
+        printf("   - %s: %s\n", pair->key, pair->value->string);
     }
 }
 
 static void
-_get_backend_source(const struct rbh_value *value)
+_get_backend_source(const struct rbh_value *value, const char *header)
 {
     assert(value->type == RBH_VT_SEQUENCE);
+
+    if (value->sequence.count == 0) {
+        printf("%s: no source backend found\n", header);
+        return;
+    }
+
+    printf("%s:\n", header);
 
     for (size_t i = 0; i < value->sequence.count; i++) {
         const struct rbh_value_map *submap = &value->sequence.values[i].map;
@@ -65,10 +87,11 @@ _get_backend_source(const struct rbh_value *value)
                             "plugin") == 0);
 
         for (j = 0; j < submap->count; j++) {
-            if ((is_plugin && strcmp(submap->pairs[j].key, "plugin") == 0) ||
-                (!is_plugin && strcmp(submap->pairs[j].key, "extension") == 0))
-                printf("%s: %s\n", is_plugin ? "Plugin" : "Extension",
-                       submap->pairs[j].value->string);
+            if (is_plugin && strcmp(submap->pairs[j].key, "plugin") == 0)
+                printf(" - Plugin: %s\n", submap->pairs[j].value->string);
+
+            if (!is_plugin && strcmp(submap->pairs[j].key, "extension") == 0)
+                printf("   - Extension: %s\n", submap->pairs[j].value->string);
 
             if (is_plugin && strcmp(submap->pairs[j].key, "param") == 0)
                 print_conn_param(&submap->pairs[j].value->map);
@@ -77,37 +100,20 @@ _get_backend_source(const struct rbh_value *value)
 }
 
 static void
-_get_collection_count(const struct rbh_value *value)
+_get_collection_sync(const struct rbh_value *value, const char *header)
 {
-    printf("%ld\n", value->int64);
-}
-
-static void
-_get_collection_size(const struct rbh_value *value)
-{
-    char _buffer[32];
-    char *buffer;
-
-    buffer = _buffer;
-
-    size_printer(buffer, sizeof(_buffer), value->int32);
-
-    printf("%s\n", buffer);
-}
-
-static void
-_get_collection_sync(const struct rbh_value *value)
-{
-    assert(value->type == RBH_VT_MAP);
-
     const struct rbh_value_map metadata_map = value->map;
     time_t time;
+
+    assert(value->type == RBH_VT_MAP);
+
+    printf("%s:\n", header);
 
     for (size_t i = 0 ; i < metadata_map.count ; i++) {
         const struct rbh_value_pair *pair = &metadata_map.pairs[i];
         if (strcmp(pair->key, "sync_debut") == 0) {
             time = (time_t)pair->value->int64;
-            printf("%-*s %s\n", WIDTH, "Start of the sync:",
+            printf(" - %-*s %s\n", WIDTH, "Start of the sync:",
                    time_from_timestamp(&time));
         }
 
@@ -121,54 +127,102 @@ _get_collection_sync(const struct rbh_value *value)
 
             difftime_printer(buffer, bufsize, pair->value->int64);
 
-            printf("%-*s %s\n", WIDTH, "Duration of the sync:",
+            printf(" - %-*s %s\n", WIDTH, "Duration of the sync:",
                    buffer);
         }
 
         if (strcmp(pair->key, "sync_end") == 0) {
             time = (time_t)pair->value->int64;
-            printf("%-*s %s\n", WIDTH, "End of the sync:",
+            printf(" - %-*s %s\n", WIDTH, "End of the sync:",
                    time_from_timestamp(&time));
         }
 
         if (strcmp(pair->key, "mountpoint") == 0)
-            printf("%-*s %s\n", WIDTH, "Mountpoint used for the sync:",
+            printf(" - %-*s %s\n", WIDTH, "Mountpoint used for the sync:",
                    pair->value->string);
 
         if (strcmp(pair->key, "command_line") == 0)
-            printf("%-*s %s\n", WIDTH, "Command used for the sync:",
+            printf(" - %-*s %s\n", WIDTH, "Command used for the sync:",
                    pair->value->string);
 
         if (strcmp(pair->key, "converted_entries") == 0)
-            printf("%-*s %ld\n", WIDTH, "Amount of entries converted:",
+            printf(" - %-*s %ld\n", WIDTH, "Amount of entries converted:",
                    pair->value->int64);
 
         if (strcmp(pair->key, "skipped_entries") == 0)
-            printf("%-*s %ld\n", WIDTH, "Amount of entries skipped:",
+            printf(" - %-*s %ld\n", WIDTH, "Amount of entries skipped:",
                    pair->value->int64);
 
         if (strcmp(pair->key, "total_entries_seen") == 0)
-            printf("%-*s %ld\n", WIDTH,
+            printf(" - %-*s %ld\n", WIDTH,
                    "Total entries seen by the sync:",
                    pair->value->int64);
     }
 }
 
 static void
-_get_string_value(const struct rbh_value *value)
+_get_string_value(const struct rbh_value *value, const char *header)
 {
-    printf("%s\n", value->string);
+    printf("%s: %s\n", header, value->string);
 }
 
 static struct rbh_info_fields INFO_FIELDS[] = {
-    { "average_object_size", _get_collection_avg_obj_size },
-    { "backend_source", _get_backend_source},
-    { "command_backend", _get_string_value },
-    { "count", _get_collection_count },
-    { "first_sync", _get_collection_sync },
-    { "last_sync", _get_collection_sync },
-    { "mountpoint", _get_string_value },
-    { "size", _get_collection_size },
+    {
+        RBH_INFO_AVG_OBJ_SIZE,
+        "-a: print the average size of the entries in the mirror",
+        "Average object size",
+        "average_object_size",
+        _get_size_value
+    },
+    {
+        RBH_INFO_BACKEND_SOURCE,
+        "-b: print the backend sources of the mirror",
+        "Source backends",
+        "backend_source",
+        _get_backend_source
+    },
+    {
+        RBH_INFO_COMMAND_BACKEND,
+        "-B: print the backend specified in the last rbh-sync",
+        "Last backend",
+        "command_backend",
+        _get_string_value
+    },
+    {
+        RBH_INFO_COUNT,
+        "-c: print the amount of entries inside the mirror",
+        "Mirror entry count",
+        "count",
+        _get_int_value
+    },
+    {
+        RBH_INFO_FIRST_SYNC,
+        "-f: print info about the first rbh-sync done",
+        "First sync",
+        "first_sync",
+        _get_collection_sync
+    },
+    {
+        RBH_INFO_LAST_SYNC,
+        "-l: print info about the last rbh-sync done",
+        "Last sync",
+        "last_sync",
+        _get_collection_sync
+    },
+    {
+        RBH_INFO_MOUNTPOINT,
+        "-m: print info about the mountpoint of the last rbh-sync",
+        "Last mountpoint",
+        "mountpoint",
+        _get_string_value
+    },
+    {
+        RBH_INFO_SIZE,
+        "-s: print the size of the mirror backend",
+        "Mirror backend size",
+        "size",
+        _get_size_value
+    },
 };
 
 int
@@ -186,8 +240,9 @@ print_info_fields(struct rbh_backend *from, int flags)
         const struct rbh_value_pair *pair = &info_map->pairs[i];
 
         for (size_t j = 0 ; j < field_count ; j++)
-            if (strcmp(pair->key, INFO_FIELDS[j].field_name) == 0)
-                INFO_FIELDS[j].value_function(pair->value);
+            if (strcmp(pair->key, INFO_FIELDS[j].field) == 0)
+                INFO_FIELDS[j].value_function(pair->value,
+                                              INFO_FIELDS[j].header);
     }
 
     return 0;
@@ -196,6 +251,7 @@ print_info_fields(struct rbh_backend *from, int flags)
 void
 info_translate(const struct rbh_backend_plugin *plugin)
 {
+    size_t field_count = sizeof(INFO_FIELDS) / sizeof(INFO_FIELDS[0]);
     const uint8_t info = plugin->info;
 
     if (!info) {
@@ -205,24 +261,7 @@ info_translate(const struct rbh_backend_plugin *plugin)
     }
 
     printf("Available info for plugin '%s': \n", plugin->plugin.name);
-    if (info & RBH_INFO_AVG_OBJ_SIZE)
-        printf("- a: give the average size of objects inside entries collection\n");
-
-    if (info & RBH_INFO_BACKEND_SOURCE)
-        printf("- b: give the backend sources of the backend\n");
-
-    if (info & RBH_INFO_COMMAND_BACKEND)
-        printf("- B: give the backend specified in the last command\n");
-
-    if (info & RBH_INFO_COUNT)
-        printf("- c: retrieve the amount of document inside entries collection\n");
-
-    if (info & RBH_INFO_SIZE)
-        printf("- s: size of entries collection\n");
-
-    if (info & RBH_INFO_FIRST_SYNC)
-        printf("- f: info about the first rbh-sync done\n");
-
-    if (info & RBH_INFO_LAST_SYNC)
-        printf("- y: info about the last rbh-sync done\n");
+    for (size_t i = 0 ; i < field_count ; i++)
+        if (info & INFO_FIELDS[i].flag)
+            printf("%s\n", INFO_FIELDS[i].helper);
 }
