9da112502f44e885a109346b230ed848f98cc7a2
Nikolai MONTICELLI <nikolai.monticelli@cea.fr>
2026-06-10 13:46:31 +0200

    mongo: support fielded elemMatch subfilters
    The Mongo backend currently ignores subfilter fields when
    generating $elemMatch expressions. It only supports arrays
    of scalar values.
    
    Add an explicit boolean to the rbh_filter->array sub-structure
    to specify whether to use filter_field or not.
    
    Change-Id: Ie30a65fbb9330c63b1902223acb9f5cd83ff3749

--- librobinhood/src/filter.c
+++ librobinhood/src/filter.c
@@ -592,14 +592,16 @@ rbh_filter_exists_new(const struct rbh_filter_field *field)
 struct rbh_filter *
 rbh_filter_array_new(enum rbh_filter_operator op,
                      const struct rbh_filter_field *field,
-                     const struct rbh_filter * const *filters, size_t count)
+                     const struct rbh_filter * const *filters, size_t count,
+                     bool use_subfilters_fields)
 {
     const struct rbh_filter ARRAY = {
         .op = op,
         .array = {
             .field = *field,
             .filters = filters,
-            .count = count
+            .count = count,
+            .use_subfilters_fields = use_subfilters_fields
         },
     };
 
@@ -614,9 +616,9 @@ rbh_filter_array_new(enum rbh_filter_operator op,
 struct rbh_filter *
 rbh_filter_array_elemmatch_new(const struct rbh_filter_field *field,
                                const struct rbh_filter * const *filters,
-                               size_t count)
+                               size_t count, bool use_subfilters_fields)
 {
-    return rbh_filter_array_new(RBH_FOP_ELEMMATCH, field, filters, count);
+    return rbh_filter_array_new(RBH_FOP_ELEMMATCH, field, filters, count, use_subfilters_fields);
 }
 
 struct rbh_filter *
