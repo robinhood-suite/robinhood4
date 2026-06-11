52ef56d1df9028f8ac979a52c5f85338966b9b27
Nikolai MONTICELLI <nikolai.monticelli@cea.fr>
2026-06-11 11:29:06 +0200

    selinux: add POSIX enricher tests
    Add rbh-sync tests for selinux enrichment.
    
    Change-Id: I0f80ec744ee45098d4b08cc26d25d12ea0b35556
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1238867
    Tested-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-sync/tests/selinux/selinux_utils.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood
+# Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+
+################################################################################
+#                               SELINUX UTILS                                  #
+################################################################################
+
+
+get_selinux_context()
+{
+    getfattr -n security.selinux -e text "$1" 2>/dev/null |
+        sed -n '/^security\.selinux=/p' |
+        sed 's/^security\.selinux="//; s/"$//'
+}
+
+set_selinux_range()
+{
+    local range="$1"
+    local file="$2"
+    local ctx
+
+    sudo chcon -l "$range" "$file" 2>/dev/null ||
+        error "failed to set SELinux range '$range' on '$file'"
+
+    ctx=$(get_selinux_context "$file")
+
+    if [[ "$ctx" != *":$range" ]]; then
+        error "SELinux range '$range' was not applied to '$file': '$ctx'"
+    fi
+}
+
+rbh_sync_selinux()
+{
+    rbh_sync "rbh:selinux:$1" "$2" $3
+}
