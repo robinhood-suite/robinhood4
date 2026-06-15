7a22ff6c467c9cb7bea84cb21aa68573559a6eb8
Nikolai MONTICELLI <nikolai.monticelli@cea.fr>
2026-06-15 14:33:32 +0200

    selinux: add basic find filters
    Add filters that allow rbh-find to query entries by SELinux context,
    user, role, type and range.
    
    Change-Id: Iaed4fea13b2de8aed4c4f0d9d2be00025a805bf3

---
+++ librobinhood/src/plugins/posix/enrichers/selinux/parser.h
@@ -0,0 +1,45 @@
+/* This file is part of RobinHood
+ * Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#ifndef RBH_EXTENSION_SELINUX_PARSER_H
+#define RBH_EXTENSION_SELINUX_PARSER_H
+
+enum selinux_predicate {
+    SPRED_SELINUX_CTX,
+    SPRED_SELINUX_USER,
+    SPRED_SELINUX_ROLE,
+    SPRED_SELINUX_TYPE,
+    SPRED_SELINUX_RANGE,
+
+    SPRED_MIN = SPRED_SELINUX_CTX,
+    SPRED_MAX = SPRED_SELINUX_RANGE,
+};
+
+/**
+ * str2selinux_predicate - convert a string to an integer corresponding to a
+ * selinux_predicate
+ *
+ * @param string          a string representing a valid predicate
+ *
+ * @return                the predicate that \p string represents
+ *
+ * This function will exit if \p string is not a valid predicate
+ */
+int
+str2selinux_predicate(const char *string);
+
+/**
+ * selinux_predicate2str - convert a selinux_predicate to a string
+ *
+ * @param predicate a predicate
+ *
+ * @return          the string that represents \p predicate
+ */
+const char *
+selinux_predicate2str(int predicate);
+
+#endif
