3bf0ae88b928f33feeb81cb6856a53194672aee4
Nikolai MONTICELLI <nikolai.monticelli@cea.fr>
2026-06-16 15:49:50 +0200

    selinux: add range dominance filter
    Add -selinux-range-dominates filter to query entries whose
    SELinux range dominates a given level.
    
    Change-Id: Ica37b9395a2c689ca535244892b390b180df454e

--- librobinhood/src/plugins/posix/enrichers/selinux/filter.c
+++ librobinhood/src/plugins/posix/enrichers/selinux/filter.c
@@ -12,23 +12,34 @@
 #include <stdlib.h>
 #include <sysexits.h>
 
+#include <robinhood/utils.h>
 #include <robinhood/filter.h>
 #include <robinhood/filters/core.h>
 
 #include "parser.h"
+#include "range_parser.h"
 #include "selinux_internals.h"
 
+
 static const struct rbh_filter_field predicate2filter_field[] = {
-    [SPRED_SELINUX_CTX]   = {.fsentry = RBH_FP_INODE_XATTRS,
-                             .xattr = "selinux.context"},
-    [SPRED_SELINUX_USER]  = {.fsentry = RBH_FP_INODE_XATTRS,
-                             .xattr = "selinux.user"},
-    [SPRED_SELINUX_ROLE]  = {.fsentry = RBH_FP_INODE_XATTRS,
-                             .xattr = "selinux.role"},
-    [SPRED_SELINUX_TYPE]  = {.fsentry = RBH_FP_INODE_XATTRS,
-                             .xattr = "selinux.type"},
-    [SPRED_SELINUX_RANGE] = {.fsentry = RBH_FP_INODE_XATTRS,
-                             .xattr = "selinux.range"},
+    [SPRED_SELINUX_CTX]       = {.fsentry = RBH_FP_INODE_XATTRS,
+                                 .xattr = "selinux.context"},
+    [SPRED_SELINUX_USER]      = {.fsentry = RBH_FP_INODE_XATTRS,
+                                 .xattr = "selinux.user"},
+    [SPRED_SELINUX_ROLE]      = {.fsentry = RBH_FP_INODE_XATTRS,
+                                 .xattr = "selinux.role"},
+    [SPRED_SELINUX_TYPE]      = {.fsentry = RBH_FP_INODE_XATTRS,
+                                 .xattr = "selinux.type"},
+    [SPRED_SELINUX_RANGE]     = {.fsentry = RBH_FP_INODE_XATTRS,
+                                 .xattr = "selinux.range"},
+    [SELINUX_HIGH_SENS_FIELD] = {.fsentry = RBH_FP_INODE_XATTRS,
+                                 .xattr = "selinux.high_sens"},
+    [SELINUX_HIGH_CAT_FIELD]  = {.fsentry = RBH_FP_INODE_XATTRS,
+                                 .xattr = "selinux.high_cat"},
+    [INTERVAL_LAST_FIELD]     = {.fsentry = RBH_FP_INODE_XATTRS,
+                                 .xattr = "l"},
+    [INTERVAL_FIRST_FIELD]    = {.fsentry = RBH_FP_INODE_XATTRS,
+                                 .xattr = "f"},
 };
 
 static struct rbh_filter *
@@ -45,20 +56,82 @@ string_predicate2filter(int predicate, const char *arg)
     return filter;
 }
 
-static bool
-predicate_has_argument(int predicate)
+
+static struct rbh_filter *
+interval_cover_filter(const struct selinux_interval *interval)
 {
-    switch(predicate) {
-        case SPRED_SELINUX_CTX:
-        case SPRED_SELINUX_USER:
-        case SPRED_SELINUX_ROLE:
-        case SPRED_SELINUX_TYPE:
-        case SPRED_SELINUX_RANGE:
-            return true;
-        default:
-            return false;
-    }
+    const struct rbh_filter *subfilters[2];
+    struct rbh_filter *filter;
+    struct rbh_filter *first;
+    struct rbh_filter *last;
+
+    first = rbh_filter_compare_int32_new(
+                RBH_FOP_LOWER_OR_EQUAL,
+                &predicate2filter_field[INTERVAL_FIRST_FIELD],
+                interval->first);
+
+    if (first == NULL)
+        error(EXIT_FAILURE, errno, "rbh_filter_compare_int32_new");
+
+    last = rbh_filter_compare_int32_new(
+               RBH_FOP_GREATER_OR_EQUAL,
+               &predicate2filter_field[INTERVAL_LAST_FIELD],
+               interval->last);
+
+    if (last == NULL)
+        error(EXIT_FAILURE, errno, "rbh_filter_compare_int32_new");
+
+    subfilters[0] = first;
+    subfilters[1] = last;
+
+    filter = rbh_filter_array_elemmatch_new(
+                &predicate2filter_field[SELINUX_HIGH_CAT_FIELD],
+                subfilters, 2, true);
+
+    if (filter == NULL)
+        error(EXIT_FAILURE, errno, "rbh_filter_array_elemmatch_new");
+
+    free(first);
+    free(last);
+
+    return filter;
 }
+
+
+static struct rbh_filter *
+selinux_dominates_filter(const char *arg)
+{
+    struct selinux_level level;
+    struct rbh_filter *filter;
+    char *copy;
+
+    copy = xstrdup(arg);
+
+    if (selinux_parse_level(copy, &level)) {
+        free(copy);
+        error(EX_USAGE, errno, "invalid SELinux level `%s'", arg);
+    }
+
+    free(copy);
+
+    filter = rbh_filter_compare_int32_new(
+                RBH_FOP_GREATER_OR_EQUAL,
+                &predicate2filter_field[SELINUX_HIGH_SENS_FIELD],
+                level.sens);
+
+    if (filter == NULL)
+        error(EXIT_FAILURE, errno, "rbh_filter_compare_int32_new");
+
+    for (size_t i = 0; i < level.intervals_count; i++) {
+        struct rbh_filter *cat_filter;
+
+        cat_filter = interval_cover_filter(&level.intervals[i]);
+        filter = rbh_filter_and(filter, cat_filter);
+    }
+
+    return filter;
+}
+
 
 struct rbh_filter *
 rbh_selinux_build_filter(struct filters_context *context, int *index)
@@ -71,7 +144,7 @@ rbh_selinux_build_filter(struct filters_context *context, int *index)
 
     predicate = str2selinux_predicate(argv[i]);
 
-    if (predicate_has_argument(predicate) && i + 1 >= argc)
+    if (i + 1 >= argc)
         error(EX_USAGE, 0, "missing argument to `%s'", argv[i]);
 
     /* In the following block, functions should call error() themselves rather
@@ -88,6 +161,9 @@ rbh_selinux_build_filter(struct filters_context *context, int *index)
         case SPRED_SELINUX_RANGE:
             filter = string_predicate2filter(predicate, argv[++i]);
         break;
+        case SPRED_SELINUX_RANGE_DOMINATES:
+            filter = selinux_dominates_filter(argv[++i]);
+            break;
         default:
             error(EX_USAGE, 0, "invalid filter found `%s'", argv[i]);
     }
