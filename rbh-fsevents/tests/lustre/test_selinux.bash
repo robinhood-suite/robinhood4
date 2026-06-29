788a82be1c107eaa2041e5d3c4e7d920883d02f3
Nikolai MONTICELLI <nikolai.monticelli@cea.fr>
2026-06-29 14:55:17 +0200

    selinux: add rbh-fsevents test
    Add a rbh-fsevents test checking that rbh-fsevents enriches SELinux
    metadata when security.selinux is updated and logged in a Lustre
    changelog.
    
    Change-Id: I3ff06668e5b0a09d1e1d63ad340f2d7cd08ef72f
    Signed-off-by: Nikolai MONTICELLI <nikolai.monticelli@cea.fr>

---
+++ rbh-fsevents/tests/lustre/test_selinux.bash
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood
+# Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../../utils/tests/framework.bash
+. $test_dir/lustre_utils.bash
+
+invoke_rbh-fsevents_selinux()
+{
+    rbh_fsevents --enrich rbh:lustre-selinux:"$LUSTRE_DIR" \
+        src:lustre:"$LUSTRE_MDT"?ack-user=$userid "rbh:$db:$testdb"
+}
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_selinux()
+{
+    local entry="test_file"
+
+    touch "$entry"
+
+    chcon -l "s2:c2,c5-s8:c2,c5" "$entry"
+
+    invoke_rbh-fsevents_selinux
+
+    find_attribute '"xattrs.selinux.context":{$exists:true}' \
+        '"ns.name":"'$entry'"'
+
+    find_attribute '"xattrs.selinux.range":"s2:c2,c5-s8:c2,c5"' \
+        '"ns.name":"'$entry'"'
+
+    find_attribute '"xattrs.selinux.high_sens":8' \
+        '"ns.name":"'$entry'"'
+
+    chcon -l "s2:c2,c5-s12:c2,c5,c12,c14" "$entry"
+
+    invoke_rbh-fsevents_selinux
+
+    find_attribute '"xattrs.selinux.range":"s2:c2,c5-s12:c2,c5,c12,c14"' \
+        '"ns.name":"'$entry'"'
+
+    find_attribute '"xattrs.selinux.high_sens":12' \
+        '"ns.name":"'$entry'"'
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_selinux)
+
+LUSTRE_DIR=/mnt/lustre/
+cd "$LUSTRE_DIR"
+
+LUSTRE_MDT=lustre-MDT0000
+userid="$(start_changelogs "$LUSTRE_MDT")"
+
+tmpdir=$(mktemp --directory --tmpdir=$LUSTRE_DIR)
+lfs setdirstripe -D -i 0 $tmpdir
+trap -- "rm -rf '$tmpdir'; stop_changelogs '$LUSTRE_MDT' '$userid'" EXIT
+cd "$tmpdir"
+
+sub_setup=lustre_setup
+sub_teardown=lustre_teardown
+run_tests "${tests[@]}"
