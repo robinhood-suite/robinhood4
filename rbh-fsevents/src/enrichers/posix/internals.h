ac7b7ced3fe2a0a7aaac8ccf66bf3cdd2c21c00a
Nikolai MONTICELLI <nikolai.monticelli@cea.fr>
2026-06-29 11:26:29 +0200

    selinux: add build option
    Add a Meson option to enable SELinux enricher explicitly.
    
    Change-Id: I1f6e046af2a46b9066969dae0ae255f13c5d6974
    Signed-off-by: Nikolai MONTICELLI <nikolai.monticelli@cea.fr>

--- rbh-fsevents/src/enrichers/posix/internals.h
+++ rbh-fsevents/src/enrichers/posix/internals.h
@@ -58,11 +58,13 @@ sparse_enrich_fsevent(struct enricher *enricher,
                       struct rbh_posix_enrich_ctx *ctx,
                       const struct rbh_fsevent *original);
 
+#ifdef HAVE_SELINUX
 int
 selinux_enrich_fsevent(struct enricher *enricher,
                        const struct enrich_request *req,
                        struct rbh_posix_enrich_ctx *ctx,
                        const struct rbh_fsevent *original);
+#endif
 
 struct enricher {
     struct rbh_iterator iterator;
