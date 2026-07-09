36913d9b9fb726a29d558163aedadf33fffbc1a3
VALERI Yoann <yoann.valeri@cea.fr>
2026-07-09 17:30:29 +0200

    mongo: allow retrieving more than one log
    This patch updates the mongo backend to allow retrieval of more than one
    log by iterating on the database cursor more than once. We now also use
    the count provided in the log options structure to limit how many logs
    are retrieved.
    
    Change-Id: If9aaa5ba799b1c7bcfbe443a20946b1be5c7b349
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1241578
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: MONTICELLI Nikolai <nikolai.monticelli@cea.fr>

--- librobinhood/src/plugins/mongo/logs.c
+++ librobinhood/src/plugins/mongo/logs.c
@@ -27,67 +27,107 @@ destroy_sstack(void)
 
 static int
 get_collection_sync(const struct mongo_backend *mongo, char *field_to_find,
-                    struct rbh_value_pair *pair)
+                    struct rbh_value_pair *pair, size_t *count)
 {
-    struct rbh_value *value;
     mongoc_cursor_t *cursor;
+    struct rbh_value value;
     bson_t *opts = NULL;
+    bson_error_t error;
     char _buffer[4096];
     const bson_t *doc;
     bson_iter_t iter;
     bson_t *filter;
     size_t bufsize;
+    int index = 0;
     char *buffer;
     int rc = 0;
 
     buffer = _buffer;
     filter = bson_new();
     bufsize = sizeof(_buffer);
-    value = RBH_SSTACK_PUSH(logs_sstack, NULL, sizeof(*value));
 
     if (strcmp(field_to_find, "first_sync") == 0)
-        opts = BCON_NEW("sort", "{", "_id", BCON_INT32(1), "}");
+        opts = BCON_NEW("limit", BCON_INT64(*count),
+                        "sort", "{", "_id", BCON_INT32(1), "}");
 
     if (strcmp(field_to_find, "last_sync") == 0)
-        opts = BCON_NEW("sort", "{", "_id", BCON_INT32(-1), "}");
+        opts = BCON_NEW("limit", BCON_INT64(*count),
+                        "sort", "{", "_id", BCON_INT32(-1), "}");
 
     cursor = mongoc_collection_find_with_opts(mongo->log, filter, opts, NULL);
     if (!cursor) {
-        rc = -1;
-        goto out;
-    }
-
-    if (!mongoc_cursor_next(cursor, &doc)) {
         rc = 1;
         goto out;
     }
 
-    if (!bson_iter_init(&iter, doc)) {
-        rc = 1;
-        goto out;
-    }
-
-    while (bson_iter_next(&iter)) {
-        const char *key = bson_iter_key(&iter);
-
-        if (strcmp(key, "sync_metadata") == 0) {
-            if (!bson_iter_rbh_value(&iter, value, &buffer, &bufsize)) {
+    for (index = 0; index < *count; ++index) {
+        if (!mongoc_cursor_more(cursor)) {
+            if (mongoc_cursor_error(cursor, &error)) {
                 rc = 1;
-                goto out;
+                goto handle_error;
             }
 
-            pair->key = field_to_find;
-            pair->value = value_clone(value);
+            rc = 0;
+            break;
+        }
+
+        if (!mongoc_cursor_next(cursor, &doc)) {
+            if (mongoc_cursor_error(cursor, &error)) {
+                rc = 1;
+                goto handle_error;
+            }
+
+            rc = 0;
+            break;
+        }
+
+        if (!bson_iter_init(&iter, doc)) {
+            rc = 1;
+            goto out;
+        }
+
+        while (bson_iter_next(&iter)) {
+            const char *key = bson_iter_key(&iter);
+
+            if (strcmp(key, "sync_metadata") == 0) {
+                if (!bson_iter_rbh_value(&iter, &value, &buffer, &bufsize)) {
+                    rc = 1;
+                    goto out;
+                }
+
+                pair[index].key = field_to_find;
+                pair[index].value = value_clone(&value);
+            }
         }
     }
+
+    *count = index;
 
 out:
     if (cursor)
         mongoc_cursor_destroy(cursor);
-    if (filter)
-        bson_destroy(filter);
-    if (opts)
-        bson_destroy(opts);
+    bson_destroy(filter);
+    bson_destroy(opts);
+
+    return rc;
+
+handle_error:
+    mongoc_cursor_destroy(cursor);
+    bson_destroy(filter);
+    bson_destroy(opts);
+
+    switch (error.domain) {
+    case MONGOC_ERROR_SERVER_SELECTION:
+        switch (error.code) {
+        case MONGOC_ERROR_SERVER_SELECTION_FAILURE:
+            errno = ENOTCONN;
+            return 1;
+        }
+        break;
+    }
+    snprintf(rbh_backend_error, sizeof(rbh_backend_error), "%d.%d: %s",
+             error.domain, error.code, error.message);
+    errno = RBH_BACKEND_ERROR;
 
     return rc;
 }
@@ -98,26 +138,31 @@ mongo_backend_get_logs(void *backend, struct rbh_log_options options)
     struct mongo_backend *mongo = backend;
     struct rbh_value_map *map_value;
     struct rbh_value_pair *pairs;
-    int count = 1;
-    int idx = 0;
+    size_t count;
+
+    count = (options.count > INT64_MAX ? INT64_MAX : options.count);
 
     if (logs_sstack == NULL)
         logs_sstack = rbh_sstack_new(MIN_VALUES_SSTACK_ALLOC *
                                     (sizeof(struct rbh_value_map *)));
 
+    /* XXX: since we might request more logs than available, there will be more
+     * pairs allocated than necessary. But since this is a one-shot command, no
+     * need to care for that.
+     */
     pairs = RBH_SSTACK_PUSH(logs_sstack, NULL, count * sizeof(*pairs));
     map_value = RBH_SSTACK_PUSH(logs_sstack, NULL, sizeof(*map_value));
 
     if (options.ascending) {
-        if (get_collection_sync(mongo, "first_sync", &pairs[idx++]))
+        if (get_collection_sync(mongo, "first_sync", pairs, &count))
             goto out;
     } else {
-        if (get_collection_sync(mongo, "last_sync", &pairs[idx++]))
+        if (get_collection_sync(mongo, "last_sync", pairs, &count))
             goto out;
     }
 
     map_value->pairs = pairs;
-    map_value->count = idx;
+    map_value->count = count;
 
     return map_value;
 
