9b7409f6d7aa2f5fe59439d7f4078e78f5bd5962
VALERI Yoann <yoann.valeri@cea.fr>
2026-07-10 14:37:18 +0200

    log: add log_value enum for printing values in logs
    This patch adds a new enum for printing values in logs. Its goal is
    mainly to avoid doing too many strcmp for each key found in a log.
    
    Change-Id: I6feb23e3a543f5ce5516d3a66cc1f24bd7376f0f
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-log/src/sync.c
+++ rbh-log/src/sync.c
@@ -13,6 +13,61 @@
 
 #include "log.h"
 
+enum log_value {
+    COMMAND_LINE,
+    CONVERTED_ENTRIES,
+    DURATION,
+    END_TIME,
+    SKIPPED_ENTRIES,
+    SOURCE_MOUNTPOINT,
+    START_TIME,
+    TOTAL_ENTRIES,
+};
+
+static enum log_value
+key2log_value(const char *key)
+{
+    switch (key[0]) {
+    case 'c':
+        if (key[1] != 'o')
+            break;
+
+        if (key[2] == 'm' && !strcmp(&key[3], "mand_line"))
+            return COMMAND_LINE;
+        else if (key[2] == 'n' && !strcmp(&key[3], "verted_entries"))
+            return CONVERTED_ENTRIES;
+
+        break;
+    case 'd':
+        if (!strcmp(&key[1], "uration"))
+            return DURATION;
+
+        break;
+    case 'e':
+        if (!strcmp(&key[1], "nd_time"))
+            return END_TIME;
+
+        break;
+    case 's':
+        if (key[1] == 't' && !strcmp(&key[2], "art_time"))
+            return START_TIME;
+        else if (key[1] == 'o' && !strcmp(&key[2], "urce_mountpoint"))
+            return SOURCE_MOUNTPOINT;
+        else if (key[1] == 'k' && !strcmp(&key[2], "ipped_entries"))
+            return SKIPPED_ENTRIES;
+
+        break;
+    case 't':
+        if (!strcmp(&key[1], "otal_entries"))
+            return TOTAL_ENTRIES;
+
+        break;
+    }
+
+    error(EXIT_FAILURE, EINVAL, "Unknown key found in sync log: '%s'", key);
+    __builtin_unreachable();
+}
+
 void
 print_sync_log(const struct rbh_value_map *log, const char *header)
 {
@@ -22,13 +77,15 @@ print_sync_log(const struct rbh_value_map *log, const char *header)
 
     for (size_t i = 0 ; i < log->count ; i++) {
         const struct rbh_value_pair *pair = &log->pairs[i];
-        if (strcmp(pair->key, "start_time") == 0) {
+        enum log_value log_value = key2log_value(pair->key);
+
+        switch (log_value) {
+        case START_TIME:
             time = (time_t)pair->value->int64;
             printf(" - %-*s %s\n", WIDTH, "Start of the sync:",
                    time_from_timestamp(&time));
-        }
-
-        if (strcmp(pair->key, "duration") == 0) {
+            break;
+        case DURATION: {
             char _buffer[32];
             size_t bufsize;
             char *buffer;
@@ -40,33 +97,36 @@ print_sync_log(const struct rbh_value_map *log, const char *header)
 
             printf(" - %-*s %s\n", WIDTH, "Duration of the sync:",
                    buffer);
+            break;
         }
-
-        if (strcmp(pair->key, "end_time") == 0) {
+        case END_TIME:
             time = (time_t)pair->value->int64;
             printf(" - %-*s %s\n", WIDTH, "End of the sync:",
                    time_from_timestamp(&time));
-        }
-
-        if (strcmp(pair->key, "source_mountpoint") == 0)
+            break;
+        case SOURCE_MOUNTPOINT:
             printf(" - %-*s %s\n", WIDTH, "Mountpoint used for the sync:",
                    pair->value->string);
-
-        if (strcmp(pair->key, "command_line") == 0)
+            break;
+        case COMMAND_LINE:
             printf(" - %-*s %s\n", WIDTH, "Command used for the sync:",
                    pair->value->string);
-
-        if (strcmp(pair->key, "converted_entries") == 0)
+            break;
+        case CONVERTED_ENTRIES:
             printf(" - %-*s %ld\n", WIDTH, "Amount of entries converted:",
                    pair->value->int64);
-
-        if (strcmp(pair->key, "skipped_entries") == 0)
+            break;
+        case SKIPPED_ENTRIES:
             printf(" - %-*s %ld\n", WIDTH, "Amount of entries skipped:",
                    pair->value->int64);
-
-        if (strcmp(pair->key, "total_entries") == 0)
+            break;
+        case TOTAL_ENTRIES:
             printf(" - %-*s %ld\n", WIDTH,
                    "Total entries seen by the sync:",
                    pair->value->int64);
+            break;
+        default:
+            __builtin_unreachable();
+        }
     }
 }
