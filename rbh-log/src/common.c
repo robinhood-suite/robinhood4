aca0d1e46c3980a23b3fd82b283148b15dbdf7e3
VALERI Yoann <yoann.valeri@cea.fr>
2026-07-15 17:07:58 +0200

    log: add common enum and print function
    For values that are common to each log, i.e. start time, duration, end
    time and command line. The goal is mainly to reduce code duplication.
    
    Change-Id: Id70c11b97962130e19befe532036efe0dc467949
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-log/src/common.c
+++ rbh-log/src/common.c
@@ -48,3 +48,57 @@ print_value(const struct rbh_value *value, const char *header)
         __builtin_unreachable();
     }
 }
+
+enum common_log_value
+key2common_log_value(const char *key)
+{
+    switch (key[0]) {
+    case 'c':
+        if (!strcmp(&key[1], "ommand_line"))
+            return CLV_COMMAND_LINE;
+
+        break;
+    case 'd':
+        if (!strcmp(&key[1], "uration"))
+            return CLV_DURATION;
+
+        break;
+    case 'e':
+        if (!strcmp(&key[1], "nd_time"))
+            return CLV_END_TIME;
+
+        break;
+    case 's':
+        if (!strcmp(&key[1], "tart_time"))
+            return CLV_START_TIME;
+
+        break;
+    }
+
+    return CLV_UNKNOWN;
+}
+
+static const struct formatted_log_value common_formatted_log_value[] = {
+    [CLV_START_TIME] =    { .key = "start_time",
+                            .header = "Start of the command",
+                            .print_log_value = print_time_from_timestamp },
+    [CLV_DURATION] =      { .key = "duration",
+                            .header = "Duration of the command",
+                            .print_log_value = print_difftime },
+    [CLV_END_TIME] =      { .key = "end_time",
+                            .header = "End of the command",
+                            .print_log_value = print_time_from_timestamp },
+    [CLV_COMMAND_LINE] =  { .key = "command_line",
+                            .header = "Command used",
+                            .print_log_value = print_value },
+};
+
+void
+print_common_log_info(const struct rbh_value *value,
+                      enum common_log_value log_value)
+{
+    struct formatted_log_value formatted_log_value =
+        common_formatted_log_value[log_value];
+
+    formatted_log_value.print_log_value(value, formatted_log_value.header);
+}
