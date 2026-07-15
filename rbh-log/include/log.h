27d298ec307f2f4f9cdcf0fe6ba86c8eb872521e
VALERI Yoann <yoann.valeri@cea.fr>
2026-07-15 17:07:58 +0200

    log: add common enum and print function
    For values that are common to each log, i.e. start time, duration, end
    time and command line. The goal is mainly to reduce code duplication.
    
    Change-Id: Id70c11b97962130e19befe532036efe0dc467949
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1242115
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-by: MONTICELLI Nikolai <nikolai.monticelli@cea.fr>

--- rbh-log/include/log.h
+++ rbh-log/include/log.h
@@ -19,11 +19,40 @@ struct formatted_log_value {
     void (*print_log_value)(const struct rbh_value *, const char *);
 };
 
+enum common_log_value {
+    CLV_UNKNOWN,
+    CLV_COMMAND_LINE,
+    CLV_DURATION,
+    CLV_END_TIME,
+    CLV_START_TIME,
+};
+
+/**
+ * Convert a key to a common log value.
+ *
+ * @param key       the key to convert
+ *
+ * @return          the converted key
+ */
+enum common_log_value
+key2common_log_value(const char *key);
+
+/**
+ * Print the given value as if it were common information about a log.
+ *
+ * Can correspond to the start time, duration, end time and command line.
+ *
+ * @param value      the value whose content should be printed as common log info
+ * @param log_value  the type of information to print
+ */
+void
+print_common_log_info(const struct rbh_value *value,
+                      enum common_log_value log_value);
+
 /**
  * Print a sync log.
  *
- * @param log       the map whose content corresponds to a sync log and should
- *                  be printed
+ * @param log       the map whose content should be printed
  */
 void
 print_sync_log(const struct rbh_value_map *log);
@@ -31,8 +60,7 @@ print_sync_log(const struct rbh_value_map *log);
 /**
  * Print a fsevents log.
  *
- * @param log       the map whose content corresponds to a fsevents log and
- *                  should be printed
+ * @param log       the map whose content should be printed
  */
 void
 print_fsevents_log(const struct rbh_value_map *log);
