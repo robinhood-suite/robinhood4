93e3fd5953a9030b5de2ca7482faf627295f7f66
VALERI Yoann <yoann.valeri@cea.fr>
2026-07-17 10:29:21 +0200

    log: add option to show find logs
    Change-Id: Ib849f065c6444958d69a16b61727859403ac7917
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-log/src/find.c
@@ -0,0 +1,25 @@
+/* This file is part of RobinHood 4
+ * Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+ *                    alternatives
+ *
+ * SPDX-License-Identifer: LGPL-3.0-or-later
+ */
+
+#include <robinhood.h>
+
+#include "log.h"
+
+void
+print_find_log(const struct rbh_value_map *log)
+{
+    for (size_t i = 0 ; i < log->count ; i++) {
+        const struct rbh_value_pair *pair = &log->pairs[i];
+        enum common_log_value common_log_value;
+
+        common_log_value = key2common_log_value(pair->key);
+        if (common_log_value != CLV_UNKNOWN) {
+            print_common_log_info(pair->value, common_log_value);
+            continue;
+        }
+    }
+}
