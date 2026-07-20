0dbf324965d8f855d27a197cc7ed6c5d27a57817
Nikolai MONTICELLI <nikolai.monticelli@cea.fr>
2026-07-20 09:28:48 +0200

    acl: add ACL enricher tests
    Change-Id: I28de5dd475d9974f04a61a393a76aabb5fc4a392
    Signed-off-by: Nikolai MONTICELLI <nikolai.monticelli@cea.fr>

---
+++ rbh-sync/tests/acl/acl_utils.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood
+# Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+
+################################################################################
+#                                   ACL UTILS                                  #
+################################################################################
+
+set_access_acl()
+{
+    local acl="$1"
+    local path="$2"
+
+    setfacl -n -m "$acl" "$path" ||
+        error "Failed to set access ACL '$acl' on '$path'"
+}
+
+set_default_acl()
+{
+    local acl="$1"
+    local path="$2"
+
+    setfacl -d -n -m "$acl" "$path" ||
+        error "Failed to set default ACL '$acl' on '$path'"
+}
+
+rbh_sync_acl()
+{
+    rbh_sync "rbh:acl:$1" "$2" $3
+}
