4eeb11940364a91d65f835778ac0ae5f029cf432
VALERI Yoann <yoann.valeri@cea.fr>
2026-07-27 11:55:14 +0200

    log: add printing of gc logs
    Change-Id: I2173a31cc40eba7f56a921adfed04b8213bd1e25
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-log/include/log.h
+++ rbh-log/include/log.h
@@ -81,6 +81,14 @@ void
 print_report_log(const struct rbh_value_map *log);
 
 /**
+ * Print a gc log.
+ *
+ * @param log       the map whose content should be printed
+ */
+void
+print_gc_log(const struct rbh_value_map *log);
+
+/**
  * All following functions are callback for the `print_log_value` field in the
  * `formatted_log_value` structure. They each take in a `rbh_value` to print
  * and a header.
