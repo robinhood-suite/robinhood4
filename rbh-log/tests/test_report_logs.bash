6eee45b5ccf3cfae584484deb469fbe05ba3f278
VALERI Yoann <yoann.valeri@cea.fr>
2026-07-27 11:43:03 +0200

    log: add report log testing
    Change-Id: Iaa7e4c8954ff70ec8ad7c30ea1fe6bfb71d601df
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

---
+++ rbh-log/tests/test_report_logs.bash
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood.
+# Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../utils/tests/framework.bash
+. $test_dir/common_logs.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_invalid()
+{
+    rbh_sync "rbh:posix:." "rbh:$db:$testdb"
+
+    rbh_log "rbh:$db:$testdb" --last blob &&
+        error "log with invalid last count should have failed"
+
+    rbh_log "rbh:$db:$testdb" --last 42invalid &&
+        error "log with invalid last count should have failed"
+
+    return 0
+}
+
+check_log_result()
+{
+    local output="$1"
+
+    check_common_logs "$output" rbh-report \
+        "rbh-report rbh:$db:$testdb --group-by statx.uid --output sum(statx.size)"
+}
+
+test_N_logs()
+{
+    local requested=$1
+    local expected=$2
+    local count=6
+
+    rbh_sync "rbh:posix:." "rbh:$db:$testdb"
+    sleep 1
+    for i in $(seq 1 $expected); do
+        rbh_report "rbh:$db:$testdb" \
+            --group-by "statx.uid" --output "sum(statx.size)" > /dev/null
+        sleep 1
+    done
+
+    local output=$(rbh_log "rbh:$db:$testdb" --report $requested)
+    local n_lines=$(echo "$output" | wc -l)
+
+    if ((n_lines != $count * $expected)); then
+        error "There should be $count * $expected lines about report ($(($count - 2)) for content, 2 for header/footer, time $expected logs), got '$output'"
+    fi
+
+    for i in $(seq 1 $expected); do
+        local one_log="$(echo "$output" | head -n $count)"
+        check_log_result "$one_log"
+        output="$(echo "$output" | sed 1,${count}d)"
+    done
+}
+
+test_last_1()
+{
+    test_N_logs 1 1
+}
+
+test_last_N()
+{
+    test_N_logs 3 3
+}
+
+test_more_than_N()
+{
+    test_N_logs 6 3
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_invalid test_last_1 test_last_N test_more_than_N)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
