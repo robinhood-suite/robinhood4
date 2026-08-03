153588c47e0ffe61d3d6004e68cf91e924fe200c
Nikolai MONTICELLI <nikolai.monticelli@cea.fr>
2026-08-03 13:55:29 +0200

    acl: add helper test
    Add a test checking that the ACL extension helper exists.
    
    Change-Id: I4bf677e5727f23157635ff400a3ae0f36f7c9b12
    Signed-off-by: Nikolai MONTICELLI <nikolai.monticelli@cea.fr>

---
+++ rbh-find/tests/acl/test_helper.bash
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This file is part of RobinHood
+# Copyright (C) 2026 Commissariat a l'energie atomique et aux energies
+#                    alternatives
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+test_dir=$(dirname $(readlink -e $0))
+. $test_dir/../../../utils/tests/framework.bash
+
+################################################################################
+#                                    TESTS                                     #
+################################################################################
+
+test_helper()
+{
+    rbh_find --help acl ||
+        error "Find's helper with acl extension should have succeeded"
+
+    rbh_find --help acl | grep "ACL" ||
+        error "Find's helper with acl extension should have shown ACL predicates"
+
+    rbh_find --help acl | grep "ACL" ||
+        error "Find's helper with acl extension should have shown POSIX predicates"
+}
+
+################################################################################
+#                                     MAIN                                     #
+################################################################################
+
+declare -a tests=(test_helper)
+
+tmpdir=$(mktemp --directory)
+trap -- "rm -rf '$tmpdir'" EXIT
+cd "$tmpdir"
+
+run_tests ${tests[@]}
