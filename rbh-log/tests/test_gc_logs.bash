6678e9b765cfbba5ce6079265a37acc13edf72d0
VALERI Yoann <yoann.valeri@cea.fr>
2026-08-04 17:08:25 +0200

    log: add timestamps and command line tests for gc logs
    Change-Id: I023c8a8476e5dea5effb388a99b9c34e07cb1a0c
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-log/tests/test_gc_logs.bash
+++ rbh-log/tests/test_gc_logs.bash
@@ -87,11 +87,53 @@ test_more_than_N()
     test_N_logs 6 3
 }
 
+test_timestamps()
+{
+    rbh_sync rbh:posix:. rbh:$db:$testdb
+    check_common_timestamps "--gc" "rbh_gc rbh:$db:$testdb"
+}
+
+test_command_line()
+{
+    local conf="conf"
+    local file="file"
+    local dir="dir"
+    local command="rbh-gc"
+
+    mkdir $dir
+    touch $file
+
+    rbh_sync rbh:posix:. rbh:$db:$testdb
+
+    echo "---
+ mongo:
+     address: \"mongodb://localhost:27017\"
+---" > $conf
+
+    touch blob.sh
+    chmod +x blob.sh
+
+    rbh_gc rbh:$db:$testdb > /dev/null
+    rbh_gc --config $conf rbh:$db:$testdb --dry-run > /dev/null
+    rbh_gc --config $conf rbh:$db:$testdb -s 42 --verbose > /dev/null
+    rbh_gc --config $conf rbh:$db:$testdb -type d -size +3 -s 53 > /dev/null
+    rbh_gc --dry-run rbh:$db:$testdb --check "$PWD/blob.sh"> /dev/null
+
+    rbh_log rbh:$db:$testdb --gc 6 | grep "Command" | cut -d':' -f2- |
+        sed 's/^[ \t]*//' | sed -n "s/.*$command/$command/p" |
+        difflines "rbh-gc --dry-run rbh:$db:$testdb --check $PWD/blob.sh" \
+                  "rbh-gc --config $conf rbh:$db:$testdb -type d -size +3 -s 53" \
+                  "rbh-gc --config $conf rbh:$db:$testdb -s 42 --verbose" \
+                  "rbh-gc --config $conf rbh:$db:$testdb --dry-run" \
+                  "rbh-gc rbh:$db:$testdb"
+}
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
-declare -a tests=(test_invalid test_last_1 test_last_N test_more_than_N)
+declare -a tests=(test_invalid test_last_1 test_last_N test_more_than_N
+                  test_timestamps test_command_line)
 
 tmpdir=$(mktemp --directory)
 trap -- "rm -rf '$tmpdir'" EXIT
