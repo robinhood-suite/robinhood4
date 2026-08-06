4bb4f92997c2806c6d0373294965ef00d39aeda9
VALERI Yoann <yoann.valeri@cea.fr>
2026-08-06 12:09:31 +0200

    log: add complete last logs test
    Generates random logs and check they have the correct form.
    
    Change-Id: Icf6b57d6932c76272a34c885d3205e321fa5be2b
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-log/tests/test_last_logs.bash
+++ rbh-log/tests/test_last_logs.bash
@@ -9,6 +9,7 @@
 test_dir=$(dirname $(readlink -e $0))
 . $test_dir/../../utils/tests/framework.bash
 . $test_dir/common_logs.bash
+. $test_dir/lustre_utils.bash
 
 ################################################################################
 #                                    TESTS                                     #
@@ -27,77 +28,184 @@ test_invalid()
     return 0
 }
 
-check_sync_log_result()
+# Each of the following functions will check the associated command look to be
+# outputting the correct lines, and return the whole output with the current
+# command truncated.
+#
+# The "|| true" at the end of each functions is necessary because tail outputs
+# an error if there are not enough lines to read, which should be the case for
+# the very last log.
+
+check_rbh_sync()
 {
     local output="$1"
+
+    local count=10
+    local output="$(echo "$full_output" | head -n $count)"
 
     check_common_logs "$output" rbh-sync "rbh-sync rbh:posix:. rbh:$db:$testdb"
 
-    local mountpoint=$(echo "$output" | grep "Mountpoint used" |
-                       cut -d':' -f2- | xargs)
+    echo "$output" | grep "Mountpoint" > /dev/null ||
+        error "source_mountpoint should have been retrieved"
 
-    if [ "$mountpoint" != "$(pwd)" ]; then
-        error "Invalid mountpoint"
-    fi
+    echo "$output" | grep "converted" > /dev/null ||
+        error "converted_entries should have been retrieved"
 
-    local converted_entries=$(echo "$output" | grep "converted" |
-                              cut -d':' -f2- |xargs)
+    echo "$output" | grep "skipped" > /dev/null ||
+        error "skipped_entries should have been retrieved"
 
-    local skipped_entries=$(echo "$output" | grep "skipped" |
-                            cut -d':' -f2- |xargs)
+    echo "$output" | grep "seen" > /dev/null ||
+        error "total_entries should have been retrieved"
 
-    local total_entries=$(echo "$output" | grep "seen" |
-                          cut -d':' -f2- |xargs)
-
-    local sum_entries=$((skipped_entries + converted_entries))
-
-    local find_entries=$(find . | wc -l)
-
-    if [ "$sum_entries" != "$find_entries" ]; then
-        error "The sum of converted and skipped entries does not match the
-               number of entries inside the directory"
-    fi
-
-    if [ "$sum_entries" != "$total_entries" ]; then
-        error "The sum of converted and skipped entries does not match
-               total_entries_seen"
-    fi
+    echo "$full_output" | tail +$count || true
 }
 
-check_find_log_result()
+check_rbh_find()
 {
-    local output="$1"
+    local full_output="$1"
 
-    check_common_logs "$output" rbh-find "rbh-find rbh:$db:$testdb"
+    local count=8
+    local output="$(echo "$full_output" | head -n $count)"
+
+    check_common_logs "$output" rbh-find \
+        "rbh-find rbh:$db:$testdb -exec ls ;"
 
     echo "$output" | grep "post-filtering" > /dev/null ||
         error "entry_count should have been retrieved"
+
+    echo "$output" | grep "exec" > /dev/null ||
+        error "exec_success_count should have been retrieved"
+
+    echo "$full_output" | tail +$count || true
+}
+
+check_rbh_fsevents()
+{
+    local full_output="$1"
+
+    local count=15
+    local output="$(echo "$full_output" | head -n $count)"
+
+    check_common_logs "$output" rbh-fsevents \
+        "rbh-fsevents --enrich rbh:lustre:$LUSTRE_DIR src:lustre:$LUSTRE_MDT rbh:$db:$testdb"
+
+    echo "$output" | grep "Enrichment" > /dev/null ||
+        error "enrich_mountpoint should have been retrieved, got '$output'"
+
+    echo "$output" | grep "Source" > /dev/null ||
+        error "source_read should have been retrieved, got '$output'"
+
+    echo "$output" | grep "Number" > /dev/null ||
+        error "worker_count should have been retrieved, got '$output'"
+
+    echo "$output" | grep "Amount" > /dev/null ||
+        error "changelog_read should have been retrieved, got '$output'"
+
+    echo "$output" | grep "Starting" > /dev/null ||
+        error "start_index should have been retrieved, got '$output'"
+
+    echo "$output" | grep "reading/deduplicating" > /dev/null ||
+        error "time_read_dedup should have been retrieved, got '$output'"
+
+    echo "$output" | grep "enriching/updating" > /dev/null ||
+        error "time_enrich_update should have been retrieved, got '$output'"
+
+    echo "$output" | grep "skipped" > /dev/null ||
+        error "enrich_skip_count should have been retrieved, got '$output'"
+
+    echo "$output" | grep "Ratio" > /dev/null ||
+        error "deduplication_ratio should have been retrieved, got '$output'"
+
+    echo "$full_output" | tail +$count || true
+}
+
+check_rbh_report()
+{
+    local full_output="$1"
+
+    local count=6
+    local output="$(echo "$full_output" | head -n $count)"
+
+    check_common_logs "$output" rbh-report \
+        "rbh-report rbh:$db:$testdb --group-by statx.uid --output sum(statx.size)"
+
+    echo "$full_output" | tail +$count || true
+}
+
+check_rbh_gc()
+{
+    local full_output="$1"
+
+    local count=10
+    local output="$(echo "$full_output" | head -n $count)"
+
+    check_common_logs "$output" rbh-gc "rbh-gc rbh:$db:$testdb --sync-time 42"
+
+    echo "$output" | grep " deleted " > /dev/null ||
+        error "deleted_entries should have been retrieved, got '$output'"
+
+    echo "$output" | grep "non-deleted" > /dev/null ||
+        error "non_deleted_entries should have been retrieved, got '$output'"
+
+    echo "$output" | grep "Sync" > /dev/null ||
+        error "sync_time should have been retrieved, got '$output'"
+
+    echo "$output" | grep "seen" > /dev/null ||
+        error "total_entries should have been retrieved, got '$output'"
+
+    echo "$full_output" | tail +$count || true
 }
 
 test_last_logs()
 {
     rbh_sync "rbh:posix:." "rbh:$db:$testdb"
-    rbh_find "rbh:$db:$testdb" > /dev/null
 
-    local output=$(rbh_log "rbh:$db:$testdb" --last 2)
-    local tmp_output=$(rbh_log "rbh:$db:$testdb" --last 20)
+    # Output 20 random ints between 0 and 4
+    for i in $(shuf -i 0-4 -r -n 20); do
+        case "$i" in
+            0)
+                rbh_sync rbh:posix:. rbh:$db:$testdb
+                ;;
+            1)
+                rbh_find rbh:$db:$testdb -exec ls \; > /dev/null
+                ;;
+            2)
+                rbh_fsevents --enrich rbh:lustre:$LUSTRE_DIR \
+                    src:lustre:$LUSTRE_MDT rbh:$db:$testdb > /dev/null
+                ;;
+            3)
+                rbh_report rbh:$db:$testdb \
+                    --group-by "statx.uid" \
+                    --output "sum(statx.size)" > /dev/null
+                ;;
+            4)
+                rbh_gc rbh:$db:$testdb --sync-time 42
+                ;;
+        esac
+    done
+
+    local output=$(rbh_log "rbh:$db:$testdb" --last 5)
+    local tmp_output=$(rbh_log "rbh:$db:$testdb" --last 5)
 
     if [ "$output" != "$tmp_output" ]; then
         error "Outputted logs should have been the same, got '$output' and '$tmp_output'"
     fi
 
-    local n_lines=$(echo "$output" | wc -l)
+    while [ ! -z "$output" ]; do
+        local command="$(echo "$output" | head -n 1)"
 
-    if ((n_lines != 10 + 7)); then
-        error "There should be 10 lines about sync (8 for content, 2 for header/footer) + 7 for find (5/2 aswell), got '$output'"
-    fi
-
-    local find_log="$(echo "$output" | head -n 7)"
-    check_find_log_result "$find_log"
-    output="$(echo "$output" | sed 1,7d)"
-
-    local sync_log="$(echo "$output" | head -n 10)"
-    check_sync_log_result "$sync_log"
+        if [[ $command == *"rbh-sync"* ]]; then
+            output="$(check_rbh_sync "$output")"
+        elif [[ $command == *"rbh-find"* ]]; then
+            output="$(check_rbh_find "$output")"
+        elif [[ $command == *"rbh-fsevents"* ]]; then
+            output="$(check_rbh_fsevents "$output")"
+        elif [[ $command == *"rbh-report"* ]]; then
+            output="$(check_rbh_report "$output")"
+        elif [[ $command == *"rbh-gc"* ]]; then
+            output="$(check_rbh_gc "$output")"
+        fi
+    done
 }
 
 ################################################################################
@@ -106,8 +214,17 @@ test_last_logs()
 
 declare -a tests=(test_invalid test_last_logs)
 
-tmpdir=$(mktemp --directory)
-trap -- "rm -rf '$tmpdir'" EXIT
+LUSTRE_DIR=/mnt/lustre/
+cd "$LUSTRE_DIR"
+
+LUSTRE_MDT=lustre-MDT0000
+userid="$(start_changelogs "$LUSTRE_MDT")"
+
+tmpdir=$(mktemp --directory --tmpdir=$LUSTRE_DIR)
+lfs setdirstripe -D -i 0 $tmpdir
+trap -- "rm -rf '$tmpdir'; stop_changelogs '$LUSTRE_MDT' '$userid'" EXIT
 cd "$tmpdir"
 
-run_tests ${tests[@]}
+sub_setup=lustre_setup
+sub_teardown=lustre_teardown
+run_tests "${tests[@]}"
