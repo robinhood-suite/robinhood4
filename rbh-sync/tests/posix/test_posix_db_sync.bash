b1ff279300b191673279d8380d8f67d1ea529166
VALERI Yoann <yoann.valeri@cea.fr>
2026-08-25 23:31:13 +0200

    sync: add small test for stats shown
    This patch adds a small test for stats shown. Doing more testing on the
    values will be complicated, especially for logs shown during the command
    is run, so instead we verify the very last log contains the correct
    values. We will see later if and how better testing can be done.
    
    Change-Id: I106be0516154c89e4921dcd7115dada08e6abda1
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://review.gerrithub.io/c/robinhood-suite/robinhood4/+/1246960
    Reviewed-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- rbh-sync/tests/posix/test_posix_db_sync.bash
+++ rbh-sync/tests/posix/test_posix_db_sync.bash
@@ -586,6 +586,23 @@ test_nb_children_two_sync()
         '"xattrs.nb_children.value": 0'
 }
 
+test_stats()
+{
+    mkdir -p {1..9}/{1..9}
+
+    local output="$(rbh_sync_posix . rbh:$db:$testdb)"
+
+    echo "$output" | grep "rbh-sync" > /dev/null ||
+        error "Should have found 'rbh-sync' mentionned, got '$output'"
+
+    # 9 * 9 + 9 directories + root = 91 entries
+    echo "$output" | grep "progress" | grep "91" > /dev/null ||
+        error "Last log shown should have found 91 entries in total, got '$output'"
+
+    echo "$output" | grep "current speed" | grep "entries/sec" > /dev/null ||
+        error "Logs should show the current speed in entries per second, got '$output'"
+}
+
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
@@ -600,6 +617,8 @@ declare -a tests=(test_sync_2_files test_sync_size test_sync_3_files
 
 if [[ $WITH_MPI == true ]]; then
     tests+=(test_sync_large_path test_sync_dir_delete_while_mfu_walk)
+else
+    tests+=(test_stats)
 fi
 
 tmpdir=$(mktemp --directory)
