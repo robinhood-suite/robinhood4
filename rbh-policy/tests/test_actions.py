c92393391f1c82d3d676f6988539f1ef62d62ce5
YValeri <yoannvaleri@netcourrier.com>
2026-08-28 14:18:49 +0200

    policy: ensure 'test_actions' work for different languages
    'test_actions' use regex to determine the output of certains policies,
    but it expects the output format to follow english format for dates,
    i.e. month then day. Since it doesn't work for other format, for
    instance, french, simply update the regex to match any date format used.
    
    Change-Id: I4b62b304f17d2eeda43c05b4d5902bd484d6364a
    Signed-off-by: YValeri <yoannvaleri@netcourrier.com>

--- rbh-policy/tests/test_actions.py
+++ rbh-policy/tests/test_actions.py
@@ -622,7 +622,7 @@ declare_policy(
 
             expected_line = (
                 fr"-rw-r--r--\.? \d+ root root {file_size} "
-                fr"\w+\s+\d+\s+\d+:\d+ {file_path}"
+                fr"\w+\s+\w+\s+\d+:\d+ {file_path}"
             )
 
             self.assertRegex(output, expected_line,
@@ -702,7 +702,7 @@ declare_policy(
 
             file_pattern = (
                 fr"-rw-r--r--\.? \d+ root root {file_size} "
-                fr"\w+\s+\d+\s+\d+:\d+ {expected_path}"
+                fr"\w+\s+\w+\s+\d+:\d+ {expected_path}"
             )
             self.assertRegex(output, file_pattern,
                              f"Expected file line for {name} not found in "
