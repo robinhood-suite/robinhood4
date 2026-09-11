90263fde445dce8b27601189165ddfb3ed975853
VALERI Yoann <yoann.valeri@cea.fr>
2026-09-11 16:48:56 +0200

    mpi: properly test parallel log insertion
    This patch adds a stat test to verify the log recorded in the database
    by multiple MPI processes are correct. The current POSIX stat test is
    duplicated, as the non-MPI test only has to check the output and can
    expect all actual values to be in one log. On the other hand, the MPI
    test will have multiple processes each showing their own values, so we
    have to do so more formatting and sums to check the expected values.
    
    Change-Id: Ie2303213d4b078031b8850020ecc32a75a4682f3
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-sync/tests/posix/test_posix_db_sync.bash
+++ rbh-sync/tests/posix/test_posix_db_sync.bash
@@ -295,36 +295,6 @@ EOF
     ! (find_attribute '"ns.xattrs.path": "/dir"')
 }
 
-test_stats()
-{
-    mkdir -p {1..9}/{1..9}
-
-    local output="$(rbh_sync_posix . rbh:$db:$testdb --stats)"
-
-    echo "$output" | grep "rbh-sync" > /dev/null ||
-        error "Should have found 'rbh-sync' mentionned, got '$output'"
-
-    # 9 * 9 + 9 directories + root = 91 entries
-    echo "$output" | grep "progress" | grep "91" > /dev/null ||
-        error "Last log shown should have found 91 entries in total, got '$output'"
-
-    echo "$output" | grep "current speed" | grep "entries/sec" > /dev/null ||
-        error "Logs should show the current speed in entries per second, got '$output'"
-
-    rbh_sync_posix . rbh:$db:$testdb --stats --log-file logs.txt
-    output="$(cat logs.txt)"
-
-    echo "$output" | grep "rbh-sync" > /dev/null ||
-        error "Should have found 'rbh-sync' mentionned, got '$output'"
-
-    # 9 * 9 + 9 directories + root + logs.txt = 92 entries
-    echo "$output" | grep "progress" | grep "92" > /dev/null ||
-        error "Last log shown should have found 92 entries in total, got '$output'"
-
-    echo "$output" | grep "current speed" | grep "entries/sec" > /dev/null ||
-        error "Logs should show the current speed in entries per second, got '$output'"
-}
-
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
@@ -337,8 +307,6 @@ declare -a tests=(test_sync_2_files test_sync_size test_sync_3_files
 
 if [[ $WITH_MPI == true ]]; then
     tests+=(test_sync_large_path test_sync_dir_delete_while_mfu_walk)
-else
-    tests+=(test_stats)
 fi
 
 tmpdir=$(mktemp --directory)
